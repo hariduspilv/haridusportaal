93c15f209b14ad2cb4a1951989f18d3f5f11e80f
Indrek Vallsalu <indrek.vallsalu@tieto.com>
2018-04-04 14:41:25 +0300

    Arendus: Liides - Initial commit

---
+++ services/xRoad/src/main/java/ee/htm/portal/services/JRoadApplication.java
@@ -0,0 +1,17 @@
+package ee.htm.portal.services;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class JRoadApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JRoadApplication.class, args);
+    }
+
+    @Override
+    public void run(String[] args) {
+        System.out.println("Tere Maailm!");
+    }
+}
---
+++ services/xRoad/src/test/java/ee/htm/portal/services/jRoad/JRoadApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.htm.portal.services.jRoad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JRoadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
