0e7ea03137dd539ee0c5aec3a8088addab962ec3
Indrek Vallsalu <indrek.vallsalu@tieto.com>
2018-05-09 16:44:37 +0300

    #65 vpTaotlusOpingud impl

--- services/jRoad/src/main/java/ee/htm/portal/services/client/EhisV6XRoadService.java
+++ services/jRoad/src/main/java/ee/htm/portal/services/client/EhisV6XRoadService.java
@@ -2,17 +2,20 @@ package ee.htm.portal.services.client;
 
 import com.nortal.jroad.client.exception.XRoadServiceConsumptionException;
 import ee.htm.portal.services.types.ee.riik.xtee.ehis.producers.producer.ehis.EeIsikukaartResponseDocument.EeIsikukaartResponse;
+import ee.htm.portal.services.types.ee.riik.xtee.ehis.producers.producer.ehis.VpTaotlusOpingudResponseDocument.VpTaotlusOpingudResponse;
 
 /**
  * <code>ehisv6</code> EHIS (Eesti Hariduse Infosüsteem -- Haridus- ja Teadusministeerium) database
  * X-tee v6 service.
+ *
+ * @param userId - if null gets its value from xroad.properties id-code
  */
 public interface EhisV6XRoadService {
 
-  /**
-   * @param userId - if null gets its value from xroad.properties id-code
-   */
   EeIsikukaartResponse eeIsikukaart(String isikukood, String format, String userId)
+      throws XRoadServiceConsumptionException;
+
+  VpTaotlusOpingudResponse vptOpingud(String isikukood, Object taotlusId, String userId)
       throws XRoadServiceConsumptionException;
 
 }
