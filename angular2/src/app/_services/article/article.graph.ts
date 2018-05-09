aa519ca6bcf93e6664a99759f3f2fc11351b7e2d
Jegor Salajev <fixis.rus@gmail.com>
2018-05-09 12:14:24 +0300

    Article right sidebar added;

--- angular2/src/app/_services/article/article.graph.ts
+++ angular2/src/app/_services/article/article.graph.ts
@@ -1,51 +1,128 @@
 import gql from 'graphql-tag';
 
 export class ArticleGraph {
-
+  
   buildSingle(path) {
     return gql`
-      query {
-        route(path: "${path}"){
-          ... on EntityCanonicalUrl {
-            languageSwitchLinks{
-              active
+    query {
+      route(path: "${path}"){
+        ... on EntityCanonicalUrl {
+          languageSwitchLinks{
+            active
+            title
+            language {
+              id
+            }
+            url {
+              path
+              routed
+              pathAlias
+              pathInternal
+            }
+          }
+          breadcrumb{
+            text
+            url {
+              path
+              routed
+            }
+          }
+          entity {
+            ... on NodeArticle{
               title
-              language {
-                id
+              body {
+                value
+                summary
+                format
               }
-              url {
-                path
-                routed
-                pathAlias
-                pathInternal
-              }
-            }
-            breadcrumb{
-              text
-              url {
-                path
-                routed
-              }
-            }
-            entity {
-             ... on NodeArticle{
+              fieldImage {
+                url
+                targetId
+                alt
                 title
-                body {
-                  value
-                  summary
-                  format
-                }
-                fieldImage {
-                  url
-                  targetId
-                  alt
-                  title
-                }
               }
             }
           }
         }
       }
+    }
     `;
   }
 }
+export const getArticleData = gql`
+query getArticleData($path: String!) {
+  route(path: $path) {
+    ... on EntityCanonicalUrl {
+      languageSwitchLinks {
+        active
+        title
+        language {
+          id
+        }
+        url {
+          path
+          routed
+          pathAlias
+          pathInternal
+        }
+      }
+      breadcrumb {
+        text
+        url {
+          path
+          routed
+        }
+      }
+      
+      entity {
+        ... on NodeArticle {
+          title
+          body {
+            value
+            summary
+          }
+          fieldImage {
+            url
+            targetId
+            alt
+            title
+          }
+          fieldRightSidebar {
+            entity {
+              fieldAdditional {
+                entity {
+                  fieldTitle
+                  fieldAdditionalBody {
+                    value
+                  }
+                }
+              }
+              fieldContactSection {
+                entity {
+                  fieldPhone
+                  fieldPerson
+                  fieldEmail
+                  fieldOrganization
+                }
+              }
+              fieldRelatedArticle{
+                url {
+                  path
+                }
+                title
+              }
+              fieldHyperlinks {
+                url {
+                  path
+                }
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+`;
--- angular2/src/app/_views/article/article.component.ts
+++ angular2/src/app/_views/article/article.component.ts
@@ -1,49 +1,103 @@
 import { Component, OnDestroy, ViewChild, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, ActivatedRoute } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+import { Params, Router, ActivatedRoute } from '@angular/router';
 import { ArticleService, RootScopeService } from '../../_services';
+import { getArticleData } from '../../_services/article/article.graph';
+
+
+import { Apollo } from 'apollo-angular';
+import { Subscription } from 'rxjs/Subscription'; 
+
+import { throwMatDialogContentAlreadyAttachedError } from '@angular/material';
+import { link } from 'fs';
 
 @Component({
   templateUrl: './article.component.html'
 })
 
-export class ArticleComponent {
-
+export class ArticleComponent implements OnInit, OnDestroy{
+  
   content: any;
   breadcrumb: any;
   error: boolean;
-
-  constructor(private router: Router, private route: ActivatedRoute, private articleService: ArticleService, private rootScope:RootScopeService) {
-
+  
+  fieldRightSidebar: any;
+  toggleRightSidebar: boolean = false;
+  
+  fieldAdditional: any;
+  fieldAdditionalTitle: any;
+  fieldAdditionalBody: any;
+  
+  fieldContactSection: any;
+  fieldContactPerson: any;
+  fieldContactPhone: any;
+  fieldContactEmail: any;
+  fieldContactOrganization: any;
+  
+  articleLinks: any[];
+  relatedArticles: any[];
+  
+  constructor(private router: Router, private route: ActivatedRoute, private articleService: ArticleService, private rootScope:RootScopeService, private apollo: Apollo) {
+    
     this.route.params.subscribe( params => {
-
-      this.content = false;
-      this.error = false;
-      this.breadcrumb = false;
-      
-      const path = this.router.url;
-
-      const that = this;
-
+      const path = this.router.url;      
       articleService.getArticle(path, function(data) {
-
         const langOptions = data['route']['languageSwitchLinks'];
         let langValues = {};
         for( var i in langOptions ){
           langValues[langOptions[i].language.id] = langOptions[i].url.path;
         }
         rootScope.set('langOptions', langValues);
-
-        if ( data['route'] == null ) {
-          that.error = true;
-        } else {
-          that.content = data['route']['entity'];
-          that.breadcrumb = data['route']['breadcrumb'];
-        }
-
       });
-
     });
   }
-
+  
+  private querySubscription: Subscription;
+  
+  ngOnInit() {
+    const path = this.router.url;
+    const lang = this.rootScope.get("currentLang");   
+      
+    this.querySubscription = this.apollo.watchQuery({
+      query: getArticleData,
+      variables: {
+        path: path,
+        lang: lang.toUpperCase(),
+      },
+    })
+    .valueChanges.subscribe(({data, loading}) => {
+      this.content = data['route']['entity'];
+      this.breadcrumb = data['route']['breadcrumb'];
+      
+      this.fieldRightSidebar = data['route']['entity']['fieldRightSidebar']
+      
+      if (this.fieldRightSidebar !== null) {
+        
+        this.toggleRightSidebar = true;
+        this.fieldContactSection = data['route']['entity']['fieldRightSidebar']['entity']['fieldContactSection'];
+        this.fieldAdditional = data['route']['entity']['fieldRightSidebar']['entity']['fieldAdditional'];
+        this.articleLinks = data['route']['entity']['fieldRightSidebar']['entity']['fieldHyperlinks'];
+        this.relatedArticles = data['route']['entity']['fieldRightSidebar']['entity']['fieldRelatedArticle'];        
+        
+        if(this.fieldAdditional !== null) {
+          this.fieldAdditionalTitle = data['route']['entity']['fieldRightSidebar']['entity']['fieldAdditional']['entity']['fieldTitle'];
+          this.fieldAdditionalBody = data['route']['entity']['fieldRightSidebar']['entity']['fieldAdditional']['entity']['fieldAdditionalBody']['value'];
+        }
+        
+        if(this.fieldContactSection !== null) {
+          this.fieldContactPerson = data['route']['entity']['fieldRightSidebar']['entity']['fieldContactSection']['entity']['fieldPerson'];
+          this.fieldContactPhone = data['route']['entity']['fieldRightSidebar']['entity']['fieldContactSection']['entity']['fieldPhone'];
+          this.fieldContactEmail = data['route']['entity']['fieldRightSidebar']['entity']['fieldContactSection']['entity']['fieldEmail'];
+          this.fieldContactOrganization = data['route']['entity']['fieldRightSidebar']['entity']['fieldContactSection']['entity']['fieldOrganization'];
+        }
+      }
+      // console.log(data['route']['languageSwitchLinks'])
+    });
+  }
+  
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
+  }
+  
 }
