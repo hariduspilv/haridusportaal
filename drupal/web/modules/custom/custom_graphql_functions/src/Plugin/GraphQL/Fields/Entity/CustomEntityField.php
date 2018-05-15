4d1bd64826390886abd8dfeba969284fb1c728d4
ruut12 <henri.ruut@twn.ee>
2018-05-15 18:41:57 +0300

    GraphQl paragraphs field translation check

---
+++ drupal/web/modules/custom/custom_graphql_functions/src/Plugin/GraphQL/Fields/Entity/CustomEntityField.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\custom_graphql_functions\Plugin\GraphQL\Fields\Entity;
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\graphql\GraphQL\Cache\CacheableValue;
+use Drupal\graphql\GraphQL\Execution\ResolveContext;
+use Drupal\graphql_core\Plugin\GraphQL\Fields\EntityFieldBase;
+use GraphQL\Type\Definition\ResolveInfo;
+
+/**
+ * @GraphQLField(
+ *   id = "custom_entity_field",
+ *   secure = true,
+ *   weight = -2,
+ *   deriver = "Drupal\graphql_core\Plugin\Deriver\Fields\EntityFieldDeriver",
+ * )
+ */
+class CustomEntityField extends EntityFieldBase {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
+		if ($value instanceof FieldableEntityInterface) {
+			//dump($value);
+			$definition = $this->getPluginDefinition();
+			$name = $definition['field'];
+			if ($value->hasField($name)) {
+				/** @var \Drupal\Core\Field\FieldItemListInterface $items */
+				$items = $value->get($name);
+				$access = $items->access('view', NULL, TRUE);
+
+				if ($access->isAllowed()) {
+					foreach ($items as $item) {
+						if(empty($definition['property'])){
+							if($item->entity->getEntityTypeId() === 'paragraph'){
+								$translated = ($item->entity->hasTranslation($context->getContext('language', $info)));
+
+								//if paragraph is not translated continue;
+								if(!$translated) continue;
+							}
+						}
+
+						$output = !empty($definition['property']) ? $this->resolveItem($item, $args, $context, $info) : $item;
+						yield new CacheableValue($output, [$access]);
+					}
+				}
+			}
+		}
+	}
+}
