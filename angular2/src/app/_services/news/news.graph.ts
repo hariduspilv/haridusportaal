9eb2c855884ae2d01c392e43be3185d105f627f1
Jegor Salajev <fixis.rus@gmail.com>
2018-05-22 13:39:54 +0300

    Title and Date filter query added; Component changed;

--- angular2/src/app/_services/news/news.graph.ts
+++ angular2/src/app/_services/news/news.graph.ts
@@ -1,49 +1,49 @@
 import gql from 'graphql-tag';
 
 export class NewsGraph {
-
+  
   buildList(lang, offset, limit) {
-
+    
     lang = lang.toUpperCase();
-
+    
     return gql`
-      query{
-        nodeQuery(offset: ${offset}, limit: ${limit}, sort: {field: "created", direction: DESC}, filter: {conditions: [{operator: EQUAL, field: "type", value: ["news"], language: ${lang}}]}) {
-          entities {
-            entityTranslation(language: ${lang}) {
-              ... on NodeNews {
-                entityLabel
-                created
-                entityUrl {
-                  ... on EntityCanonicalUrl {
-                    path
-                    languageSwitchLinks {
-                      active
-                      title
-                    }
+    query{
+      nodeQuery(offset: ${offset}, limit: ${limit}, sort: {field: "created", direction: DESC}, filter: {conditions: [{operator: EQUAL, field: "type", value: ["news"], language: ${lang}}]}) {
+        entities {
+          entityTranslation(language: ${lang}) {
+            ... on NodeNews {
+              entityLabel
+              created
+              entityUrl {
+                ... on EntityCanonicalUrl {
+                  path
+                  languageSwitchLinks {
+                    active
+                    title
                   }
                 }
-                fieldShortDescription
-                fieldAuthor
-                fieldIntroductionImage {
-                  url
-                  alt
-                }
-                fieldNewsTag {
-                  entity {
-                    entityLabel
-                  }
+              }
+              fieldShortDescription
+              fieldAuthor
+              fieldIntroductionImage {
+                url
+                alt
+              }
+              fieldNewsTag {
+                entity {
+                  entityLabel
                 }
               }
             }
           }
         }
       }
+    }
     `;
   }
-
+  
   buildSingle(lang, path) {
-
+    
     lang = lang.toUpperCase();
     return gql`
     query{
@@ -113,11 +113,11 @@ export class NewsGraph {
     }
     `;
   }
-
+  
   buildRecent(nid, lang) {
-
+    
     lang = lang.toUpperCase();
-
+    
     return gql`
     query{
       nodeQuery(limit: 3, sort: {field: "created", direction: DESC}, filter: {conditions: [{operator: EQUAL, field: "type", value: ["news"], language: ${lang} }, {operator: NOT_EQUAL, field: "nid", value: ["${nid}"], language:${lang}}]}) {
@@ -145,8 +145,16 @@ export class NewsGraph {
     `;
   }
 }
+export const getNewsTags = gql`
+query getNewsTags($lang: LanguageId!){
+  taxonomyTermQuery(limit: 1000, filter: {conditions: [{operator: EQUAL, field: "vid", value: ["tags"], language: $lang}]}) {
+    entities {
+      entityLabel
+    }
+  }
+}
 
-
+`;
 
 // query{
 //   nodeQuery(
@@ -187,43 +195,65 @@ export class NewsGraph {
 // }
 
 
-
-// sildi jargi
+export const sortByOptions = gql`
+query sortByOptions (
+  $tagValue: [String],
+  $tagEnabled: Boolean,
+  $titleValue: String,
+  $titleEnabled: Boolean,
+  $minDate: String,
+  $maxDate: String,
+  $lang: LanguageId!,
+  $offset: Int,
+  $limit: Int ) {
+  nodeQuery(offset: $offset, limit: $limit, sort: {field: "created", direction: DESC},
+  filter: {conjunction: AND, conditions: [
+    {operator: EQUAL, field: "type", value: ["news"], language: $lang},
+    {operator: IN, field: "field_news_tag.entity.tid", value: $tagValue, language: $lang, enabled: $tagEnabled },
+    {operator: LIKE, field: "title", value: [$titleValue], language: $lang, enabled: $titleEnabled },
+    {operator: GREATER_THAN_OR_EQUAL, field: "created", value: [$minDate], language: $lang },
+    {operator: SMALLER_THAN_OR_EQUAL, field: "created", value: [$maxDate], language: $lang },
+    
+  ]}) {
+    entities {
+      ... on NodeNews {
+        entityLabel
+        created
+        entityUrl {
+          ... on EntityCanonicalUrl {
+            path
+            languageSwitchLinks {
+              active
+              title
+            }
+          }
+        }
+        fieldShortDescription
+        fieldAuthor
+        fieldIntroductionImage {
+          url
+          alt
+        }
+        fieldNewsTag {
+          entity {
+            entityLabel
+            tid
+          }
+        }
+      }
+    }
+  }
+}
+`;
 
 // {
-//   nodeQuery(offset: 0, limit: 10, sort: {field: "created", direction: DESC},
-//     filter: {conditions: [
-//       {operator: EQUAL, field: "type", value: ["news"], language: ET},
-//       {operator: EQUAL, field: "field_news_tag.entity.tid", value: ["21"], language: ET}
-//     ]}) {
-//     entities {
-//       entityTranslation(language: ET) {
-//         ... on NodeNews {
-//           entityLabel
-//           created
-//           entityUrl {
-//             ... on EntityCanonicalUrl {
-//               path
-//               languageSwitchLinks {
-//                 active
-//                 title
-//               }
-//             }
-//           }
-//           fieldShortDescription
-//           fieldAuthor
-//           fieldIntroductionImage {
-//             url
-//             alt
-//           }
-//           fieldNewsTag {
-//             entity {
-//               entityLabel
-//               tid
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
+//   "lang": "ET",
+//   "offset": 0,
+//   "limit": 10,
+//   "tagValue": "21",
+//   "tagEnabled": false,
+//   "titleValue": "%n%",
+//   "titleEnabled": false,
+//   "minDate": "-2147483647",
+//   "maxDate": "2147483647"
 // }
--- angular2/src/app/_views/news/news.component.ts
+++ angular2/src/app/_views/news/news.component.ts
@@ -1,57 +1,76 @@
 import { Component, OnDestroy, ViewChild, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NewsService, RootScopeService } from '../../_services';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { componentFactoryName } from '@angular/compiler';
-import { AppComponent } from '../../app.component';
-import { Subscription } from 'rxjs/Subscription';
 import { MomentModule } from 'angular2-moment/moment.module';
-import { Apollo } from 'apollo-angular';
+import { Apollo, QueryRef } from 'apollo-angular';
+import * as moment from 'moment';
+
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/filter';
+
+import { NewsService, RootScopeService } from '../../_services';
+import { AppComponent } from '../../app.component';
 import { getBreadcrumb } from '../../_services/breadcrumb/breadcrumb.graph';
+import { sortByOptions, getNewsTags } from '../../_services/news/news.graph';
 
 @Component({
   templateUrl: './news.component.html'
 })
 
-export class NewsComponent implements OnInit{
+export class NewsComponent implements OnInit, OnDestroy{
   
-  private querySubscription: Subscription;  
-  private path: string;
-  private lang: string;
+  // TAGS
   
-  breadcrumb: any;
+  newsTags: any[];
+  selected: any[];
+  // for datepicker
   
-  content: any;
-  unix: any;
-  error: boolean;
-  offset: number;
-  limit: number;
-  listEnd: boolean;
-  list: any;
+  public newsTagsSelectForm = new FormControl();
+  public titleForm = new FormControl();
+  public minDateForm = new FormControl();
+  public maxDateForm = new FormControl();
   
-  constructor( private router: Router, private route: ActivatedRoute, private newsService: NewsService, private rootScope:RootScopeService, private apollo: Apollo ) {
-    
-    const that = this;
-    this.limit = 10;
-    this.offset = 0;
-    this.listEnd = false;
-    
-    this.setPaths();
-    
-    
-    this.route.params.subscribe( params => {
-      
-      newsService.getList(this.offset, this.limit, function(data){
-        that.list = data['nodeQuery']['entities'];
-        
-        if( that.list.length < that.limit ){
-          that.listEnd = true;
-        }
-      });
-      
-    });
-  }
+  
+  public querySubscription: Subscription;  
+  public breadcrumb: any;
+  public error: boolean;
+  
+  public listEnd: boolean = false;
+  public path: string;
+  public lang: string;
+  
+  public list: any;
+  public offset: number = 0; 
+  public limit: number = 10;
+  
+  public tagValue: Array<string> = [];
+  public titleValue: string = "";
+  public tagEnabled: boolean = false;
+  public titleEnabled: boolean = false;
+  public minDate: String = "-2147483647";
+  public maxDate: String = "2147483647";
+  
+  constructor ( 
+    private router: Router,
+    private route: ActivatedRoute,
+    private newsService: NewsService,
+    private rootScope: RootScopeService,
+    private apollo: Apollo
+  ) { }
+  
+  
+  private subBreadcrumbs: Subscription;
+  
+  paramName: string;
+  paramLastName: string
+  
+  
   
   setPaths() {
     this.rootScope.set('langOptions', {
@@ -60,30 +79,85 @@ export class NewsComponent implements OnInit{
     });
   }
   
-  loadMore() {
-    let that = this;
-    that.offset = that.list.length;
-    
-    that.newsService.getList(that.offset, that.limit, function(data) {
-      if ( data['nodeQuery'] == null ) {
-        that.error = true;
-      } else {
-        
-        that.list = that.list.concat( data['nodeQuery']['entities'] );
-        
-        if( data['nodeQuery']['entities'].length < that.limit ){
-          that.listEnd = true;
-        }
-      }
-    });
-  }
   
-  ngOnInit() {
+  
+  loadMore() {
+    this.offset = this.list.length;
     this.route.params.subscribe(
       (params: ActivatedRoute) => {
         this.path = this.router.url;
         this.lang = params['lang'];
         
+        this.apollo.watchQuery({
+          query: sortByOptions,
+          variables: {
+            tagValue: this.tagValue,
+            tagEnabled: false,
+            titleValue: "%" + this.titleValue + "%",
+            titleEnabled: false,
+            minDate: "-2147483647",
+            maxDate: "2147483647",
+            lang: this.lang.toUpperCase(),
+            offset: this.offset,
+            limit: this.limit            
+          },
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all',
+        }).valueChanges.subscribe(({data, loading}) => {
+          this.list = this.list.concat(data['nodeQuery']['entities']);
+          if ( data['nodeQuery']['entities'] && (data['nodeQuery']['entities'].length < this.limit) ){
+            this.listEnd = true;
+          }
+        });        
+      }
+    )
+  }
+  
+  ngOnInit() {
+    
+    
+    
+    this.setPaths();
+    
+    // query parameters observable
+    this.route.queryParams.subscribe(params => {
+      this.titleValue = params.title;
+      if(this.titleValue != null) {
+        this.titleEnabled = true;
+      }
+      // console.log(this.titleValue)
+    });
+    
+    this.route.params.subscribe(
+      (params: ActivatedRoute) => {
+        
+        this.path = this.router.url;
+        this.lang = params['lang'];     
+        
+        // get news list
+        this.querySubscription = this.apollo.watchQuery({
+          query: sortByOptions,
+          variables: {
+            tagValue: this.tagValue,
+            tagEnabled: false,
+            titleValue: "%" + this.titleValue + "%",
+            titleEnabled: false,
+            minDate: "-2147483647",
+            maxDate: "2147483647",
+            lang: this.lang.toUpperCase(),
+            offset: this.offset,
+            limit: this.limit            
+          },
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all',
+        }).valueChanges.subscribe(({data, loading}) => {
+          this.list = data['nodeQuery']['entities'];
+          if (this.list && (this.list.length < this.limit)){
+            this.listEnd = true;
+          }
+        });
+        
+        // get breadcrumbs
         this.querySubscription = this.apollo.watchQuery({
           query: getBreadcrumb,
           variables: {
@@ -97,7 +171,69 @@ export class NewsComponent implements OnInit{
         .subscribe(({data}) => {
           this.breadcrumb = data['route']['breadcrumb'];
         });
+        
+        // get tags
+        this.querySubscription = this.apollo.watchQuery({
+          query: getNewsTags,
+          variables: {
+            lang: this.lang.toUpperCase(),
+            fetchPolicy: 'no-cache',
+            errorPolicy: 'all',
+          },
+        })
+        .valueChanges
+        .subscribe(({data}) => {
+          this.newsTags = data['taxonomyTermQuery']['entities'];
+        });
+        
       }
     )
+  }
+  
+  newsFilter() {
+    // TITLE FILTER
+    if(this.titleForm.value != null) {
+      this.titleEnabled = true;
+    }
+
+    // DATE FILTER
+    if(this.minDateForm.value != null) { 
+      // this.minDate = (this.minDateForm.value.getTime()/1000).toString();
+      this.minDate = (this.minDateForm.value.getTime()/1000).toString();
+    } else { this.minDate = "-2147483647"; }
+    if(this.maxDateForm.value != null) {
+      this.maxDate = (this.maxDateForm.value.getTime()/1000 + 86399).toString();
+    } else { this.maxDate = "2147483647"; }
+    console.log(this.minDate)
+    console.log(this.maxDate)
+    
+
+    this.offset = 0;
+    this.apollo.watchQuery({
+      query: sortByOptions,
+      variables: {
+        tagValue: this.tagValue,
+        tagEnabled: false,
+        titleValue: "%" + this.titleForm.value + "%",
+        titleEnabled: this.titleEnabled,
+        minDate: this.minDate,
+        maxDate: this.maxDate,
+        lang: this.lang.toUpperCase(),
+        offset: this.offset,
+        limit: this.limit            
+      },
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    }).valueChanges.subscribe(({data, loading}) => {
+      this.list = data['nodeQuery']['entities'];
+      this.listEnd = false;      
+      if (  data['nodeQuery']['entities'] && (data['nodeQuery']['entities'].length < this.limit) ){
+        this.listEnd = true;
+      }
+    });
+  }
+  
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
   }
 }
