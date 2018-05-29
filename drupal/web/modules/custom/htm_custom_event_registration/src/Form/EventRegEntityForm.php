d171512097e67c71fe03186d401f1d10c4a138db
ruut12 <henri.ruut@twn.ee>
2018-05-29 18:04:32 +0300

    Event registration entity

---
+++ drupal/web/modules/custom/htm_custom_event_registration/src/Form/EventRegEntityForm.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\htm_custom_event_registration\Form;
+
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+/**
+ * Form controller for Event registration edit forms.
+ *
+ * @ingroup htm_custom_event_registration
+ */
+class EventRegEntityForm extends ContentEntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    /* @var $entity \Drupal\htm_custom_event_registration\Entity\EventRegEntity */
+    $form = parent::buildForm($form, $form_state);
+
+    $entity = $this->entity;
+
+		$form['name_group'] = [
+			'#type' => 'fieldset',
+			'#title' => $this->t('Participant information'),
+			'#weight' => 2,
+		];
+
+		$form['name_group']['participant_first_name'] = $form['participant_first_name'];
+		unset($form['participant_first_name']);
+
+		$form['name_group']['participant_last_name'] = $form['participant_last_name'];
+		unset($form['participant_last_name']);
+
+		$form['name_group']['participant_organization'] = $form['participant_organization'];
+		unset($form['participant_organization']);
+
+		$form['name_group']['participant_email'] = $form['participant_email'];
+		unset($form['participant_email']);
+
+		$form['name_group']['participant_phone'] = $form['participant_phone'];
+		unset($form['participant_phone']);
+
+		$form['name_group']['participant_comment'] = $form['participant_comment'];
+		unset($form['participant_comment']);
+
+		$form['additional'] = [
+			'#type' => 'fieldset',
+			'#title' => $this->t('Additional information'),
+			'#weight' => 3,
+		];
+
+		$form['additional']['created'] = $form['created'];
+		unset($form['created']);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $entity = $this->entity;
+
+    $status = parent::save($form, $form_state);
+
+    switch ($status) {
+      case SAVED_NEW:
+        drupal_set_message($this->t('Created the %label Event registration.', [
+          '%label' => $entity->label(),
+        ]));
+        break;
+
+      default:
+        drupal_set_message($this->t('Saved the %label Event registration.', [
+          '%label' => $entity->label(),
+        ]));
+    }
+    $form_state->setRedirect('entity.event_reg_entity.canonical', ['event_reg_entity' => $entity->id()]);
+  }
+
+}
---
+++ drupal/web/modules/custom/htm_custom_event_registration/src/Form/EventRegEntitySettingsForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\htm_custom_event_registration\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class EventRegEntitySettingsForm.
+ *
+ * @ingroup htm_custom_event_registration
+ */
+class EventRegEntitySettingsForm extends FormBase {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'eventregentity_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+  /**
+   * Defines the settings form for Event registration entities.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['eventregentity_settings']['#markup'] = 'Settings form for Event registration entities. Manage field settings here.';
+    return $form;
+  }
+
+}
