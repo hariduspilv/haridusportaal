f35e896c7b3b2f79e1507c5896686ee23ba48a69
root <lars.soonvald@gmail.com>
2018-05-31 11:00:35 +0000

    Added more fields to parse

---
+++ drupal/web/modules/custom/custom_study_programme_import/src/Plugin/QueueWorker/StudyProgrammeQueue.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\custom_study_programme_import\Plugin\QueueWorker;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\custom_study_programme_import\Controller\StudyProgrammeController;
+/**
+ * Processes Tasks for Learning.
+ *
+ * @QueueWorker(
+ *   id = "cron_custom_study_programme_import",
+ *   title = @Translation("Import task worker: programme import queue"),
+ *   cron = {"time" = 30}
+ * )
+ */
+class StudyProgrammeQueue extends QueueWorkerBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($programme) {
+    $import_controller = new StudyProgrammeController();
+    $iscedtaxonomy = $import_controller->get_taxonomy_terms('isced_f');
+    $import_controller->save_programme($programme, $iscedtaxonomy);
+  }
+}
