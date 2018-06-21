73494db93e8e7bf47e3485a5773fd0e97b7ca359
ruut12 <henri.ruut@twn.ee>
2018-06-21 11:38:38 +0300

    Favorite entity

---
+++ web/modules/custom/htm_custom_favorites/src/FavoriteEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\htm_custom_favorites;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for favorite_entity.
+ */
+class FavoriteEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
---
+++ web/modules/custom/htm_custom_favorites/src/Form/FavoriteEntityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\htm_custom_favorites\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Favorite entity entities.
+ *
+ * @ingroup htm_custom_favorites
+ */
+class FavoriteEntityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
