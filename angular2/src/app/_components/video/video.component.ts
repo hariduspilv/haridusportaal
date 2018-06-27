a04e1ea1bda0b50fd2b43ae5fe3d8ab03b5471a2
Madis Pold <madis.pold@twn.ee>
2018-06-27 02:36:03 -0700

    video IE compatibility

--- angular2/src/app/_components/video/video.component.ts
+++ angular2/src/app/_components/video/video.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {EmbedVideoService} from 'ngx-embed-video';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'video-component',
@@ -12,10 +13,18 @@ export class VideoComponent {
   @Input() videoObject: any;
   videoUrl: string = "";
 
-  constructor(private embedService: EmbedVideoService){}
+  constructor(private embedService: EmbedVideoService, private sanitizer: DomSanitizer){}
   
   ngOnInit() {
     this.videoUrl = this.embedService.embed(this.videoObject.input);
   }
   
+  fallbackUrl(domain, id) {
+    switch (domain) {
+      case 'youtube.com':
+        return `https://www.youtube.com/embed/${id}`;
+      default:
+        return null
+    }
+  }
 }
