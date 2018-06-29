108e267278ea7185dc99af410926df7598721592
ruut12 <henri.ruut@twn.ee>
2018-06-29 16:57:07 +0300

    Admin + event registration with email template from admin configuration

--- drupal/web/modules/custom/htm_custom_admin_form/src/Form/CustomAdminForm.php
+++ drupal/web/modules/custom/htm_custom_admin_form/src/Form/CustomAdminForm.php
@@ -31,7 +31,7 @@ class CustomAdminForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 		$form = parent::buildForm($form, $form_state);
-    $config = $this->config('htm_custom_admin_form.customadmin');
+		$config = $this->config('htm_custom_admin_form.customadmin');
 
 		$form['tabs'] = [
 			'#type' => 'vertical_tabs',
@@ -44,71 +44,70 @@ class CustomAdminForm extends ConfigFormBase {
 			'#description' => $this->t('Description text here!'),
 			'#group' => 'tabs',
 		];
+			$form['email_event_registration'] = [
+				'#type' => 'details',
+				'#title' => $this->t('Event emails'),
+				'#description' => $this->t('Event registration email settings'),
+				'#group' => 'emails',
+			];
 
-		/* Do we need this? */
-		$form['emails']['token_tree'] = [
-			'#theme' => 'token_tree_link',
-			'#token_types' => array('event_reg_entity'),
-			'#show_restricted' => TRUE,
-			'#global_types' => FALSE,
-			'#weight' => 90,
-		];
+			/* Do we need this? */
+			$form['email_event_registration']['token_tree'] = [
+					'#theme' => 'token_tree_link',
+					'#token_types' => array('event_reg_entity', 'htm_custom_event_tokens'),
+					'#show_restricted' => TRUE,
+					'#global_types' => FALSE,
+					'#weight' => 90,
+			];
 
+			$form['email_event_registration']['fieldset'] = [
+				'#type' => 'fieldset',
+				'#title' => 'Event registration confirmation'
+			];
+			$form['email_event_registration']['fieldset']['email_event_registration_subject'] = [
+				'#type' => 'textfield',
+				'#title' => $this->t('Subject'),
+				'#default_value' => $config->get('emails.email_event_registration.registration_email_subject'),
+				'#maxlength' => 180,
+			];
+			$form['email_event_registration']['fieldset']['email_event_registration_body'] = [
+				'#type' => 'textarea',
+				'#title' => $this->t('Body'),
+				'#default_value' => $config->get('emails.email_event_registration.registration_email_body'),
+			];
 
-		$form['email_event_registration'] = [
-			'#type' => 'details',
-			'#title' => $this->t('Event registration email'),
-			'#description' => $this->t('Event registration email settings'),
-			'#group' => 'emails',
-		];
-
-		$form['email_event_registration']['fieldset'] = [
-			'#type' => 'fieldset',
-			'#title' => 'Event registration confirmation'
-		];
-		$form['email_event_registration']['fieldset']['email_event_registration_subject'] = [
-			'#type' => 'textfield',
-			'#title' => $this->t('Subject'),
-			'#default_value' => $config->get('emails.email_event_registration.registration_email_subject'),
-			'#maxlength' => 180,
-		];
-		$form['email_event_registration']['fieldset']['email_event_registration_body'] = [
-			'#type' => 'textarea',
-			'#title' => $this->t('Body'),
-			'#default_value' => $config->get('emails.email_event_registration.registration_email_body'),
-		];
-
-		$form['email_event_registration']['fieldset_2'] = [
-			'#type' => 'fieldset',
-			'#title' => 'Event organizer notice'
-		];
-		$form['email_event_registration']['fieldset_2']['email_event_notice_subject'] = [
-			'#type' => 'textfield',
-			'#title' => $this->t('Subject'),
-			'#default_value' => $config->get('emails.email_event_registration.organizer_email_subject'),
-			'#maxlength' => 180,
-		];
-		$form['email_event_registration']['fieldset_2']['email_event_notice_body'] = [
-			'#type' => 'textarea',
-			'#title' => $this->t('Body'),
-			'#default_value' => $config->get('emails.email_event_registration.organizer_email_body'),
-		];
+			$form['email_event_registration']['fieldset_2'] = [
+				'#type' => 'fieldset',
+				'#title' => 'Event organizer notice'
+			];
+			$form['email_event_registration']['fieldset_2']['email_event_notice_subject'] = [
+				'#type' => 'textfield',
+				'#title' => $this->t('Subject'),
+				'#default_value' => $config->get('emails.email_event_registration.organizer_email_subject'),
+				'#maxlength' => 180,
+			];
+			$form['email_event_registration']['fieldset_2']['email_event_notice_body'] = [
+				'#type' => 'textarea',
+				'#title' => $this->t('Body'),
+				'#default_value' => $config->get('emails.email_event_registration.organizer_email_body'),
+			];
 
 
-
-
-
-		$form['other_settings'] = array(
+		$form['general'] = [
 			'#type' => 'details',
 			'#title' => $this
-					->t('Some other things here'),
+					->t('General settings'),
 			'#group' => 'tabs',
-		);
-		$form['other_settings']['example'] = array(
-			'#type' => 'textfield',
-			'#title' => $this
-					->t('Example'),
-		);
+			'#weight' => -1
+		];
+		$form['general']['fe_url'] = [
+			'#type' => 'url',
+			'#title' => $this->t('Front-end location'),
+			'#default_value' => $config->get('general.fe_url'),
+			'#maxlength' => 255,
+			'#size' => 30,
+		];
+
     return $form;
   }
 
@@ -129,6 +128,8 @@ class CustomAdminForm extends ConfigFormBase {
       ->set('emails.email_event_registration.registration_email_body', $form_state->getValue('email_event_registration_body'))
 			->set('emails.email_event_registration.organizer_email_subject', $form_state->getValue('email_event_notice_subject'))
 			->set('emails.email_event_registration.organizer_email_body', $form_state->getValue('email_event_notice_body'))
+
+			->set('general.fe_url', $form_state->getValue('fe_url'))
       ->save();
   }
 
