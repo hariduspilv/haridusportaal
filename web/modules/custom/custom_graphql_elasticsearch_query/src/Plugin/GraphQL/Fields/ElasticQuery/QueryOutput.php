ee77feee7427744f8056308d8afac1efe86d1c84
root <lars.soonvald@gmail.com>
2018-07-30 13:09:57 +0000

    Created dynamic fields for elasticsearch indexes

--- web/modules/custom/custom_graphql_elasticsearch_query/src/Plugin/GraphQL/Fields/ElasticQuery/QueryOutput.php
+++ web/modules/custom/custom_graphql_elasticsearch_query/src/Plugin/GraphQL/Fields/ElasticQuery/QueryOutput.php
@@ -4,8 +4,8 @@ namespace Drupal\custom_graphql_elasticsearch_query\Plugin\GraphQL\Fields\Elasti
 
 use Drupal\graphql\GraphQL\Execution\ResolveContext;
 use Drupal\graphql\Plugin\GraphQL\Fields\FieldPluginBase;
-use Drupal\system\MenuInterface;
 use GraphQL\Type\Definition\ResolveInfo;
+use Drupal\graphql\Utility\StringHelper;
 
 /**
  * Retrieve a Drupal menu's description.
@@ -13,9 +13,8 @@ use GraphQL\Type\Definition\ResolveInfo;
  * @GraphQLField(
  *   id = "custom_elasticquery_output",
  *   secure = true,
- *   name = "JSONValue",
- *   type = "[String]",
- *   parents = {"CustomElastic"}
+ *   parents = {"CustomElastic"},
+ *	 deriver = "Drupal\custom_graphql_elasticsearch_query\Plugin\GraphQL\Derivers\ElasticFieldDeriver"
  * )
  */
 class QueryOutput extends FieldPluginBase {
@@ -25,8 +24,21 @@ class QueryOutput extends FieldPluginBase {
 	 */
 	public function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
 
-		yield $value;
-		//yield $value['_source']['lat'][0];
-	}
+		//$derivative['type'] = StringHelper::camelCase('field', $entityTypeId, $fieldName);
 
+		$derivative_id = $this->getDerivativeId();
+    // Not all documents have values for all fields so we need to check.
+    if (isset($value[$derivative_id])) {
+      // Checking if the value of this derivative is a list or single value so
+      // we can parse accordingly.
+      if (is_array($value[$derivative_id])) {
+        foreach ($value[$derivative_id] as $value_item) {
+          yield $value_item;
+        }
+      }
+      else {
+        yield $value[$derivative_id];
+      }
+    }
+	}
 }
