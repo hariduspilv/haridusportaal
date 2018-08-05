3b90b768faaf1c6366f6f3f91d76748340ae52b5
Mark Shabalin <mark.shabalin.work@gmail.com>
2018-08-05 22:06:54 +0300

    Map wrapper zoom update + coding style.

--- angular2/src/app/_components/map.wrapper/map.wrapper.component.ts
+++ angular2/src/app/_components/map.wrapper/map.wrapper.component.ts
@@ -14,15 +14,15 @@ export class MapWrapperComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    if (typeof this.latitude == 'string') {
+    if (typeof this.latitude === 'string') {
       this.latitude = parseFloat(this.latitude);
     }
-    if (typeof this.longitude == 'string') {
+    if (typeof this.longitude === 'string') {
       this.longitude = parseFloat(this.longitude);
     }
-    if (typeof this.zoom == 'string') {
-      if (this.zoom == "null") {
-        this.zoom = 12;
+    if (typeof this.zoom === 'string') {
+      if (this.zoom === 'null') {
+        this.zoom = 11;
       } else {
         this.zoom = parseInt(this.zoom);
       }
