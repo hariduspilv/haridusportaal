9e90299ead0a9bce7ff9dcf7ebbd1589f8c5c568
Madis Põld <madis.pold@twn.ee>
2018-08-08 17:51:28 +0300

    Studyprogramme fix #357

--- angular2/src/app/_views/studyProgramme.single/studyProgramme.single.component.ts
+++ angular2/src/app/_views/studyProgramme.single/studyProgramme.single.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SingleQuery } from '../../_services/studyProgramme/studyProgramme.service';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   templateUrl: "studyProgramme.single.template.html",
@@ -28,8 +27,7 @@ export class StudyProgrammeSingleComponent implements OnInit{
   constructor(
     private apollo: Apollo,
     private router: Router,
-    private route: ActivatedRoute,
-    private translate: TranslateService
+    private route: ActivatedRoute
   ){
 
   }
