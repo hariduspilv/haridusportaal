67547e8d7558b48f6e5bbe7ca7c88bbf8881a140
Madis Põld <madis.pold@twn.ee>
2018-08-08 13:48:34 +0300

    404 page #371

--- angular2/src/app/_views/index.ts
+++ angular2/src/app/_views/index.ts
@@ -9,3 +9,4 @@ export * from './schools/schools.component';
 export * from './schools.single/schools.single.component';
 export * from './studyProgramme/studyProgramme.component';
 export * from './studyProgramme.single/studyProgramme.single.component';
+export * from './notfound/notfound.component';
