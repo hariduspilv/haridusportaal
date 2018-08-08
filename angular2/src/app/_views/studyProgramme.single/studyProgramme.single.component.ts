ee591a1efeb580ecd0e820c6aa32c36dcdd97df3
Madis Põld <madis.pold@twn.ee>
2018-08-08 17:50:20 +0300

    Studyprogramme list mobile styles

--- angular2/src/app/_views/studyProgramme.single/studyProgramme.single.component.ts
+++ angular2/src/app/_views/studyProgramme.single/studyProgramme.single.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SingleQuery } from '../../_services/studyProgramme/studyProgramme.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   templateUrl: "studyProgramme.single.template.html",
@@ -27,7 +28,8 @@ export class StudyProgrammeSingleComponent implements OnInit{
   constructor(
     private apollo: Apollo,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private translate: TranslateService
   ){
 
   }
