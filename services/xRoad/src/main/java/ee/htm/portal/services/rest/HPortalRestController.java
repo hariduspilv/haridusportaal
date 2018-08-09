323ff4b30377b3ccf0f57d2a1a0cffbdb2aef737
Indrek Vallsalu <indrek.vallsalu@tieto.com>
2018-08-09 16:32:16 +0300

    [IV][#168] xroad service vpTaotlusDokument impl

--- services/xRoad/src/main/java/ee/htm/portal/services/rest/HPortalRestController.java
+++ services/xRoad/src/main/java/ee/htm/portal/services/rest/HPortalRestController.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,17 @@ public class HPortalRestController {
     }
 
     LOGGER.error("Tundmatu request JSON - " + requestJson);
+    return new ResponseEntity<>("{\"ERROR\":\"Tehniline viga!\"}", HttpStatus.NOT_FOUND);
+  }
+
+  @RequestMapping(value = "/getDocumentFile/{documentId}/{personalCode}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+  public ResponseEntity<?> getDockumentFile(@PathVariable("documentId") String documentId,
+      @PathVariable("personalCode") String personalCode) {
+    if (documentId.startsWith("VPT_")) {
+        return new ResponseEntity<>(vptWorker.getDocument(documentId, personalCode), HttpStatus.OK);
+    }
+
+    LOGGER.error("Tundmatu request documentId - " + documentId);
     return new ResponseEntity<>("{\"ERROR\":\"Tehniline viga!\"}", HttpStatus.NOT_FOUND);
   }
 }
