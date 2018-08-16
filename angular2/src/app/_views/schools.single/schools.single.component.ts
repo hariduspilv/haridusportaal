5c0531b970c1ae8d54acc41fffa13ca2bc794286
Mark Shabalin <mark-felix.sabalin@twn.ee>
2018-08-16 16:25:43 +0300

    School detail add table scroll style.

--- angular2/src/app/_views/schools.single/schools.single.component.ts
+++ angular2/src/app/_views/schools.single/schools.single.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewChecked } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import { Subscription } from 'rxjs/Subscription';
 
 import { SingleQuery, InstitutionTypeQuery } from '../../_services/school/school.service';
 import { RootScopeService } from '../../_services';
+import { TableService } from '../../_services/table/table.service';
 
 @Component({
   templateUrl: './schools.single.component.html',
   styleUrls: ['./schools.single.component.scss']
 })
-export class SchoolsSingleComponent implements OnInit, OnDestroy {
+export class SchoolsSingleComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   loading = true;
   data: any;
   path: String;
   types: any;
+  tableOverflown: boolean = false;
+  elemAtStart: boolean = true;
+  initialized: boolean = false;
 
   private querySubscription: Subscription;
 
@@ -23,7 +27,8 @@ export class SchoolsSingleComponent implements OnInit, OnDestroy {
     private apollo: Apollo,
     private route: ActivatedRoute,
     private router: Router,
-    private rootScope: RootScopeService
+    private rootScope: RootScopeService,
+    private tableService: TableService
   ) {}
 
   ngOnInit() {
@@ -74,4 +79,17 @@ export class SchoolsSingleComponent implements OnInit, OnDestroy {
       subscription.unsubscribe();
     });
   }
+
+  ngAfterViewChecked() {
+    this.initialTableCheck('tableRef')
+  }
+
+  initialTableCheck(id) {
+    const element = document.getElementById(id);
+    if (element) {
+      this.tableOverflown = (element.scrollWidth - element.scrollLeft) > element.clientWidth;
+      this.initialized = true;
+    }
+  }
+
 }
