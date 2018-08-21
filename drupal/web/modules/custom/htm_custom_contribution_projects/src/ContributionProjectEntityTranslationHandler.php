e9719443a503bc592d2d0c3f09f55f981a097727
Ruut <henri.ruut@twn.ee>
2018-08-21 12:54:39 +0300

    HTM Contrib project entity

---
+++ drupal/web/modules/custom/htm_custom_contribution_projects/src/ContributionProjectEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\htm_custom_contribution_projects;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for contribution_project_entity.
+ */
+class ContributionProjectEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
