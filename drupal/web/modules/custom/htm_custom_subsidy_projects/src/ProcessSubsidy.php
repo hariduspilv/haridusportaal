048de228d426fc25e50907ca74c78f1485055063
Ruut <henri.ruut@twn.ee>
2018-08-28 14:46:25 +0300

    CSV import updates

--- drupal/web/modules/custom/htm_custom_subsidy_projects/src/ProcessSubsidy.php
+++ drupal/web/modules/custom/htm_custom_subsidy_projects/src/ProcessSubsidy.php
@@ -14,11 +14,18 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ProcessSubsidy {
 
 	public $get_error_messages = [
-		'EXPIRED_TRANSACTION'  => 'There was a timeout before the user could sign with Mobile ID.',
+		'ehis_id'  => 'There was a timeout before the user could sign with Mobile ID.',
 		'USER_CANCEL'          => 'User has cancelled the signing operation.',
 		'NOT_VALID'            => 'Signature is not valid.',
 		'MID_NOT_READY'        => 'Mobile ID is not yet available for this phone. Please try again later.',
 	];
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function create(ContainerInterface $container) {
+		return new static();
+	}
+
 
 	public static function ValidateFile($items, &$context){
 		$message = t('Validating file');
@@ -27,19 +34,28 @@ class ProcessSubsidy {
 		self::deleteAllEntities();
 
 		$results = [];
-
 		foreach ($items as $index => $item){
+			$ehis_id = self::loadEntity('node', 'field_ehis_id', $item['ehis id']);
+			$meede = self::loadEntity('taxonomy_term', 'name', $item['meede']);
+			$summa = (is_numeric(preg_replace('/\s+/', '', $item['summa'])) ? $item['summa'] : FALSE);
+			$tahtaeg = (self::checkDateFormat($item['tahtaeg'], 'd.m.Y') ? $item['tahtaeg'] : FALSE);
+
 			if(
-					!self::loadEntity('node', 'field_ehis_id', $item['ehis id'])
+					!$meede
 					||
-					!self::loadEntity('taxonomy_term', 'name', $item['meede'])
+					!$ehis_id
 					||
-					!is_numeric(preg_replace('/\s+/', '', $item['summa']))
+					!$summa
 					||
-					!self::checkDateFormat($item['tahtaeg'], 'd.m.Y')
-			){
-				#dump($index);
+					!$tahtaeg){
 				$context['results']['error'][] = t('Error on line: '. ($index + 2));
+			}else{
+				$results[] = [
+					'ehis_id' => $ehis_id,
+					'ivestment_measure' => $meede,
+					'investment_amount' => $summa,
+					'investment_deadline' => $tahtaeg,
+				];
 			}
 		}
 
@@ -49,27 +65,36 @@ class ProcessSubsidy {
 
 	public static function ProcessSubsidy($items, &$context){
 		// do nothing if error's
-		$entity = SubsidyProjectEntity::create([
-			'name' => 'testi projekt 3',
-			'ehis_id' => 1414,
-			/*'investment_measure' => 0,*/
-			'investment_amount' => 99,
-			'investment_deadline' => '2018-05-18'
-		]);
-		#$entity->save();
+		#dump($context);
+		#dump($items);
+		#die();
+		#dump(count($context['results']['values']));
+		//process only if no errors otherwise nothing
 		if(empty($context['results']['error'])){
-			foreach($items as $key => $value){
-				// create entity;
-				$results[] = $key . ' - processed';
+			if(empty($context['sandbox'])){
+				$context['sandbox']['progress'] = 0;
+				$context['sandbox']['current_id'] = 0;
+				#$context['sandbox']['max'] = count($context['results']['values']);
+				$context['sandbox']['max'] = 200;
 			}
-			$context['results']['processed'] = $results;
+			$limit = 10;
+			for($i = $context['sandbox']['current_id']; $i <= $limit; $i++){
+				// do something
+				$context['sandbox']['progress']++;
+				$context['message'] = $i;
+			}
+
+			if($context['sandbox']['progress'] != $context['sandbox']['max']){
+				$context['finished'] = $context['sandbox']['progress'] / $context['sandbox']['max'];
+			}
 		}
 	}
 
 	public static function ProcessSubsidyFinishedCallback($success, $results, $operations){
 		// The 'success' parameter means no fatal PHP errors were detected. All
 		// other error management should be handled using 'results'.
-
+		#dump($results['values']);
+		#die();
 		if ($success) {
 			if(isset($results['error'])){
 				$message = [implode(', ', $results['error']), 'error'];
@@ -98,7 +123,9 @@ class ProcessSubsidy {
 		}else{
 			$properties = [$field => $id];
 		}
-		return ($storage->loadByProperties($properties)) ? TRUE : FALSE;
+		$entity = reset($storage->loadByProperties($properties));
+		#dump($entity);
+		return ($entity) ? $entity->id() : FALSE;
 	}
 
 	private function deleteAllEntities(){
