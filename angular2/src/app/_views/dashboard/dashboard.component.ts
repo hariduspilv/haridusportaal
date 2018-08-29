467fd1ac481a8952f479da2a95aae78d9ed5daae
Raimo <>
2018-08-29 10:14:15 +0300

    #383 Created certificates component. Created first draft of the dashboard view.

--- angular2/src/app/_views/dashboard/dashboard.component.ts
+++ angular2/src/app/_views/dashboard/dashboard.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '@app/_services/userService';
 
 @Component({
   templateUrl: "dashboard.component.html",
   styleUrls: ["dashboard.component.scss"]
 })
 
-export class DashboardComponent{
+export class DashboardComponent implements OnInit{
+  public mainMenu = [ {link: '/applications', label: 'Taotlused'},
+                      {link: '/certificates', label: 'Tunnistused'},
+                      {link: '/student', label: 'Õpingud'},
+                      {link: '/teacher', label: 'Õpetan'}  ];
+  public userData;
 
   constructor(
-    /* private muutuja:Service */
+    private user: UserService
   ){
 
   }
-  
+  ngOnInit(){
+    this.userData = this.user.getData();
+    console.log(this.userData);
+  }
 }
