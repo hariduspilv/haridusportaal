3362e7139a80a86920e9b0acc53374c37f292672
Raimo <>
2018-09-04 09:36:41 +0300

    #383 removed dummy data generating from profession-certificate list

--- angular2/src/app/_components/certificates/certificates.component.ts
+++ angular2/src/app/_components/certificates/certificates.component.ts
@@ -48,21 +48,7 @@ export class CertificatesComponent implements OnInit{
       } else {
         this.professionalCertificates = response['value']['kutsetunnistused'];
         
-        if(this.professionalCertificates.length){
-          let temp = [];
-          //DUMMY DATA from dummy data :)
-          for(let x = 0; x < this.professionalCertificates.length; x++){
-            for(let i = 2; i < 10; i++){
-              let cert = JSON.parse(JSON.stringify(this.professionalCertificates[x]));
-              cert.nimi += 'ddata' + i;
-              cert.valjaantud = cert.valjaantud.substring(0, 3) + "0" + i + cert.valjaantud.substring(5, cert.valjaantud.length);
-              
-              cert.path = this.router.url + '/' + cert.registrinumber;
-              temp.push(cert);
-            }
-          }
-          this.professionalCertificates = temp;
-         
+        if(this.professionalCertificates.length){        
           let regex = /(\d{2}).(\d{2}).(\d{4})/;
           this.professionalCertificates = this.professionalCertificates.sort(function(a,b){
             return Number(new Date(a.valjaantud.replace( regex , "$2/$1/$3" ))) - Number(new Date(b.valjaantud.replace( regex, "$2/$1/$3")));
