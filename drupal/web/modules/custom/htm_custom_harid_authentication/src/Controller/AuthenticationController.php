151e48506ce9e1e08ddce4019528cec3bd4bee58
root <lars.soonvald@gmail.com>
2018-09-05 12:47:06 +0000

    testing harid

--- drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
+++ drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
@@ -14,7 +14,7 @@ class AuthenticationController extends ControllerBase {
     $oidc = new OpenIDConnectClient('https://test.harid.ee', '0855cd5d8e5418a5e8c3dd3187dd0a6f', 'f75da21ad0d015fb71dba9895204429e57c7c9fa375779c00ae055cefcf9feac');
     $oidc->setVerifyHost(false);
     $oidc->setVerifyPeer(false);
-    $oidc->setResponseTypes(array('code','token','id_token','code token', 'code id_token', 'id_token token', 'code id_token token'));
+    $oidc->setResponseTypes(array('id_token'));
     $oidc->authenticate();
     $name = $oidc->requestUserInfo('name');
     dump('jou');
