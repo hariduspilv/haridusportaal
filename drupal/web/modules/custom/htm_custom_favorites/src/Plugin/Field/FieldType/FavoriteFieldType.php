43bbd43b81cc4ea35f13918ff3f26442201bdc49
Ruut <henri.ruut@twn.ee>
2018-09-07 19:29:46 +0300

    New favorites
    (cherry picked from commit d95dc9e3c5d1d5644d9caf29c647e8d67b9439f7)

---
+++ drupal/web/modules/custom/htm_custom_favorites/src/Plugin/Field/FieldType/FavoriteFieldType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\htm_custom_favorites\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'favorite_field_type' field type.
+ *
+ * @FieldType(
+ *   id = "favorite_field_type",
+ *   label = @Translation("Favorite field type"),
+ *   description = @Translation("Favorite field"),
+ *   default_widget = "favorite_widget_type",
+ *   default_formatter = "favorite_formatter_type",
+ * 	 list_class = "\Drupal\Core\Field\EntityReferenceFieldItemList"
+ * )
+ */
+class FavoriteFieldType extends EntityReferenceItem {
+
+	public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+		$properties = parent::propertyDefinitions($field_definition);
+		$properties['title'] = DataDefinition::create('string')
+			->setLabel(new TranslatableMarkup('Title'))
+			->setRequired(TRUE);
+		return $properties;
+	}
+
+	public static function schema(FieldStorageDefinitionInterface $field_definition) {
+		$schema = parent::schema($field_definition);
+		$schema['columns']['title'] = [
+			'type' => 'varchar',
+			'length' => 225,
+			'not null' => TRUE,
+			'default' => '',
+		];
+
+
+		return $schema;
+	}
+}
