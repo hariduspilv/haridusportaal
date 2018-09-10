506bc59781d8586002a1a8faa656d8b80c8b2c40
root <lars.soonvald@gmail.com>
2018-09-10 13:09:37 +0000

    testing harid

--- drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
+++ drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
@@ -17,7 +17,13 @@ class AuthenticationController extends ControllerBase {
     $oidc->setAllowImplicitFlow(true);
     $oidc->addAuthParam(array('response_mode' => 'form_post'));
     $oidc->authenticate();
-    $sub = $oidc->getVerifiedClaims('personal_code');
+    $sub[] = $oidc->getVerifiedClaims('sub');
+    $sub[] = $oidc->getVerifiedClaims('iss');
+    $sub[] = $oidc->getVerifiedClaims('name');
+    $sub[] = $oidc->getVerifiedClaims('email');
+    $sub[] = $oidc->getVerifiedClaims('phone_number');
+    $sub[] = $oidc->getVerifiedClaims('personal_code');
+    $sub[] = $oidc->getVerifiedClaims('roles');
     kint($sub);
     die();
     #$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
