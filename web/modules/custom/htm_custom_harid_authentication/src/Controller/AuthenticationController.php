956315ebcb357e2327309b108ac2d6a6abcbf0f5
root <lars.soonvald@gmail.com>
2018-09-10 13:12:06 +0000

    testing harid

--- web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
+++ web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
@@ -17,14 +17,7 @@ class AuthenticationController extends ControllerBase {
     $oidc->setAllowImplicitFlow(true);
     $oidc->addAuthParam(array('response_mode' => 'form_post'));
     $oidc->authenticate();
-    $sub[] = $oidc->getVerifiedClaims('sub');
-    $sub[] = $oidc->getVerifiedClaims('iss');
-    $sub[] = $oidc->getVerifiedClaims('name');
-    $sub[] = $oidc->getVerifiedClaims('email');
-    $sub[] = $oidc->getVerifiedClaims('phone_number');
-    $sub[] = $oidc->getVerifiedClaims('personal_code');
-    $sub[] = $oidc->getVerifiedClaims('roles');
-    kint($sub);
+    kint($oidc);
     die();
     #$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     #kint($actual_link);
