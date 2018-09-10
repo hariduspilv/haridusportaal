1f80278f1f9eb59ad0f8899803c927e1219335b5
root <lars.soonvald@gmail.com>
2018-09-10 13:31:45 +0000

    testing harid

--- web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
+++ web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
@@ -11,9 +11,16 @@ use Jumbojett\OpenIDConnectClient;
 class AuthenticationController extends ControllerBase {
 
   public function startAuthentication() {
+    $sub = [];
     $oidc = new OpenIDConnectClient('https://test.harid.ee', '0855cd5d8e5418a5e8c3dd3187dd0a6f', 'f75da21ad0d015fb71dba9895204429e57c7c9fa375779c00ae055cefcf9feac');
+    $oidc->setResponseTypes(array('id_token'));
+    $oidc->addScope(array('openid'));
+    $oidc->setAllowImplicitFlow(true);
+    $oidc->addAuthParam(array('response_mode' => 'form_post'));
     $oidc->authenticate();
-    kint($oidc);
+    $sub[] = $oidc->getVerifiedClaims('name');
+    $sub[] = $oidc->getVerifiedClaims('sub');
+    kint($sub);
     die();
     #$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     #kint($actual_link);
