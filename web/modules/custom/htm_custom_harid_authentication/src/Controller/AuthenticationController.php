3fc85a9f877032286e0e78177ca7f511f1bff60d
root <lars.soonvald@gmail.com>
2018-09-10 15:24:44 +0000

    testing harid

--- web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
+++ web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
@@ -15,9 +15,11 @@ class AuthenticationController extends ControllerBase {
     $oidc = new OpenIDConnectClient('https://test.harid.ee', '0855cd5d8e5418a5e8c3dd3187dd0a6f', 'f75da21ad0d015fb71dba9895204429e57c7c9fa375779c00ae055cefcf9feac');
     $oidc->providerConfigParam(array('token_endpoint' => 'https://test.harid.ee/et/access_tokens'));
     $oidc->addScope('personal_code');
+    $oidc->addScope('openid');
     $oidc->authenticate();
     kint($oidc);
     kint($oidc->requestUserInfo('personal_code'));
+    kint($clientCredentialsToken);
     die();
     #$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     #kint($actual_link);
