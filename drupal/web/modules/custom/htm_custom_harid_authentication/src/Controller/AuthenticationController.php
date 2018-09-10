765e9f0ecd4e02913bcc074461ae1cae473a223b
root <lars.soonvald@gmail.com>
2018-09-10 13:17:55 +0000

    testing harid

--- drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
+++ drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
@@ -12,9 +12,6 @@ class AuthenticationController extends ControllerBase {
 
   public function startAuthentication() {
     $oidc = new OpenIDConnectClient('https://test.harid.ee', '0855cd5d8e5418a5e8c3dd3187dd0a6f', 'f75da21ad0d015fb71dba9895204429e57c7c9fa375779c00ae055cefcf9feac');
-    $oidc->setResponseTypes(array('code'));
-    $oidc->addScope(array('personal_code'));
-    $oidc->setAllowImplicitFlow(true);
     $oidc->authenticate();
     kint($oidc);
     die();
