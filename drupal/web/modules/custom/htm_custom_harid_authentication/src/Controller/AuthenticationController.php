3529cff92a0074fbf8112d119be05bd540237e3c
root <lars.soonvald@gmail.com>
2018-09-10 15:41:51 +0000

    testing harid

--- drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
+++ drupal/web/modules/custom/htm_custom_harid_authentication/src/Controller/AuthenticationController.php
@@ -11,7 +11,6 @@ use Jumbojett\OpenIDConnectClient;
 class AuthenticationController extends ControllerBase {
 
   public function startAuthentication() {
-    $sub = [];
     $oidc = new OpenIDConnectClient('https://test.harid.ee', '0855cd5d8e5418a5e8c3dd3187dd0a6f', 'f75da21ad0d015fb71dba9895204429e57c7c9fa375779c00ae055cefcf9feac');
     $oidc->providerConfigParam(array('token_endpoint' => 'https://test.harid.ee/et/access_tokens'));
     $oidc->addScope('personal_code');
