d4ad252197594aa659b54b6664d547813c581fce
Madis Põld <madis.pold@twn.ee>
2018-09-20 17:22:27 +0300

    #433 Dashboard general styles and studies section.

--- angular2/src/app/_components/teachings/teachings.component.ts
+++ angular2/src/app/_components/teachings/teachings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, OnDestroy } from '@angular/core';
+import { HttpService } from '@app/_services/httpService';
 
 @Component({
   selector: 'teachings',
@@ -8,9 +9,24 @@ import { Component, OnInit, Input, Output, OnDestroy } from '@angular/core';
 
 export class TeachingsComponent{
   
+  content: any = false;
+  loading: boolean = false;
+  error: boolean = false;
+  errorMessage: any = false;
    
-  constructor() {
+  constructor(private http: HttpService) {}
 
+  ngOnInit() {
+    this.loading = true;
+    let sub = this.http.get('/dashboard/eeIsikukaart/teachings?_format=json').subscribe(response => {
+      if(response['error']){
+        this.error = true;
+      } else {
+        this.content = response['oping']['value'];
+      }
+      sub.unsubscribe();
+      this.loading = false;
+    });
   }
-  
+    
 }
