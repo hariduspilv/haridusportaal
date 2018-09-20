9224b044dcfb996175e3cfe9a9b1ef35c70382cb
Ruut <henri.ruut@twn.ee>
2018-09-20 15:39:52 +0300

    Dashboard get xJson document form service added

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -85,7 +85,6 @@ class EhisConnectorService {
 			/*TODO make post URL configurable*/
 			$response = $client->get(self::LOIME_DEFAULT_URL.$service_name . '/' . implode($params['url'], '/'));
 			$response = json_decode($response->getBody()->getContents(), TRUE);
-			$response['redis_hit'] = FALSE;
 			return $response;
 		}catch (RequestException $e){
 			throw $e;
@@ -103,7 +102,7 @@ class EhisConnectorService {
 	}
 	public function testApplications(){
 		$json = '{"documents":[{"form_name":"VPT_ESITATUD_TAOTLUS_OTSUS","identifier":38328,"document_date":"2015-01-21","status":"Heaks kiidetud"},{"form_name":"VPT_ESITATUD_TAOTLUS_OTSUS","identifier":3424,"document_date":"2013-09-03","status":"Tagasi lükatud"}]}';
-		$this->client->hset('47108249296', 'vpTaotlus', $json);
+		$this->client->hset('get', 'vpTaotlus', $json);
 	}
 
 	/**
@@ -175,6 +174,10 @@ class EhisConnectorService {
 		}
 		
 		return $this->invokeWithRedis('vpTaotlus', $params);
+	}
+
+	public function getDocument(array $params = []){
+		return $this->invoke('getDocument', $params);
 	}
 
 	/**
