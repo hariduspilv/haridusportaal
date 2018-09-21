c4c997c00b2046291a2825c4db83cd8463f7d202
Raimo <>
2018-09-21 12:01:53 +0300

    xjson helpertext

--- angular2/src/app/_views/xjson/xjson.component.ts
+++ angular2/src/app/_views/xjson/xjson.component.ts
@@ -84,7 +84,13 @@ export class XjsonComponent implements OnInit, OnDestroy{
   selectListCompare(a, b) {
     return a && b ? a == b : a == b;
   }
- 
+  isFieldDisabled(readonly:boolean): boolean{
+    if(readonly == true || this.max_step != this.opened_step) {
+      return true;
+    } else {
+      return false
+    }
+  }
   fileChange(event, model) {
     let fileList: FileList = event.target.files;
     if(fileList.length > 0) {
