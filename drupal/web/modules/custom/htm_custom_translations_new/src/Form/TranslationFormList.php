67e45ee0e374bf872b4d56a5d9f7dc00e331fa85
Ruut <henri.ruut@twn.ee>
2018-09-25 10:40:31 +0300

    translation new module

---
+++ drupal/web/modules/custom/htm_custom_translations_new/src/Form/TranslationFormList.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Drupal\htm_custom_translations_new\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\htm_custom_translations_new\formatKey;
+
+/**
+ * Class TranslationFormList.
+ */
+class TranslationFormList extends ConfigFormBase {
+	public function __construct(ConfigFactoryInterface $config_factory)
+	{
+		parent::__construct($config_factory);
+		$this->formatter = new formatKey();
+	}
+
+	/**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'htm_custom_translations_new.translation',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'translation_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('htm_custom_translations_new.translation');
+    /*dump($config->getRawData());*/
+		#$config->delete();
+    $header = [
+    	'key' => $this->t('Translation key'),
+			'translation_languages' => $this->t('Languages'),
+			'translation_value' => $this->t('Translation'),
+			'operations' => $this->t('Operations'),
+		];
+
+		$form['table'] = [
+			'#type' => 'table',
+			'#header' => $header,
+			'#empty' => t('No translations found. <a href="@add-url">Add translation.</a>', [
+					'@add-url' => Url::fromRoute('htm_custom_translations_new.add_translation')->toString(),
+			]),
+		];
+		$languages = \Drupal::languageManager()->getLanguages();
+    foreach($config->getRawData() as $key => $data){
+			$form['table'][$key]['key'] = [
+				'#plain_text' => $this->formatter->parseSlash($key),
+			];
+			#dump($data);
+			foreach($languages as $lang_key => $value){
+				$form['table'][$key]['translation_languages'][] = [
+					'#type' => 'item',
+					'#title' => $lang_key,
+				];
+				$translation_value = (is_array($data[$lang_key]))? $data[$lang_key]['value'] : $data[$lang_key];
+				$form['table'][$key]['translation_value'][] = [
+					'#type' => 'item',
+					'#title' => ($translation_value) ? $translation_value : 'MISSING',
+				];
+			}
+
+			$form['table'][$key]['operations'] = [
+				'#type' => 'operations',
+				'#links' => []
+			];
+			$form['table'][$key]['operations']['#links']['edit'] = [
+				'title' => $this->t('Edit'),
+				'url' => Url::fromRoute('htm_custom_translations_new.edit_translation', ['type'=> 'edit', 'translation_key' => $key]),
+			];
+			$form['table'][$key]['operations']['#links']['delete'] = [
+				'title' => $this->t('Delete'),
+				'url' => Url::fromRoute('<none>'),
+			];
+
+		}
+
+
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('htm_custom_translations_new.translation')
+      ->set('text', $form_state->getValue('text'))
+      ->save();
+  }
+
+}
