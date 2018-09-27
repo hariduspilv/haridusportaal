4dcad0f8f70934584274fd00ccb98b2c66f8c81a
root <lars.soonvald@gmail.com>
2018-09-27 10:21:09 +0000

    tweaked oska csv import processing

--- drupal/web/modules/custom/htm_custom_oska/src/ProcessOskaData.php
+++ drupal/web/modules/custom/htm_custom_oska/src/ProcessOskaData.php
@@ -46,7 +46,7 @@ class ProcessOskaData {
             $object['valdkond'] = self::checkTaxonomyTerm('taxonomy_term', 'oska_field', $item['valdkond']);
             $object['alavaldkond'] = self::addTaxonomyTerm('taxonomy_term', 'oska_field', $item['alavaldkond'], $item['valdkond']);
             $object['pohikutseala'] = self::checkTaxonomyTerm('taxonomy_term', 'oska_main_profession', $item['pohikutseala']);
-            $object['aasta'] = strlen($item['aasta'])==4 ? $item['aasta'] : FALSE;
+            $object['aasta'] = strlen($item['aasta'])==4 && is_numeric($item['aasta']) ? $item['aasta'] : FALSE;
             $object['silt'] = is_string($item['silt']) ? $item['silt'] : FALSE;
             $object['vaartus'] = is_numeric($item['vaartus']) ? $item['vaartus'] : FALSE;
             if(
@@ -162,7 +162,7 @@ class ProcessOskaData {
             ]);
             $entity->save();
         }
-        return ($entity) ? $entity->id() : FALSE;
+        return ($entity) ? $entity->id() : TRUE;
     }
 
     public function checkTaxonomyTerm($entity_type, $vocabulary, $name){
@@ -176,7 +176,7 @@ class ProcessOskaData {
 
         $entity = reset($storage->loadByProperties($properties));
 
-        return ($entity) ? $entity->id() : FALSE;
+        return ($entity) ? $entity->id() : TRUE;
     }
 
     private function deleteAllEntities(){
