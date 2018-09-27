1edd5141ddb5ffb97ff67c9b6a9c84aafd915dc5
root <lars.soonvald@gmail.com>
2018-09-27 11:13:07 +0000

    added tara authorization

--- drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/AuthenticationController.php
+++ drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/AuthenticationController.php
@@ -3,6 +3,9 @@
 namespace Drupal\htm_custom_tara_authentication\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Jumbojett\OpenIDConnectClient;
+use Jumbojett\OpenIDConnectClientException;
+use Drupal\Core\Site\Settings;
 
 /**
  * Class AuthenticationController.
@@ -10,10 +13,11 @@ use Drupal\Core\Controller\ControllerBase;
 class AuthenticationController extends ControllerBase {
 
   public function startAuthentication() {
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Implement method: startAuthentication')
-    ];
+      $tara_secret = settings::get('tara_secret');
+
+      $oidc = new OpenIDConnectClient('https://tara.ria.ee', 'eduportaal', $tara_secret);
+      kint($oidc);
+      die();
   }
 
 }
