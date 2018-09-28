8516c374146e0d264bdf61e13b6fb384f6be25ce
srautam <sander.rautam@twn.ee>
2018-09-28 14:07:07 +0300

    steps spacing

--- angular2/src/app/_views/xjson/xjson.component.ts
+++ angular2/src/app/_views/xjson/xjson.component.ts
@@ -18,7 +18,7 @@ const XJSON_DATEPICKER_FORMAT = {
     dateInput: 'YYYY-MM-DD',
   },
   display: {
-    dateInput: 'DD.MM.YYYY',
+    dateInput: 'DD-MM-YYYY',
     monthYearLabel: 'MMM YYYY',
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'MMMM YYYY',
