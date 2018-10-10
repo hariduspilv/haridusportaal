104654bfa1dc0d4a1cc1cd7c36f8e16c255e2cf8
root <lars.soonvald@gmail.com>
2018-10-10 09:14:02 +0000

    working custom oska field with oska indicator import for oska main field page

---
+++ drupal/web/modules/custom/htm_custom_oska/src/OskaIndicatorEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\htm_custom_oska;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for oska_indicator_entity.
+ */
+class OskaIndicatorEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
