ca097c3f022343b70f0f5660528124bc387876a6
Henri Kokk <henri.kokk@twn.ee>
2018-10-10 15:28:13 +0300

    OSKA põhikutseala detailvaate kuvamine #t453

--- angular2/src/app/app.routing.module.ts
+++ angular2/src/app/app.routing.module.ts
@@ -83,7 +83,8 @@ const appRoutes: Routes = [
 
   { path: ':lang/valdkonnad/:id', component:OskaAreasComponent},
   { path: ':lang/professions/:id', component:OskaAreasComponent},
-  
+  { path: ':lang/pohikutsealad/:id', component: OskaAreasComponent },
+  { path: ':lang/sectors/:id', component:OskaAreasComponent},
   
   { path: ':lang/otsing', component: SearchComponent },
   { path: ':lang/search', component: SearchComponent },
