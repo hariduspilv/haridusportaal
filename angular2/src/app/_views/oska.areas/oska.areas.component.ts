fce46cbabae5ae2dc42289e18837ec63533b2bcb
Henri Kokk <henri.kokk@twn.ee>
2018-10-11 14:43:03 +0300

    Oska updates

--- angular2/src/app/_views/oska.areas/oska.areas.component.ts
+++ angular2/src/app/_views/oska.areas/oska.areas.component.ts
@@ -13,7 +13,7 @@ export class OskaAreasComponent implements OnInit{
   data: any = false;
   video: any = false;
   error: boolean = false;
-
+  compareButton: boolean = false;
   constructor(
     private http: HttpService,
     private route: ActivatedRoute,
@@ -36,10 +36,10 @@ export class OskaAreasComponent implements OnInit{
   getData(){
     let url = "/graphql?queryId=oskaFieldDetailView:1&variables=";
 
-    console.log(this.router.url);
+
     if( this.router.url.match(/pohikutsealad|sectors/ ) ){
       url = "/graphql?queryId=oskaMainProfessionDetailView:1&variables=";
-      console.log("match");
+      this.compareButton = true;
     }
 
     let variables = {
@@ -60,7 +60,7 @@ export class OskaAreasComponent implements OnInit{
       this.setLangLinks(data);
 
       subscription.unsubscribe();
-      
+
     });
   }
 
