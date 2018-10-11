6d38f05c27468eacce1046e1661c59f9e24f97af
Madis Põld <madis.pold@twn.ee>
2018-10-11 19:39:22 +0300

    Oska sidebar #497

--- angular2/src/app/_components/index.ts
+++ angular2/src/app/_components/index.ts
@@ -2,6 +2,7 @@ import { HeaderModule } from './header/header.module';
 import { SideMenuModule } from './sidemenu/sidemenu.module';
 import { RelatedEventsModule } from './related.events/related.events.module';
 import { RecentNewsModule } from './recent.news/recent.news.module';
+import { OskaAreasSidebarModule } from './oska.areas.sidebar/oska.areas.sidebar.module';
 import { NewsletterOrderModule } from './newsletter.order/newsletter.order.module';
 
 
@@ -10,5 +11,6 @@ export const AppModules = [
   SideMenuModule,
   RelatedEventsModule,
   RecentNewsModule,
-  NewsletterOrderModule
+  NewsletterOrderModule,
+  OskaAreasSidebarModule
 ];
