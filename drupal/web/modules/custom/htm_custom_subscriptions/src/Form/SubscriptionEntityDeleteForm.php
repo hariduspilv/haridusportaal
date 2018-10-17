8112fa79bc711ea6b33316b97457d6f167c3b2c0
Ruut <henri.ruut@twn.ee>
2018-10-17 15:16:36 +0300

    Subscription module

---
+++ drupal/web/modules/custom/htm_custom_subscriptions/src/Form/SubscriptionEntityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\htm_custom_subscriptions\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Subscription entity entities.
+ *
+ * @ingroup htm_custom_subscriptions
+ */
+class SubscriptionEntityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
