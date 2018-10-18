364da28ca55dc2e90b76644bbbe82e510ddfdd57
root <lars.soonvald@gmail.com>
2018-10-18 11:43:06 +0000

    added oska table csv import

---
+++ web/modules/custom/htm_custom_oska/src/OskaTableEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\htm_custom_oska;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for oska_table_entity.
+ */
+class OskaTableEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
