3c675a7f07f72d7d6f828bb0de8d0fab8d17b6f6
root <lars.soonvald@gmail.com>
2018-10-18 11:43:06 +0000

    added oska table csv import

---
+++ web/modules/custom/htm_custom_oska/src/Entity/OskaTableEntityViewsData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\htm_custom_oska\Entity;
+
+use Drupal\views\EntityViewsData;
+
+/**
+ * Provides Views data for Oska table entity entities.
+ */
+class OskaTableEntityViewsData extends EntityViewsData {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewsData() {
+    $data = parent::getViewsData();
+
+    // Additional information for Views integration, such as table joins, can be
+    // put here.
+
+    return $data;
+  }
+
+}
