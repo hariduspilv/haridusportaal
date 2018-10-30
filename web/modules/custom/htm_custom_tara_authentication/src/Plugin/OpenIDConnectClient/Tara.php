aef93a615d4b885c7674acd9ae1a8702039b745c
Ruut <henri.ruut@twn.ee>
2018-10-30 16:55:18 +0200

    TARA

--- web/modules/custom/htm_custom_tara_authentication/src/Plugin/OpenIDConnectClient/Tara.php
+++ web/modules/custom/htm_custom_tara_authentication/src/Plugin/OpenIDConnectClient/Tara.php
@@ -22,29 +22,7 @@ use Exception;
  * )
  */
 
-/**
- * A wrapper around base64_decode which decodes Base64URL-encoded data,
- * which is not the same alphabet as base64.
- */
-function base64url_decode($base64url) {
-	return base64_decode(b64url2b64($base64url));
-}
 
-/**
- * Per RFC4648, "base64 encoding with URL-safe and filename-safe
- * alphabet".  This just replaces characters 62 and 63.  None of the
- * reference implementations seem to restore the padding if necessary,
- * but we'll do it anyway.
- *
- */
-function b64url2b64($base64url) {
-	// "Shouldn't" be necessary, but why not
-	$padding = strlen($base64url) % 4;
-	if ($padding > 0) {
-		$base64url .= str_repeat("=", 4 - $padding);
-	}
-	return strtr($base64url, '-_', '+/');
-}
 
 class Tara extends OpenIDConnectClientBase {
 
@@ -213,7 +191,31 @@ class Tara extends OpenIDConnectClientBase {
 	private function decodeJWT($jwt, $section = 0) {
 
 		$parts = explode(".", $jwt);
-		return json_decode(base64url_decode($parts[$section]), true);
+		return json_decode($this->base64url_decode($parts[$section]), true);
+	}
+
+	/**
+	 * A wrapper around base64_decode which decodes Base64URL-encoded data,
+	 * which is not the same alphabet as base64.
+	 */
+	function base64url_decode($base64url) {
+		return base64_decode($this->b64url2b64($base64url));
+	}
+
+	/**
+	 * Per RFC4648, "base64 encoding with URL-safe and filename-safe
+	 * alphabet".  This just replaces characters 62 and 63.  None of the
+	 * reference implementations seem to restore the padding if necessary,
+	 * but we'll do it anyway.
+	 *
+	 */
+	function b64url2b64($base64url) {
+		// "Shouldn't" be necessary, but why not
+		$padding = strlen($base64url) % 4;
+		if ($padding > 0) {
+			$base64url .= str_repeat("=", 4 - $padding);
+		}
+		return strtr($base64url, '-_', '+/');
 	}
 
 }
