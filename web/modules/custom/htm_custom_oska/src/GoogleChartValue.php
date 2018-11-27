2c5a7a88223159d5e1b91e1e14f7a7b0a5f37dbc
root <lars.soonvald@gmail.com>
2018-11-27 23:50:11 +0000

    moved graph data computing directly towards csv file

--- web/modules/custom/htm_custom_oska/src/GoogleChartValue.php
+++ web/modules/custom/htm_custom_oska/src/GoogleChartValue.php
@@ -13,12 +13,41 @@ use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\TypedData\DataDefinition;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use League\Csv\Reader;
+use League\Csv\Statement;
 
 /**
  * Class GoogleChartValue
  * @package Drupal\htm_custom_oska
  */
 class GoogleChartValue extends TypedData {
+
+    public function applyFilters($row){
+
+        foreach($this->filter_values as $key => $filters){
+            $match = false;
+
+            if(is_array($filters) && count($filters) > 0){
+                foreach($filters as $filter){
+                    if (strpos($row[$key], $filter) !== FALSE) {
+                        $match = true;
+                    }
+                }
+            }else if(count($filters) == 0){
+                $match = true;
+            }else{
+                if(strpos($row[$key], $filters) !== FALSE){
+                    $match = true;
+                }
+            }
+
+            if($match == false){
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     /**
      * {@inheritdoc}
@@ -32,8 +61,6 @@ class GoogleChartValue extends TypedData {
         $target_type = $item->getFieldDefinition()->getSettings()['target_type'];
         $filter_values = $graph_info['graph_options'];
 
-
-
         foreach($graph_info['graph_options'] as $key => $value){
             if($key != 'graph_title' && $key != 'graph_type' && $key != 'graph_v_axis' && $key != 'secondary_graph_type' && $key != 'graph_group_by'){
                 $filter_values[$key] = $value;
@@ -44,44 +71,25 @@ class GoogleChartValue extends TypedData {
         }
 
         if(isset($filter_values['secondary_graph_indicator'])){
-
-            $entity = \Drupal::entityTypeManager()->getStorage($target_type)->loadMultiple();
-            $entity = reset($entity);
-
-            foreach($entity->getFields() as $key => $field){
-                $field_settings = $field->getSettings();
-                if(isset($field_settings['graph_indicator'])){
-                    $indicator_field = $key;
-                }
-            }
-            $filter_values[$indicator_field][] = reset($filter_values['secondary_graph_indicator']);
+            $filter_values['graph_indicator'] = [$filter_values['graph_indicator'], $filter_values['secondary_graph_indicator']];
             unset($filter_values['secondary_graph_indicator']);
         }
 
-        $query = \Drupal::entityQuery($target_type);
+        $filter_values['naitaja'] = $filter_values['graph_indicator'];
+        unset($filter_values['graph_indicator']);
 
-        foreach($filter_values as $key => $filter){
-            if(isset($filter[0]['target_id'])){
-                $search_args = $this->cleanFilters($filter);
-                if($search_args != NULL){
-                    $query->condition($key.'.target_id', $search_args, 'IN');
-                    $condition_count++;
-                }
-            }else{
-                if(count($filter) > 0){
-                    $query->condition($key, $filter, 'IN');
-                    $condition_count++;
-                }
-            }
-        }
+        $this->filter_values = $filter_values;
 
-        if($condition_count > 0){
-            $entity_ids = $query->execute();
-        }
+        $reader = Reader::createFromPath('/app/drupal/web/sites/default/files/private/oska_csv/oska_csv.csv', 'r');
+        $reader->setDelimiter(';');
+        $reader->setHeaderOffset(0);
+        $stmt = (new Statement())
+            ->where([$this,'applyFilters']);
 
-        if(isset($entity_ids)){
-            $entities = \Drupal::entityTypeManager()->getStorage($target_type)->loadMultiple($entity_ids);
-            $graph_value = $this->getGoogleGraphValue($entities, $graph_info, $filter_values);
+        $records = iterator_to_array($stmt->process($reader), false);
+
+        if(isset($records) && count($records) > 0){
+            $graph_value = $this->getGoogleGraphValue($records, $graph_info, $filter_values);
 
             return $graph_value;
         }else{
@@ -100,14 +108,7 @@ class GoogleChartValue extends TypedData {
         }
     }
 
-    public function cleanFilters($filters){
-        foreach($filters as $filter){
-            $cleaned_filters[] = $filter['target_id'];
-        }
-        return $cleaned_filters;
-    }
-
-    public function getGoogleGraphValue($entities, $graph_info, $filter_values){
+    public function getGoogleGraphValue($records, $graph_info, $filter_values){
 
         $data_array = NULL;
         $graph_data = $graph_info['graph_options'];
@@ -118,97 +119,65 @@ class GoogleChartValue extends TypedData {
             }
         }
 
-        #get entity fields for finding label and value fields
-        if(count($entities) > 0){
-            $entity_fields = reset($entities)->getFields();
+        #find label and value fields
+        $label_field = $graph_data['graph_v_axis'];
+        $value_field = 'vaartus';
+        $indicator_field = 'naitaja';
 
-            #find label and value fields
+        if($graph_info['graph_set'] === 'multi'){
+            $indicator_field = $graph_data['graph_group_by'];
+        }
 
-            $label_field = $graph_data['graph_v_axis'];
-            foreach($entity_fields as $key => $field){
-                $field_settings = $field->getSettings();
-                if(isset($field_settings['graph_value'])){
-                    $value_field = $key;
+        if($label_field && $value_field){
+            $labelsums = [];
+            $xlabels = [];
+
+            #get value for each label, sum reoccurring labels
+            foreach($records as $record){
+
+                if(isset($record[$label_field]) && $record[$label_field] != ''){
+                    $xlabel = $record[$label_field];
+                }else{
+                    continue;
                 }
-                if(isset($field_settings['graph_indicator'])){
-                    $indicator_field = $key;
+
+                $ylabel = (string)$this->t($label_field);
+                $val = $record[$value_field];
+
+
+                if(isset($record[$indicator_field]) && $record[$indicator_field] != ''){
+                    $value_label = $record[$indicator_field];
+                }else{
+                    continue;
+                }
+
+                $labelsums[$ylabel][$xlabel] = $xlabel;
+                if(!isset($labelsums[$value_label][$xlabel])){
+                    $labelsums[$value_label][$xlabel] = floatval(str_replace(",",".", $val));
+                }else{
+                    $labelsums[$value_label][$xlabel] += floatval(str_replace(",",".", $val));
+                }
+                if(!in_array($xlabel, $xlabels)){
+                    $xlabels[] = $xlabel;
                 }
             }
-            if($graph_info['graph_set'] === 'multi'){
-                $indicator_field = $graph_data['graph_group_by'];
-            }
 
-            if($label_field && $value_field){
-                $labelsums = [];
-                $xlabels = [];
-
-                #get value for each label, sum reoccurring labels
-                foreach($entities as $entity){
-                    $entity_value = $entity->toArray();
-                    if($entity->$label_field->value != NULL){
-                        $xlabel = $entity->$label_field->value;
-                    }else{
-                        if(isset($entity_value[$label_field][0]['target_id']) && $entity_value[$label_field][0]['target_id'] != 0){
-                            $xlabel = Term::load($entity_value[$label_field][0]['target_id'])->getName();
-                        }else{
-                            continue;
-                        }
-                    }
-
-
-                    $ylabel = (string)$this->t($entity->$label_field->getFieldDefinition()->getLabel()->getUntranslatedString());
-                    $val = $entity->$value_field->value;
-
-                    if(isset($entity_value[$indicator_field][0]['target_id']) && $entity_value[$indicator_field][0]['target_id'] != 0){
-                        $value_label = Term::load($entity_value[$indicator_field][0]['target_id'])->getName();
-                    }elseif(isset($entity_value[$indicator_field][0]['value']) && $entity_value[$indicator_field][0]['value'] != ''){
-                        $value_label = $entity_value[$indicator_field][0]['value'];
-                    }else{
-                        continue;
-                    }
-
-                    $labelsums[$ylabel][$xlabel] = $xlabel;
-                    if(!isset($labelsums[$value_label][$xlabel])){
-                        $labelsums[$value_label][$xlabel] = floatval(str_replace(",",".", $val));
-                    }else{
-                        $labelsums[$value_label][$xlabel] += floatval(str_replace(",",".", $val));
-                    }
-                    if(!in_array($xlabel, $xlabels)){
-                        $xlabels[] = $xlabel;
+            #add values to empty fields
+            if(count($xlabels) > 0){
+                foreach($xlabels as $label){
+                    $labelsums = $this->fillEmptyFields($labelsums, $label);
+                }
+                #add labels for chart
+                foreach($labelsums as $label => $value){
+                    $data_array[0][] = (string)$label;
+                    foreach($value as $key => $val){
+                        $data_array[$key][] = $val;
                     }
                 }
-
-                #add x-axis value to order array as first
-                $key_order[] = key($labelsums);
-
-                #get correct order for indicators
-                foreach($filter_values[$indicator_field] as $value){
-                    $key_order[] = Term::load($value['target_id'])->getName();
-                }
-
-                #create new array with correct order and values from old array
-                foreach($key_order as $key){
-                    $new_labelsums[$key] = $labelsums[$key];
-                }
-
-                $labelsums = $new_labelsums;
-
-                #add values to empty fields
-                if(count($xlabels) > 0){
-                    foreach($xlabels as $label){
-                        $labelsums = $this->fillEmptyFields($labelsums, $label);
-                    }
-                    #add labels for chart
-                    foreach($labelsums as $label => $value){
-                        $data_array[0][] = (string)$label;
-                        foreach($value as $key => $val){
-                            $data_array[$key][] = $val;
-                        }
-                    }
-                    $data_array = array_values($data_array);
-                }
+                $data_array = array_values($data_array);
             }
         }
+
         return $data_array != NULL ? json_encode($data_array, TRUE) : NULL;
     }
 
