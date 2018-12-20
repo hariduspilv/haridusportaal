275e6053cd9e215cd243e8971e04618ba24f74ef
Henri Ruut <henri.ruut@twn.ee>
2018-12-20 16:36:16 +0200

    HarID updates

--- drupal/web/modules/custom/htm_custom_tara_authentication/src/Plugin/OpenIDConnectClient/HarID.php
+++ drupal/web/modules/custom/htm_custom_tara_authentication/src/Plugin/OpenIDConnectClient/HarID.php
@@ -51,6 +51,62 @@ class HarID extends Generic {
 		return $response;
 	}
 
+
+	/**
+	 * Implements OpenIDConnectClientInterface::retrieveIDToken().
+	 *
+	 * @param string $authorization_code
+	 *   A authorization code string.
+	 *
+	 * @return array|bool
+	 *   A result array or false.
+	 */
+	public function retrieveTokens($authorization_code) {
+		$endpoints = $this->getEndpoints();
+
+		$request_options = [
+			'form_params' => [
+				'code' => $authorization_code,
+				'client_id' => $this->configuration['client_id'],
+				'client_secret' => $this->configuration['client_secret'],
+				'redirect_uri' => 'https://htm.wiseman.ee/custom/login/harid/return',
+				'grant_type' => 'authorization_code',
+			],
+			'headers' => [
+				'Accept' => 'application/json',
+			],
+		];
+
+		/* @var \GuzzleHttp\ClientInterface $client */
+		$client = $this->httpClient;
+		try {
+			$response = $client->post($endpoints['token'], $request_options);
+			$response_data = json_decode((string) $response->getBody(), TRUE);
+
+			// Expected result.
+			$tokens = [
+				'id_token' => isset($response_data['id_token']) ? $response_data['id_token'] : NULL,
+				'access_token' => isset($response_data['access_token']) ? $response_data['access_token'] : NULL,
+			];
+			if (array_key_exists('expires_in', $response_data)) {
+				$tokens['expire'] = REQUEST_TIME + $response_data['expires_in'];
+			}
+			if (array_key_exists('refresh_token', $response_data)) {
+				$tokens['refresh_token'] = $response_data['refresh_token'];
+			}
+			return $tokens;
+		}
+		catch (Exception $e) {
+			$variables = [
+				'@message' => 'Could not retrieve tokens',
+				'@error_message' => $e->getMessage(),
+			];
+			$this->loggerFactory->get('openid_connect_' . $this->pluginId)
+				->error('@message. Details: @error_message', $variables);
+			return FALSE;
+		}
+	}
+
 	/**
 	 * Implements OpenIDConnectClientInterface::retrieveUserInfo().
 	 *
@@ -73,7 +129,8 @@ class HarID extends Generic {
 		try {
 			$response = $client->get($endpoints['userinfo'], $request_options);
 			$response_data = (string) $response->getBody();
-
+			dump($response_data);
+			die();
 			return json_decode($response_data, TRUE);
 		}
 		catch (Exception $e) {
