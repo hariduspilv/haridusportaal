385d6019341dcd8e3b3940d92092bc6df1516f35
root <lars.soonvald@gmail.com>
2019-01-08 12:37:19 +0000

    added result rounding

--- web/modules/contrib/graphql_metatag/src/Plugin/GraphQL/Fields/Metatag/Key.php
+++ web/modules/contrib/graphql_metatag/src/Plugin/GraphQL/Fields/Metatag/Key.php
@@ -8,6 +8,7 @@ use GraphQL\Type\Definition\ResolveInfo;
 
 /**
  * @GraphQLField(
+ *   secure = true,
  *   id = "metatag_key",
  *   name = "key",
  *   type = "String",
@@ -20,8 +21,7 @@ class Key extends FieldPluginBase {
    * {@inheritdoc}
    */
   protected function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
-    dump('dsa');
-	if (isset($value['#tag']) && $value['#tag'] === 'meta') {
+    if (isset($value['#tag']) && $value['#tag'] === 'meta') {
       yield isset($value['#attributes']['property']) ? $value['#attributes']['property'] : $value['#attributes']['name'];
     }
     else if (isset($value['#tag']) && $value['#tag'] === 'link') {
--- web/modules/contrib/module_missing_message_fixer/src/ModuleMissingMessageFixer.php
+++ web/modules/contrib/module_missing_message_fixer/src/ModuleMissingMessageFixer.php
@@ -1,13 +1,83 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\module_missing_message_fixer\ModuleMissingMessageFixer.
- */
-
 namespace Drupal\module_missing_message_fixer;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Messenger\MessengerInterface;
+
+/**
+ * Class ModuleMissingMessageFixer.
+ *
+ * @package Drupal\module_missing_message_fixer
+ */
 class ModuleMissingMessageFixer {
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * The Messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs a new UserSelection object.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The database connection.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   */
+  public function __construct(Connection $connection, MessengerInterface $messenger) {
+    $this->connection = $connection;
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+      $container->get('messenger')
+    );
+  }
+
+  /**
+   * Helper function to check for modules to fix.
+   *
+   * @param bool $return
+   *   If we are to return to rows or just print the list.
+   *
+   * @return string|null
+   *   The printed output.
+   */
+  public function checkModules($return = FALSE) {
+
+    if ($return) {
+      return $this->getTableRows();
+    }
+
+    $rows = [];
+
+    // Use a key for the head row that is not a valid module name.
+    $rows['*HEAD*'] = ModuleMissingMessageFixer::getTableHeader();
+    $rows += ModuleMissingMessageFixer::getTableRows();
+
+    // Print Table here instead of in the hook_command.
+    $output = count($rows) > 1 ?
+      drush_format_table($rows, TRUE) : 'No Missing Modules Found!!!';
+    drush_print($output);
+
+    return NULL;
+  }
 
   /**
    * Set the table headers for the ui and drush.
@@ -15,13 +85,12 @@ class ModuleMissingMessageFixer {
    * @return string[]
    *   Format: $[$column_key] = $cell
    */
-  public static function getTableHeader() {
-    return array(
+  public function getTableHeader() {
+    return [
       'name' => 'Name',
       'type' => 'Type',
-    );
+    ];
   }
-
 
   /**
    * Produces one table row for each missing module.
@@ -31,14 +100,12 @@ class ModuleMissingMessageFixer {
    * @return array[]
    *   Format: $[$extension_name][$column_key] = $cell
    */
-  public static function getTableRows() {
+  public function getTableRows() {
     // Initalize vars.
     $rows = [];
-    $i = 0;
-    $db = \Drupal::database();
 
     // Grab all the modules in the system table.
-    $results = $db->select('key_value', 'k')
+    $results = $this->connection->select('key_value', 'k')
       ->fields('k', ['name'])
       ->condition('collection', 'system.schema')
       ->execute()
@@ -57,19 +124,19 @@ class ModuleMissingMessageFixer {
 
       if ($filename === NULL) {
         // Report this module in the table.
-        $rows[$record->name] = array(
+        $rows[$record->name] = [
           'name' => $record->name,
           'type' => 'module',
-        );
+        ];
         continue;
       }
 
       $message = NULL;
-      $replacements = array(
+      $replacements = [
         '@name' => $record->name,
         '@type' => 'module',
         '@file' => $filename,
-      );
+      ];
       if (!file_exists($filename)) {
         // This case is unexpected, because drupal_get_filename() should take care
         // of it already.
@@ -96,13 +163,13 @@ class ModuleMissingMessageFixer {
 
       if ($message !== NULL) {
         // This case should never occur.
-        drupal_set_message(
+        $this->messenger->addWarning(
           t($message, $replacements),
-          'warning',
           FALSE);
       }
     }
 
     return $rows;
   }
+
 }
