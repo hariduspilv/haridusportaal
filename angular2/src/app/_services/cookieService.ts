fb22bf9afc5ee4352b0771fbd8555bd2cccd8029
Henri Kokk <henri.kokk@twn.ee>
2019-01-10 16:42:46 +0200

    Cookie notification update

--- angular2/src/app/_services/cookieService.ts
+++ angular2/src/app/_services/cookieService.ts
@@ -16,8 +16,8 @@ export class CookieService {
   checkDnT() {
     // Do not Track
     let enabled = false;
-    if (window.doNotTrack || navigator.doNotTrack || navigator['msDoNotTrack'] || 'msTrackingProtectionEnabled' in window.external) {
-      if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator['msDoNotTrack'] == "1" || ( window['external']['msTrackingProtectionEnabled'] && window['external']['msTrackingProtectionEnabled']() ) ) {
+    if (window['doNotTrack'] || navigator['doNotTrack'] || navigator['msDoNotTrack'] || 'msTrackingProtectionEnabled' in window.external) {
+      if (window['doNotTrack'] == "1" || navigator['doNotTrack'] == "yes" || navigator['doNotTrack'] == "1" || navigator['msDoNotTrack'] == "1" || ( window['external']['msTrackingProtectionEnabled'] && window['external']['msTrackingProtectionEnabled']() ) ) {
         console.log("tracking enabled");
         enabled = true;
       } else {
