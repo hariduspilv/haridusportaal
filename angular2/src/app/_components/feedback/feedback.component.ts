886c72416d0af83e1e298f989a549b68e84b456c
Henri Kokk <henri.kokk@twn.ee>
2019-01-16 17:42:06 +0200

    Feedback form

---
+++ angular2/src/app/_components/feedback/feedback.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input } from "@angular/core";
+import { HttpService } from "@app/_services/httpService";
+
+@Component({
+  selector: "feedback",
+  templateUrl: "feedback.template.html",
+  styleUrls: ["feedback.styles.scss"]
+})
+
+export class FeedbackComponent {
+
+  @Input() nid: any;
+
+  status: String = 'vote';
+
+  values: Object = {};
+
+  constructor(
+    private http: HttpService
+  ){
+
+  }
+  vote( flag: boolean ){
+    this.values['vote'] = flag;
+    if( flag ){
+      this.sendVote();
+    }else{
+      this.status = 'add_comment';
+    }
+  }
+
+  sendVote() {
+    this.status = 'response';
+
+    let data = {
+      nid: this.nid,
+      type: this.values['vote'] ? 1 : 0,
+      message: this.values['comment'] || ''
+    }
+
+    let subscribe = this.http.post("/feedback?_format=json", data).subscribe( (response) => {
+      console.log(response);
+      subscribe.unsubscribe();
+    });
+  }
+
+}
