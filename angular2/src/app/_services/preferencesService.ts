3e52be4ec56e63cf0f7366ee6d107d63d75724d5
Henri Kokk <henri.kokk@twn.ee>
2019-02-11 17:34:48 +0200

    Chart update

---
+++ angular2/src/app/_services/preferencesService.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class PreferencesService {
+	constructor(
+		private http: HttpClient
+	){}
+
+  error: boolean = false;
+  
+	data: any;
+
+	get( key:string = '' ) {
+		if( !this.data ){
+			return undefined;
+		}
+		return this.data[key] || undefined;
+	}
+
+	load() {
+		return new Promise((resolve, reject) => {
+			this.http.get('/translations?_format=json&lang=et').subscribe(response => {
+				this.data = response;
+				resolve(true);
+			}, () => {
+				this.error = true;
+				resolve(true);
+			});
+		})
+	}
+}
