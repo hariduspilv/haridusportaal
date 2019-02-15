d9e1bcf48815027e28a3fb4c2101eaa2c8d5ab22
Henri Kokk <henri.kokk@twn.ee>
2019-02-15 12:56:14 +0200

    Graph updates

--- angular2/src/app/_components/chart/chart.component.ts
+++ angular2/src/app/_components/chart/chart.component.ts
@@ -20,7 +20,8 @@ export class ChartComponent implements OnInit{
   filtersData:any = {};
 
   requestDebounce = {};
-  requestSubscription:any = false;
+  requestSubscription = {};
+
   graphOptions = {
     height: 400,
     pieSliceTextStyle: {
@@ -86,7 +87,6 @@ export class ChartComponent implements OnInit{
       if( chartType == "Doughnut" ){
         chartType = "Pie";
       }
-
 
       if( chartType.toLowerCase() == "clustered bar"){
         isStacked = true;
@@ -213,8 +213,8 @@ export class ChartComponent implements OnInit{
       item.id = this.generateID();
       item.graph_group_by = item.filterValues.graph_options.graph_group_by;
       item.graph_v_axis = item.filterValues.graph_options.graph_v_axis;
-      
-      console.log(item);
+      item.secondaryGraphType = item.filterValues.graph_options.secondary_graph_type;
+
       this.filters[ item.id ] = {};
 
       try{
@@ -256,7 +256,6 @@ export class ChartComponent implements OnInit{
 
         this.filters[ item.id ]['näitaja'] = [ item.filters[ item.filters.length - 1 ].options[0] ];
 
-        console.log( item );
       }catch(err){
         console.error("Couldn't parse indicators!");
       }
@@ -273,10 +272,11 @@ export class ChartComponent implements OnInit{
 
     clearTimeout( this.requestDebounce[id] );
 
+
     this.requestDebounce[id] = setTimeout( () => {
 
-      if( this.requestSubscription ){
-        this.requestSubscription.unsubscribe();
+      if( this.requestSubscription[id] ){
+        this.requestSubscription[id].unsubscribe();
       }
       
       let current = this.data.filter( (item) => {
@@ -288,12 +288,13 @@ export class ChartComponent implements OnInit{
       let filters = this.filters[current.id];
   
       if( !this.filtersData[current.id] ){ this.filtersData[current.id] = {}; }
+
       this.filtersData[current.id].loading = true;
 
       let variables = {
         graphSet: current['graphSet'],
         graphType: current['graphType'],
-        secondaryGraphType: '',
+        secondaryGraphType: current.secondaryGraphType,
         indicator: filters['näitaja'].length > 0 ? filters['näitaja'] : false,
         secondaryGraphIndicator: '',
         oskaField: filters.valdkond || '',
@@ -327,7 +328,7 @@ export class ChartComponent implements OnInit{
         }
       }
   
-      this.requestSubscription = this.http.get( this.dataUrl + JSON.stringify( tmpVariables ) ).subscribe( (response) => {
+      this.requestSubscription[id] = this.http.get( this.dataUrl + JSON.stringify( tmpVariables ) ).subscribe( (response) => {
 
         let data = response['data'].GoogleChartQuery.map( (item) => {
 
@@ -339,7 +340,7 @@ export class ChartComponent implements OnInit{
             graphTitle: current.graphTitle,
             graphSet: variables['graphSet'],
             value: item.ChartValue,
-            secondaryGraphType:	null,
+            secondaryGraphType:	variables['secondaryGraphType'],
             secondaryGraphIndicator:	null
           }
         });
@@ -348,8 +349,10 @@ export class ChartComponent implements OnInit{
 
         this.filtersData[current.id].loading = false;
 
-        this.requestSubscription.unsubscribe();
-        this.requestSubscription = false;
+        this.requestSubscription[id].unsubscribe();
+        this.requestSubscription[id] = false;
+
+        console.log( JSON.stringify( this.filtersData[current.id].dataTable ) );
 
       }, (err) =>{
         this.filtersData[current.id].loading = false;
