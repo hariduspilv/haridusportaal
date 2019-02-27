2d483248233bf98aa131214085401f9408aad024
root <lars.soonvald@gmail.com>
2019-02-27 14:25:45 +0000

    new module for writing watchdog logs to file

---
+++ drupal/web/modules/custom/htm_custom_file_logging/src/Logger/FileLog.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\htm_custom_file_logging\Logger;
+
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Logs events in the watchdog database table.
+ */
+class FileLog implements LoggerInterface {
+  use RfcLoggerTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function log($level, $message, array $context = []) {
+      \Drupal::service('htm_custom_file_logging.write')->write('error', 'Watchdog logid', $message);
+  }
+
+}
