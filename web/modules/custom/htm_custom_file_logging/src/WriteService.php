ef05bf0a249b32e6207c4874c6767ad655864ed5
root <lars.soonvald@gmail.com>
2019-02-28 14:57:07 +0000

    added level for logging to file

--- web/modules/custom/htm_custom_file_logging/src/WriteService.php
+++ web/modules/custom/htm_custom_file_logging/src/WriteService.php
@@ -10,55 +10,63 @@ use Monolog\Handler\StreamHandler;
  */
 class WriteService {
 
-  public function write($severity, $type, $message) {
-    $type = $this->parse_filename($type);
-    $type != NULL || $type != '' ? $logpath = $this->createLogFile($type) : false;
-    $this->createLogEntry($severity, $logpath, $type, $message);
-  }
-
-  private function createLogFile($type){
-    $logpath = '/app/drupal/web/sites/default/files/logs/'.date('Y').'/'.date('m');
-    if(!file_exists($logpath)) mkdir($logpath, 0744, true);
-    $logpath .= '/'.$type.'.log';
-    fopen($logpath, 'a');
-
-    return $logpath;
-  }
-
-  private function createLogEntry($severity, $logpath, $type, $message){
-    $log = new Logger($type);
-    $log->pushHandler(new StreamHandler($logpath));
-
-    switch($severity){
-      case 'debug':
-        $log->debug($message);
-        break;
-      case 'info':
-        $log->info($message);
-        break;
-      case 'notice':
-        $log->notice($message);
-        break;
-      case 'warning':
-        $log->debug($message);
-        break;
-      case 'error':
-        $log->error($message);
-        break;
-      case 'critical':
-        $log->critical($message);
-        break;
-      case 'alert':
-        $log->alert($message);
-        break;
-      case 'emergency':
-        $log->emergency($message);
-        break;
+    public function write($severity, $type, $message) {
+        $type = $this->parse_filename($type);
+        $type != NULL || $type != '' ? $logpath = $this->createLogFile($type) : false;
+        $this->createLogEntry($severity, $logpath, $type, $message);
     }
-  }
 
-  private function parse_filename($filename){
-    return mb_strtolower(str_replace(' ', '_', $filename));
-  }
+    private function createLogFile($type){
+        $logpath = '/app/drupal/web/sites/default/files/logs/'.date('Y').'/'.date('m');
+        if(!file_exists($logpath)) mkdir($logpath, 0744, true);
+        $logpath .= '/'.$type.'.log';
+        fopen($logpath, 'a');
+
+        return $logpath;
+    }
+
+    private function createLogEntry($severity, $logpath, $type, $message){
+        $log = new Logger($type);
+        $log->pushHandler(new StreamHandler($logpath));
+
+        switch($severity){
+            case 'debug':
+            case '7':
+                $log->debug($message);
+                break;
+            case 'info':
+            case '6':
+                $log->info($message);
+                break;
+            case 'notice':
+            case '5':
+                $log->notice($message);
+                break;
+            case 'warning':
+            case '4':
+                $log->debug($message);
+                break;
+            case 'error':
+            case '3':
+                $log->error($message);
+                break;
+            case 'critical':
+            case '2':
+                $log->critical($message);
+                break;
+            case 'alert':
+            case '1':
+                $log->alert($message);
+                break;
+            case 'emergency':
+            case '0':
+                $log->emergency($message);
+                break;
+        }
+    }
+
+    private function parse_filename($filename){
+        return mb_strtolower(str_replace(' ', '_', $filename));
+    }
 
 }
