4eecd0cc6f7aca7c7fe5def494a3203e020d234f
root <lars.soonvald@gmail.com>
2019-03-05 19:44:10 +0000

    working admin form for auth methods with rest

---
+++ drupal/web/modules/custom/htm_custom_authentication/src/Plugin/rest/resource/AuthMethodsRestResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\htm_custom_authentication\Plugin\rest\resource;
+
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Provides a resource to get enabled authentication methods.
+ *
+ * @RestResource(
+ *   id = "auth_methods_rest_resource",
+ *   label = @Translation("Authentication methods rest resource"),
+ *   uri_paths = {
+ *     "canonical" = "auth_methods"
+ *   }
+ * )
+ */
+class AuthMethodsRestResource extends ResourceBase {
+
+    /**
+     * A current user instance.
+     *
+     * @var \Drupal\Core\Session\AccountProxyInterface
+     */
+    protected $currentUser;
+
+    /**
+     * Constructs a new BaseSettingsRestResource object.
+     *
+     * @param array $configuration
+     *   A configuration array containing information about the plugin instance.
+     * @param string $plugin_id
+     *   The plugin_id for the plugin instance.
+     * @param mixed $plugin_definition
+     *   The plugin implementation definition.
+     * @param array $serializer_formats
+     *   The available serialization formats.
+     * @param \Psr\Log\LoggerInterface $logger
+     *   A logger instance.
+     * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+     *   A current user instance.
+     */
+    public function __construct(
+        array $configuration,
+        $plugin_id,
+        $plugin_definition,
+        array $serializer_formats,
+        LoggerInterface $logger,
+        AccountProxyInterface $current_user) {
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+        $this->currentUser = $current_user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+        return new static(
+            $configuration,
+            $plugin_id,
+            $plugin_definition,
+            $container->getParameter('serializer.formats'),
+            $container->get('logger.factory')->get('htm_custom_authentication'),
+            $container->get('current_user')
+        );
+    }
+
+    /**
+     * Responds to GET requests.
+     *
+     *
+     * @return \Drupal\rest\ResourceResponse
+     *   The HTTP response object.
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     *   Throws exception expected.
+     */
+    public function get() {
+        $config = \Drupal::config('htm_custom_authentication.customauthsetting');
+        $values = $config->get();
+        // You must to implement the logic of your REST Resource here.
+        // Use current user after pass authentication to validate access.
+        if (!$this->currentUser->hasPermission('access content')) {
+            throw new AccessDeniedHttpException();
+        }
+
+        $response = new ResourceResponse($values, 200);
+        $response->addCacheableDependency($config);
+
+        return $response;
+    }
+}
