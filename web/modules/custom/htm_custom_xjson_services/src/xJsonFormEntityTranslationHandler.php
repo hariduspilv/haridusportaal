3d421cef7a8bc5b3029927a64a5dc59d9f799ff4
root <lars.soonvald@gmail.com>
2019-03-13 11:57:35 +0000

    working xjson schema validation

---
+++ web/modules/custom/htm_custom_xjson_services/src/xJsonFormEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\htm_custom_xjson_services;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for x_json_form_entity.
+ */
+class xJsonFormEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
