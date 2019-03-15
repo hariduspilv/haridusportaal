42416a3f6d7dea69f5ac73d0dd7fcaa450cf28b1
root <lars.soonvald@gmail.com>
2019-03-15 12:01:47 +0000

    xjson form rest support

--- drupal/web/modules/custom/htm_custom_xjson_services/src/xJsonFormEntityStorageInterface.php
+++ drupal/web/modules/custom/htm_custom_xjson_services/src/xJsonFormEntityStorageInterface.php
@@ -8,34 +8,34 @@ use Drupal\Core\Language\LanguageInterface;
 use Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface;
 
 /**
- * Defines the storage handler class for X json form entity entities.
+ * Defines the storage handler class for xJson form entity entities.
  *
  * This extends the base storage class, adding required special handling for
- * X json form entity entities.
+ * xJson form entity entities.
  *
  * @ingroup htm_custom_xjson_services
  */
 interface xJsonFormEntityStorageInterface extends ContentEntityStorageInterface {
 
   /**
-   * Gets a list of X json form entity revision IDs for a specific X json form entity.
+   * Gets a list of xJson form entity revision IDs for a specific xJson form entity.
    *
    * @param \Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface $entity
-   *   The X json form entity entity.
+   *   The xJson form entity entity.
    *
    * @return int[]
-   *   X json form entity revision IDs (in ascending order).
+   *   xJson form entity revision IDs (in ascending order).
    */
   public function revisionIds(xJsonFormEntityInterface $entity);
 
   /**
-   * Gets a list of revision IDs having a given user as X json form entity author.
+   * Gets a list of revision IDs having a given user as xJson form entity author.
    *
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The user entity.
    *
    * @return int[]
-   *   X json form entity revision IDs (in ascending order).
+   *   xJson form entity revision IDs (in ascending order).
    */
   public function userRevisionIds(AccountInterface $account);
 
@@ -43,7 +43,7 @@ interface xJsonFormEntityStorageInterface extends ContentEntityStorageInterface 
    * Counts the number of revisions in the default language.
    *
    * @param \Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface $entity
-   *   The X json form entity entity.
+   *   The xJson form entity entity.
    *
    * @return int
    *   The number of revisions in the default language.
@@ -51,7 +51,7 @@ interface xJsonFormEntityStorageInterface extends ContentEntityStorageInterface 
   public function countDefaultLanguageRevisions(xJsonFormEntityInterface $entity);
 
   /**
-   * Unsets the language for all X json form entity with the given language.
+   * Unsets the language for all xJson form entity with the given language.
    *
    * @param \Drupal\Core\Language\LanguageInterface $language
    *   The language object.
