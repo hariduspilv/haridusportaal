7e7a6d40e8e97eb461cfb25fc249d12c1326eb44
root <lars.soonvald@gmail.com>
2019-03-15 12:01:47 +0000

    xjson form rest support

--- web/modules/custom/htm_custom_xjson_services/src/Entity/xJsonFormEntityInterface.php
+++ web/modules/custom/htm_custom_xjson_services/src/Entity/xJsonFormEntityInterface.php
@@ -8,7 +8,7 @@ use Drupal\Core\Entity\EntityChangedInterface;
 use Drupal\user\EntityOwnerInterface;
 
 /**
- * Provides an interface for defining X json form entity entities.
+ * Provides an interface for defining xJson form entity entities.
  *
  * @ingroup htm_custom_xjson_services
  */
@@ -17,66 +17,66 @@ interface xJsonFormEntityInterface extends ContentEntityInterface, RevisionLogIn
   // Add get/set methods for your configuration properties here.
 
   /**
-   * Gets the X json form entity name.
+   * Gets the xJson form entity name.
    *
    * @return string
-   *   Name of the X json form entity.
+   *   Name of the xJson form entity.
    */
   public function getName();
 
   /**
-   * Sets the X json form entity name.
+   * Sets the xJson form entity name.
    *
    * @param string $name
-   *   The X json form entity name.
+   *   The xJson form entity name.
    *
    * @return \Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface
-   *   The called X json form entity entity.
+   *   The called xJson form entity entity.
    */
   public function setName($name);
 
   /**
-   * Gets the X json form entity creation timestamp.
+   * Gets the xJson form entity creation timestamp.
    *
    * @return int
-   *   Creation timestamp of the X json form entity.
+   *   Creation timestamp of the xJson form entity.
    */
   public function getCreatedTime();
 
   /**
-   * Sets the X json form entity creation timestamp.
+   * Sets the xJson form entity creation timestamp.
    *
    * @param int $timestamp
-   *   The X json form entity creation timestamp.
+   *   The xJson form entity creation timestamp.
    *
    * @return \Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface
-   *   The called X json form entity entity.
+   *   The called xJson form entity entity.
    */
   public function setCreatedTime($timestamp);
 
   /**
-   * Returns the X json form entity published status indicator.
+   * Returns the xJson form entity published status indicator.
    *
-   * Unpublished X json form entity are only visible to restricted users.
+   * Unpublished xJson form entity are only visible to restricted users.
    *
    * @return bool
-   *   TRUE if the X json form entity is published.
+   *   TRUE if the xJson form entity is published.
    */
   public function isPublished();
 
   /**
-   * Sets the published status of a X json form entity.
+   * Sets the published status of a xJson form entity.
    *
    * @param bool $published
-   *   TRUE to set this X json form entity to published, FALSE to set it to unpublished.
+   *   TRUE to set this xJson form entity to published, FALSE to set it to unpublished.
    *
    * @return \Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface
-   *   The called X json form entity entity.
+   *   The called xJson form entity entity.
    */
   public function setPublished($published);
 
   /**
-   * Gets the X json form entity revision creation timestamp.
+   * Gets the xJson form entity revision creation timestamp.
    *
    * @return int
    *   The UNIX timestamp of when this revision was created.
@@ -84,18 +84,18 @@ interface xJsonFormEntityInterface extends ContentEntityInterface, RevisionLogIn
   public function getRevisionCreationTime();
 
   /**
-   * Sets the X json form entity revision creation timestamp.
+   * Sets the xJson form entity revision creation timestamp.
    *
    * @param int $timestamp
    *   The UNIX timestamp of when this revision was created.
    *
    * @return \Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface
-   *   The called X json form entity entity.
+   *   The called xJson form entity entity.
    */
   public function setRevisionCreationTime($timestamp);
 
   /**
-   * Gets the X json form entity revision author.
+   * Gets the xJson form entity revision author.
    *
    * @return \Drupal\user\UserInterface
    *   The user entity for the revision author.
@@ -103,13 +103,13 @@ interface xJsonFormEntityInterface extends ContentEntityInterface, RevisionLogIn
   public function getRevisionUser();
 
   /**
-   * Sets the X json form entity revision author.
+   * Sets the xJson form entity revision author.
    *
    * @param int $uid
    *   The user ID of the revision author.
    *
    * @return \Drupal\htm_custom_xjson_services\Entity\xJsonFormEntityInterface
-   *   The called X json form entity entity.
+   *   The called xJson form entity entity.
    */
   public function setRevisionUserId($uid);
 
