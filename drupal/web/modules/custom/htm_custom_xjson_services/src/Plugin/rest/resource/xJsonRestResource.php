2a98728683e76daad43da2b5837977ff79de74e7
root <lars.soonvald@gmail.com>
2019-03-27 12:51:05 +0000

    updated error when must log in

--- drupal/web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
+++ drupal/web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
@@ -104,7 +104,7 @@ class xJsonRestResource extends ResourceBase {
                 if($this->auth_required){
                     // Use current user after pass authentication to validate access.
                     if (!$this->currentUser->isAuthenticated()) {
-                        throw new AccessDeniedHttpException();
+                        return new ModifiedResourceResponse('User must log in!', 400);
                     }
                 }
 
