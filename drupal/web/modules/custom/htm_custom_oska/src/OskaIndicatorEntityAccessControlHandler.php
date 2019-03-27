0c017120d322adfa94b9e02053abc5b665d84076
root <lars.soonvald@gmail.com>
2019-03-27 12:32:44 +0000

    hide status

--- drupal/web/modules/custom/htm_custom_oska/src/OskaIndicatorEntityAccessControlHandler.php
+++ drupal/web/modules/custom/htm_custom_oska/src/OskaIndicatorEntityAccessControlHandler.php
@@ -21,6 +21,9 @@ class OskaIndicatorEntityAccessControlHandler extends EntityAccessControlHandler
     /** @var \Drupal\htm_custom_oska\Entity\OskaIndicatorEntityInterface $entity */
     switch ($operation) {
       case 'view':
+        if (!$entity->isPublished()) {
+          return AccessResult::allowedIfHasPermission($account, 'view unpublished oska indicator entity entities');
+        }
         return AccessResult::allowedIfHasPermission($account, 'view published oska indicator entity entities');
 
       case 'update':
