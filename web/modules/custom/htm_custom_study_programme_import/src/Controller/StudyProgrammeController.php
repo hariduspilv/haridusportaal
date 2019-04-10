47113fa0ab595916a977f6a9c33abfce0ce8f056
root <lars.soonvald@gmail.com>
2019-04-10 09:26:18 +0000

    should be working, testing

--- web/modules/custom/htm_custom_study_programme_import/src/Controller/StudyProgrammeController.php
+++ web/modules/custom/htm_custom_study_programme_import/src/Controller/StudyProgrammeController.php
@@ -16,7 +16,6 @@ class StudyProgrammeController extends ControllerBase {
         $schools = $this->get_existing_schools();
         $taxonomies['studyprogrammetype'] = $this->get_taxonomy_terms('studyprogrammetype');
         $programmes = $this->get_programme_data('programme', $schools, $taxonomies['studyprogrammetype']);
-        $unpublishnodes = $this->unpublishNonExistantProgrammes($programmes);
         $update_from_ehis_nodes = $this->get_ehis_updateable_nodes();
         $taxonomies['iscedf'] = $this->get_taxonomy_outputs('isced_f','field_code');
         $taxonomies['degreeordiploma'] = $this->get_taxonomy_terms('degreeordiploma');
@@ -364,8 +363,6 @@ class StudyProgrammeController extends ControllerBase {
                 ];
             }
         }
-        kint($programme_nodes);
-        die();
         return $programme_nodes;
     }
 
