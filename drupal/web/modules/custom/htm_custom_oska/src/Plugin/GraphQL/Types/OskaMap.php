4f48567df3f95e3f85b9e3accabdad582549df37
root <lars.soonvald@gmail.com>
2019-04-19 08:29:01 +0000

    oska map data csv upload with query

---
+++ drupal/web/modules/custom/htm_custom_oska/src/Plugin/GraphQL/Types/OskaMap.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\htm_custom_oska\Plugin\GraphQL\Types;
+
+use Drupal\graphql\Plugin\GraphQL\Types\TypePluginBase;
+
+/**
+ * GraphQL type representing Google Chart query.
+ *
+ * @GraphQLType(
+ *   id = "oska_map",
+ *   name = "OskaMap"
+ * )
+ */
+class OskaMap extends TypePluginBase {
+}
