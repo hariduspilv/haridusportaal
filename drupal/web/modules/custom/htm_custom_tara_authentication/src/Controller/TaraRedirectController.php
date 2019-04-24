bfb3be72d2ef140dde8fe34f76808e2aba327949
root <lars.soonvald@gmail.com>
2019-04-24 10:16:46 +0000

    fixed little bug

--- drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
+++ drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
@@ -119,7 +119,7 @@ class TaraRedirectController extends RedirectController{
 				}
 			}
 		}
->
+
 		$fe_url = $this->config('htm_custom_admin_form.customadmin')->get('general.fe_url').'/auth.html';
 		if(empty($this->messenger()->all()) && !$redirect_home){
 			$query = ['jwt' => $this->jsonAuth->generateToken(), 'error' => 'false'];
