ac1b1accbea0939bca89fcd4429f8f9146c5c9aa
root <lars.soonvald@gmail.com>
2019-05-08 12:07:48 +0000

    switched to object

--- web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
+++ web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
@@ -183,7 +183,6 @@ class xJsonRestResource extends ResourceBase {
 	private function returnBuildedResponse ($response) {
 		$builded_response = $this->xJsonService->buildFormv2($response);
 		if (empty($builded_response)) return new ModifiedResourceResponse('Form building failed!', 500);
-		dump($builded_response);
 		$builded_response['messages'] = ["test" => "test"];
 		$builded_response['body']['messages'] = ["test" => "test"];
 		return new ModifiedResourceResponse($builded_response, 200);
