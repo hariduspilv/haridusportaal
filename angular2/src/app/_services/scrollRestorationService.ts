231c16eda059be47f21c6cf70c252bb7ec8a1fbc
Madis Põld <madis.pold@twn.ee>
2019-05-28 14:27:06 +0300

    Studyprogrammes scroll #57

--- angular2/src/app/_services/scrollRestorationService.ts
+++ angular2/src/app/_services/scrollRestorationService.ts
@@ -7,31 +7,46 @@ import { Subscription } from 'rxjs';
 export class ScrollRestorationService {
   
   public scrollableRoutes: Array<String> = ['/uudised', '/sündmused', '/erialad', '/kool', '/ametialad', '/valdkonnad', '/otsing'];
+  public scrollableRoutesByHash: Array<String> = ['?displayRelated=true'];
+  public hashRoute: boolean = false;
   public currentRoute: string = '';
   public previousRoute: string = '';
+  public previousRouteHash: string = '';
+  public initialOffset = 0;
   public data: Object = {};
   public routerSub: Subscription;
 
   constructor(private router: Router, private rootScope: RootScopeService) {
+    // Recognize scrollable route by hash;
+    this.hashRoute = this.scrollableRoutesByHash.includes(window.location.search);
+
     this.routerSub = this.router.events.subscribe( (event: Event) => {
       if (event instanceof NavigationStart) {
-        this.currentRoute = decodeURI(window.location.pathname);
+        let prevHash = this.previousRouteHash || window.location.search;
+        this.hashRoute = this.scrollableRoutesByHash.includes(prevHash);
+        this.currentRoute = !this.hashRoute ? decodeURI(window.location.pathname) : prevHash;
         const pathName = event.url.split('?')[0];
-        // Initialize current active scrollable route data;
+        // Initialize current scrollable route data;
         if (this.currentRoute !== decodeURI(pathName) && this.scrollableRoutes.includes(decodeURI(pathName))) {
           this.currentRoute = decodeURI(pathName);
           let data = {
             state: false,
-            url: this.currentRoute,
             position: 0
           };
           this.setRouteData(data);
         }
-        // Set current active scrollable route data if routed away from list(list is previous)
+        // Set current scrollable route data if routed away from list(list is previous)
         if (this.scrollableRoutes.includes(this.previousRoute) && this.previousRoute === this.currentRoute) {
           let data = {
             state: rootScope.get('scrollRestorationState'),
-            url: this.currentRoute,
+            position: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+          };
+          this.setRouteData(data);
+        // Set current scrollable route data if hash included && not popstate navigation
+        } else if (this.hashRoute && event.navigationTrigger !== 'popstate') {
+          let data = {
+            state: rootScope.get('scrollRestorationState'),
+            url: this.previousRoute,
             position: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
           };
           this.setRouteData(data);
@@ -42,6 +57,7 @@ export class ScrollRestorationService {
       }
       if (event instanceof NavigationEnd) {
         const pathName = event.url.split('?')[0];
+        this.previousRouteHash = `?${event.url.split('?')[1]}`;
         this.previousRoute = decodeURI(pathName);
       }
     });
@@ -74,7 +90,7 @@ export class ScrollRestorationService {
   }
 
   setScroll() {
-    let scrollData = this.getRoute(this.currentRoute);
+    let scrollData = this.getRoute(this.currentRoute) || this.getRoute(this.previousRouteHash)
     if (scrollData && this.rootScope.get('scrollRestorationState')) {
       window.scrollTo(0, scrollData.position);
     }
