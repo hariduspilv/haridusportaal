dc653498293aad54d133b50dba3134bbb22135a2
Ruut <henri.ruut@twn.ee>
2019-05-29 16:42:46 +0300

    xjson image upload

--- web/modules/custom/htm_custom_ehis_connector/src/Base64Image.php
+++ web/modules/custom/htm_custom_ehis_connector/src/Base64Image.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drupal\htm_custom_ehis_connector;
 
+use Mimey\MimeMappingBuilder;
 use Mimey\MimeTypes;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
@@ -13,56 +14,12 @@ class Base64Image{
 	protected $fileIdentifier;
 	protected $extensions;
 
-	public function __construct($base64Image, $custom_extensions = [])
+	public function __construct($file_path, $fileName)
 	{
-		$this->base64Image = $base64Image;
-		$this->mimes = new MimeTypes();
-		$this->extensions = $this->parseExtensions($custom_extensions);
-		$this->decodeBase64Image();
-	}
-
-	protected function decodeBase64Image(){
-		$this->fileData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $this->base64Image));
-		if($this->fileData === FALSE){
-			throw new BadRequestHttpException('File could not be processed');
-		}
-
-		$f = finfo_open();
-		$mimeType = finfo_buffer($f, $this->fileData, FILEINFO_MIME_TYPE);
-		$ext = $this->validateFile($mimeType);
-		$this->mimeType = $mimeType;
-		$randomId = uniqid(rand());
-		$this->fileName = $randomId . $ext;
-		$this->fileIdentifier = $randomId;
-
-
-	}
-
-	protected function validateFile($mimeType){
-		$insecure_filename = preg_match('/\/(x-php|php|pl|py|cgi|asp|js)(\.|$)/i', $mimeType);
-		if($insecure_filename){
-			throw new BadRequestHttpException('Potentially executable file!');
-		}
-		$mimeTypes = $this->extensions;
-		if(isset($mimeTypes[$mimeType])){
-			return '.' . $mimeTypes[$mimeType];
-		} else {
-			throw new BadRequestHttpException('File type not allowed');
-		}
-	}
-
-	protected function parseExtensions($extensions){
-		$parsed = [];
-		foreach($extensions as $extension){
-			$mime_type = $this->mimes->getMimeType($extension);
-			$parsed[$mime_type] = $extension;
-		}
-
-		return $parsed;
-	}
-
-	public function getFileData(){
-		return $this->fileData;
+		$this->filePath = $file_path;
+        $this->fileName = $fileName;
+        $this->mimeType = $this->setMimeType();
+        $this->fileIdentifier = $this->setIdentifier();
 	}
 
 	public function getFileName(){
@@ -70,7 +27,7 @@ class Base64Image{
 	}
 
 	public function getRawData(){
-		return $this->base64Image;
+		return file_get_contents($this->filePath);
 	}
 
 	public function getMimeType(){
@@ -80,5 +37,28 @@ class Base64Image{
 	public function getFileIdentifier(){
 		return $this->fileIdentifier;
 	}
+
+	protected function setMimeType(){
+        $this->builder = MimeMappingBuilder::create();
+        $this->builder->add('application/x-ddoc', 'ddoc');
+        $this->builder->add('application/vnd.etsi.asic-e+zip', 'bdoc');
+        $this->builder->add('application/vnd.etsi.asic-e+zip', 'asice');
+        $this->builder->add('application/vnd.etsi.asic-e+zip', 'sce');
+
+        $mimes = new MimeTypes($this->builder->getMapping());
+
+
+
+        return $mimes->getMimeType($this->getFileExtension());
+    }
+
+    protected function setIdentifier(){
+        $randomId = uniqid(rand());
+        return $randomId;
+    }
+
+    protected function getFileExtension(){
+	    return substr($this->fileName, strrpos($this->fileName, '.')+1);
+    }
 
 }
