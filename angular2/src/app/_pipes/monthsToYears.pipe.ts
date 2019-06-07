bfc5279dad19cb6429c31af186444366975b9431
Priit Puru <priit.puru@twn.ee>
2019-06-07 11:18:45 +0300

    HP-86

---
+++ angular2/src/app/_pipes/monthsToYears.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'monthsToYears'
+})
+
+export class MonthsToYearsPipe implements PipeTransform {
+
+  transform(value: number): any {
+    const monthOverflow = value % 12;
+    const years = Math.floor(value / 12);
+    if(monthOverflow > 0 && years > 0) {
+      return `${years} aastat ${monthOverflow} kuud`
+    }
+    if(monthOverflow === 0 && years > 1) {
+      return `${years} aastat`
+    }
+    if(monthOverflow > 0 && years === 0) {
+      return `${monthOverflow} kuud`;
+    }
+    return `${years} aasta`;
+  }
+
+}
