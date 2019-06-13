ef6a3de6dcaa1037986b64b2e4317e4d640012d8
root <lars.soonvald@gmail.com>
2019-06-13 07:49:52 +0000

    drupal update

--- drupal/vendor/composer/autoload_real.php
+++ drupal/vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit71f1f0d53ec6ae32c7e1c6bd173a621c
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit71f1f0d53ec6ae32c7e1c6bd173a621c', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        $includePaths[] = get_include_path();
+        set_include_path(implode(PATH_SEPARATOR, $includePaths));
+
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
