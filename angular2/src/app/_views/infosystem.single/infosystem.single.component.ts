87c9fe3028e044e42c37b23914215dc188132b5a
Priit Puru <priit.puru@twn.ee>
2019-06-19 15:45:04 +0300

    New infosystem.single component

---
+++ angular2/src/app/_views/infosystem.single/infosystem.single.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpService } from '@app/_services/httpService';
+import { RootScopeService } from '@app/_services';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: "infosystem",
+  templateUrl: './infosystem.single.template.html',
+  styleUrls: ['./infosystem.single.styles.scss']
+})
+
+export class InfosystemSingle implements OnInit, OnDestroy {
+
+  public data: any = {}
+  public loading: boolean = true;
+  public view: string = "details";
+
+  constructor(
+    private http: HttpService,
+    private rootScope: RootScopeService,
+    private router: Router
+  ) {
+
+  }
+  
+  changeView(view) {
+    if(this.view !== view) {
+      this.view = view;
+    }
+  }
+
+  getData() {
+    const params = {
+      path: this.router.url
+    }
+    this.http.get('infoSystemPage', { params }).subscribe((res:any) => {
+      this.loading = false;
+      this.data = res.data.route.entity;
+      console.log(this.data);
+    }, (err) => {
+      console.log(err);
+      this.loading = false;
+    });
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+  ngOnDestroy() {
+
+  }
+}
