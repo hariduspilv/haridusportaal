e1550b7aaafc5477eb53e48110e640267278042a
root <lars.soonvald@gmail.com>
2019-07-02 11:33:16 +0000

    new resaving module

--- drupal/web/modules/custom/htm_custom_elasticsearch_reindex/src/Form/IndexesForm.php
+++ drupal/web/modules/custom/htm_custom_elasticsearch_reindex/src/Form/IndexesForm.php
@@ -1,97 +1,98 @@
 <?php
-
-namespace Drupal\htm_custom_authentication\Form;
-
-use Drupal\Core\Cache\Cache;
-use Drupal\Core\Form\ConfigFormBase;
+namespace Drupal\htm_custom_elasticsearch_reindex\Form;
+use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\Cache;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Serializer\Serializer;
 
 /**
- * Class CustomAuthSettingForm.
+ * Class DeleteNodeForm.
+ *
+ * @package Drupal\batch_example\Form
  */
-class CustomAuthSettingForm extends ConfigFormBase {
+class IndexesForm extends FormBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getEditableConfigNames() {
-        return [
-            'htm_custom_authentication.customauthsetting',
-        ];
+    public function __construct(Serializer $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('serializer')
+        );
     }
 
     /**
      * {@inheritdoc}
      */
     public function getFormId() {
-        return 'custom_auth_setting_form';
+        return 'elasticsearch_reindex_form';
     }
-
-
     /**
-     * @param array              $form
-     * @param FormStateInterface $form_state
-     * @return array
+     * {@inheritdoc}
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
-        $form = parent::buildForm($form, $form_state);
-        $config = $this->config('htm_custom_authentication.customauthsetting');
 
-        $form['tabs'] = [
-            '#type' => 'vertical_tabs',
-        ];
+        $indexes = \Drupal::entityTypeManager()->getStorage('search_api_index')->loadMultiple();
 
+        $radio_fields = [];
 
-        $form['auth_methods'] = [
-            '#type' => 'fieldset',
-            '#title' => $this->t('Authentication methods')
-        ];
+        foreach($indexes as $index){
+            $radio_fields[$index->getServerInstance()->id()][$index->id()] = $index->get('name');
+        }
 
-        $form['auth_methods']['harid'] = [
-            '#type' => 'checkbox',
-            '#title' => $this->t('HarID'),
-            '#default_value' => $config->get('auth_methods.harid'),
-        ];
+        foreach($radio_fields as $field => $options){
+            $form[$field] = [
+                '#type' => 'checkboxes',
+                '#title' => $field,
+                '#options' => $options,
+                '#multiple' => TRUE
+            ];
+        }
 
-        $form['auth_methods']['tara'] = [
-            '#type' => 'checkbox',
-            '#title' => $this->t('TARA'),
-            '#default_value' => $config->get('auth_methods.tara'),
-        ];
-
-        $form['auth_methods']['mobile_id'] = [
-            '#type' => 'checkbox',
-            '#title' => $this->t('Mobile-ID'),
-            '#default_value' => $config->get('auth_methods.mobile_id'),
+        $form['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Import'),
         ];
 
         return $form;
     }
 
-
     /**
-     * @param array              $form
-     * @param FormStateInterface $form_state
-     */
-    public function validateForm(array &$form, FormStateInterface $form_state) {
-        parent::validateForm($form, $form_state);
-    }
-
-
-    /**
-     * @param array              $form
-     * @param FormStateInterface $form_state
+     * {@inheritdoc}
      */
     public function submitForm(array &$form, FormStateInterface $form_state) {
-        parent::submitForm($form, $form_state);
+        $user_input = array_diff_key($form_state->getUserInput(), array_flip($form_state->getCleanValueKeys()));
 
-        $this->config('htm_custom_authentication.customauthsetting')
-            ->set('auth_methods.harid', $form_state->getValue('harid'))
-            ->set('auth_methods.tara', $form_state->getValue('tara'))
-            ->set('auth_methods.mobile_id', $form_state->getValue('mobile_id'))
-            ->save();
+        #remove indexes that were not selected
+        foreach($user_input as $server => $indexes){
+            foreach($indexes as $index => $value){
+                if(!$value){
+                    unset($user_input[$server][$index]);
+                }
+            }
+        }
 
-        Cache::invalidateTags(['config:htm_custom_authentication.customauthsetting']);
+        $batch = [
+            'title' => t('Rebuilding indexes ....--'),
+            'operations' => [
+                [
+                    '\Drupal\htm_custom_elasticsearch_reindex\ProcessIndexes::ResaveIndexes',
+                    [$user_input]
+                ],
+                #[
+                #    '\Drupal\htm_custom_elasticsearch_reindex\ProcessIndexes::RebuildIndexes',
+                #    [$user_input]
+                #]
+            ],
+            'error_message' => t('The rebuilding process has encountered an error.'),
+            'finished' => '\Drupal\htm_custom_elasticsearch_reindex\ProcessIndexes::ProcessIndexesFinishedCallback'
+        ];
+
+        batch_set($batch);
+
     }
-
 }
--- drupal/web/modules/custom/htm_custom_elasticsearch_reindex/src/ProcessIndexes.php
+++ drupal/web/modules/custom/htm_custom_elasticsearch_reindex/src/ProcessIndexes.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace Drupal\htm_custom_oska;
+namespace Drupal\htm_custom_elasticsearch_reindex;
 
-use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\htm_custom_oska\Entity\OskaTableEntity;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Class ProcessOskaTableData
- * @package Drupal\htm_custom_oska
+ * Class ProcessIndexes
+ * @package Drupal\htm_custom_elasticsearch_reindex
  */
-class ProcessOskaTableData {
+class ProcessIndexes {
 
 
     /**
@@ -21,68 +19,26 @@ class ProcessOskaTableData {
     }
 
 
-    public static function ValidateFile($items, &$context){
-        $message = t('Validating file');
+    public static function ResaveIndexes($items, &$context){
+        $message = t('Resaving Indexes');
+        $context['results']['values'] = 0;
 
-        //first delete all subsidies
-        self::deleteAllEntities();
+        $server_storage = \Drupal::entityTypeManager()->getStorage('search_api_server')->loadMultiple();
 
-        #dump(self::$k['EHIS_ID']);
-        $results = [];
-        $object = [
-            'valdkond' => false,
-        ];
-        foreach ($items as $index => $item){
-
-            foreach($item as $key => $value){
-                if(mb_detect_encoding($key) == 'UTF-8'){
-                    unset($item[$key]);
-                    $item[cleanString($key)] = $value;
-                }
-            }
-
-            $object['valdkond'] = self::checkEntityReference('node', 'oska_field_page', $item['valdkond']);
-            $object['ettepanek'] = mb_strlen($item['ettepanek']) <= 500 ? $item['ettepanek'] : FALSE;
-            $object['peavastutaja'] = mb_strlen($item['peavastutaja']) <= 100 ? $item['peavastutaja'] : FALSE;
-            $object['staatus'] = mb_strlen($item['staatus']) <= 50 ? $item['staatus'] : FALSE;
-            $object['kommentaar'] = mb_strlen($item['kommentaar']) <= 500 ? $item['kommentaar'] : FALSE;
-
-            if(
-                !$object['valdkond']
-                ||
-                !$object['ettepanek']
-                ||
-                !$object['peavastutaja']
-                ||
-                !$object['staatus']
-                ||
-                !$object['kommentaar']){
-
-                $error_messag_func = function($values) {
-                    foreach($values as $key => $value){
-                        if($value === FALSE){
-                            return $key;
-                        }
-                    }
-                };
-                $context['results']['error'][] = t('Error on line: '. ($index + 2) . ' | column: ' . $error_messag_func($object));
-            }else{
-                $results[] = [
-                    'oska_field' => $object['valdkond'],
-                    'proposal' => $object['ettepanek'],
-                    'responsible' => $object['peavastutaja'],
-                    'proposal_status' => $object['staatus'],
-                    'expert_commentary' => $object['kommentaar'],
-
-                ];
+        foreach($items as $server => $indexes){
+            $loaded_indexes = $server_storage[$server]->getIndexes(['id' => $indexes]);
+            foreach($loaded_indexes as $index){
+                $index->save();
+                $index->reindex();
+                $context['results']['values']++;
             }
         }
 
         $context['message'] = $message;
-        $context['results']['values'] = $results;
     }
 
-    public static function ProcessOskaTableData($items, &$context){
+    public static function RebuildIndexes($items, &$context){
+
         //process only if no errors otherwise nothing
         if(empty($context['results']['error'])){
             if(empty($context['sandbox'])){
@@ -97,20 +53,16 @@ class ProcessOskaTableData {
                 if ($context['sandbox']['max'] - $context['sandbox']['current_id'] < 10){
                     $limit = $context['sandbox']['max'] + 1;
                 }
-                for($i = $context['sandbox']['current_id']; $i < $limit; $i++){
-                    // do something
-                    $values = $context['results']['values'][$i];
-                    if($values){
-                        $entity = OskaTableEntity::create($values);
+                $server_storage = \Drupal::entityTypeManager()->getStorage('search_api_server')->loadMultiple();
+                foreach($items as $server => $indexes){
+                    $loaded_indexes = $server_storage[$server]->getIndexes(['id' => $indexes]);
+                    foreach($loaded_indexes as $index){
+                        $index->indexItems();
+                        $context['sandbox']['progress']++;
+                        $context['sandbox']['current_id'] = $context['sandbox']['current_id'];
+                        $context['message'] = $context['sandbox']['max'];
+                        $context['results']['processed'][] = $index->id();
                     }
-
-                    $entity->save();
-                    $context['sandbox']['progress']++;
-                    $context['sandbox']['current_id'] = $i;
-                    #$context['message'] = t('Processing lines : @limit - @current ', ['@limit' => $limit, '@current' => $context['sandbox']['current_id'] + 1]);
-                    $context['message'] = $context['sandbox']['max'];
-
-                    $context['results']['processed'][] = $entity->id();
                 }
                 $context['sandbox']['current_id']++;
 
@@ -124,7 +76,7 @@ class ProcessOskaTableData {
         }
     }
 
-    public static function ProcessOskaTableDataFinishedCallback($success, $results, $operations){
+    public static function ProcessIndexesFinishedCallback($success, $results, $operations){
         // The 'success' parameter means no fatal PHP errors were detected. All
         // other error management should be handled using 'results'.
         if ($success) {
@@ -141,26 +93,5 @@ class ProcessOskaTableData {
             $message = [t('Finished with an error.'), 'error'];
         }
         drupal_set_message($message[0], $message[1]);
-    }
-
-    public function checkEntityReference($entity_type, $vocabulary, $name){
-
-        $storage = \Drupal::service('entity_type.manager')->getStorage($entity_type);
-
-        $properties = [
-            'type' => $vocabulary,
-            'title' => $name
-        ];
-
-        $entity = reset($storage->loadByProperties($properties));
-
-        return ($entity) ? $entity->id() : FALSE;
-    }
-
-    private function deleteAllEntities(){
-        $ids = \Drupal::entityQuery('oska_table_entity')->execute();
-        $storage_handler = \Drupal::entityTypeManager()->getStorage('oska_table_entity');
-        $entities = $storage_handler->loadMultiple($ids);
-        $storage_handler->delete($entities);
     }
 }
