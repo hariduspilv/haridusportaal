4d58bc0f67d5a72371090a2b5f836fdd4d2cb279
root <lars.soonvald@gmail.com>
2019-07-16 09:06:38 +0000

    testing xjson

--- web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
+++ web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
@@ -175,8 +175,6 @@ class xJsonRestResource extends ResourceBase {
 
 		if (empty($request_body)) return new ModifiedResourceResponse('form_name unknown', 400);
 		$response = $this->ehisService->postDocument(['json' => $request_body]);
-		dump(json_encode($response));
-		die();
 		return $this->returnBuildedResponse($response);
 	}
 
