e19460860dc696f3580698dd2e4ce767e05dd68b
Madis Põld <madis.pold@twn.ee>
2019-07-22 18:53:07 +0300

    Header #HP-177

---
+++ angular-fe/src/app/_assets/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'htm-header',
+  templateUrl: './header.template.html',
+  styleUrls: ['./header.styles.scss'],
+})
+
+export class HeaderComponent{ 
+  public active: boolean = false;
+}
---
+++ angular-fe/stories/assets/header/header.stories.ts
@@ -0,0 +1,23 @@
+import { storiesOf } from '@storybook/angular';
+import { AssetsModule } from '@app/_assets';
+import headerMd from './header.md';
+import { TranslateModule } from '@app/_modules/translate';
+
+const moduleMetadata = {
+  imports: [
+    AssetsModule,
+    TranslateModule.forRoot()
+  ]
+};
+
+const stories = storiesOf('Assets', module);
+stories.add('Header', () => {
+  return {
+    moduleMetadata,
+    template: `
+      <htm-header></htm-header>
+    `,
+  };
+},          {
+  notes: { markdown: headerMd },
+});
