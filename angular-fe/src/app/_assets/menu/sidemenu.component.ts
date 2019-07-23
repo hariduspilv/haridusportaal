b0f6c65a7af88f3cb761453beaaf7a48d2dc09ad
Priit <purupriit@gmail.com>
2019-07-23 14:46:35 +0300

    Fe refactor sidemenu (#903)
    * added sidemenucomponent
    
    * changed font-weight
    
    * formatting and bugfixes
    
    * css fix
    
    * WCAG css fixes
    
    * visual uniformity fixes
    
    * wut

---
+++ angular-fe/src/app/_assets/menu/sidemenu.component.ts
@@ -0,0 +1,79 @@
+import {
+  Component,
+  Input,
+  HostBinding,
+  OnInit,
+} from '@angular/core';
+import { RippleService } from '@app/_services';
+
+@Component({
+  selector: 'sidemenu-item',
+  styleUrls: ['./sidemenu.styles.scss'],
+  template: `
+		<ng-container *ngIf="item.links.length === 0; else expandable">
+			<a
+				*ngIf="item.url.path !== '#nolink'"
+				(mousedown)="animateRipple($event)"
+				class="sidemenu__link"
+				href="{{ item.url.path }}"
+				><span>{{ item.label }}</span></a
+			>
+		</ng-container>
+    <ng-template #expandable>
+			<button
+				class="sidemenu__link"
+				(click)="toggle($event)"
+				[attr.aria-expanded]="isOpen"
+				[attr.aria-controls]="item.label | slugify"
+			>
+				<span>{{ item.label }}</span>
+				<icon glyph="chevron-down" size="medium"></icon>
+			</button>
+			<ul id="{{item.label | slugify}}">
+				<li *ngFor="let item of item.links" (mousedown)="animateRipple($event)">
+					<a
+						*ngIf="item.url.path !== '#nolink'"
+						class="sidemenu__link"
+						href="{{ item.url.path }}"
+					>
+						<span>{{ item.label }}</span>
+					</a>
+				</li>
+			</ul>
+		</ng-template>
+	`,
+})
+export class SidemenuItemComponent {
+  public isOpen: boolean = false;
+  @Input() item: object = {};
+
+  constructor(private ripple: RippleService) { }
+
+  toggle(e) {
+    this.isOpen = !this.isOpen;
+    this.animateRipple(e);
+  }
+  animateRipple(event) {
+    this.ripple.animate(event, 'dark');
+  }
+}
+
+@Component({
+  selector: 'sidemenu',
+  templateUrl: './sidemenu.template.html',
+  styleUrls: ['./sidemenu.styles.scss'],
+})
+export class MenuComponent implements OnInit {
+  @Input() data = Object;
+  @HostBinding('class') get hostClasses(): string {
+    return 'sidemenu';
+  }
+
+  constructor() { }
+
+  // needs to have a service associated to it
+  // needs to get menu data on init
+  ngOnInit() {
+    console.log(this.data);
+  }
+}
