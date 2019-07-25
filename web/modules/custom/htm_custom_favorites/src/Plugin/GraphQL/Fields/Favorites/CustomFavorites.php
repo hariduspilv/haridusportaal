b1f52e778ba7dbf232a558896a55c13fb09f0b0f
root <lars.soonvald@gmail.com>
2019-07-25 11:19:07 +0000

    testing cache

--- web/modules/custom/htm_custom_favorites/src/Plugin/GraphQL/Fields/Favorites/CustomFavorites.php
+++ web/modules/custom/htm_custom_favorites/src/Plugin/GraphQL/Fields/Favorites/CustomFavorites.php
@@ -24,7 +24,7 @@ use Drupal\Core\Language\LanguageManager;
  *   description = @Translation("Loads all user favorites"),
  *   type = "Entity",
  *   response_cache_tags = {"favorite_entity_list"},
- *   response_cache_context = {"user", "languages:language_content"},
+ *   response_cache_context = {"user"},
  *   arguments = {
  *     "language" = "LanguageId"
  *   },
