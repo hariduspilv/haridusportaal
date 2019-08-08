ed327467f44d1ccfb68e8ed2f329b80469bd3f6d
Henri Kokk <henri.kokk@twn.ee>
2019-08-08 18:05:21 +0300

    CSS fixes

--- angular-fe/src/app/_assets/index.ts
+++ angular-fe/src/app/_assets/index.ts
@@ -15,23 +15,23 @@ import { RouterModule } from '@angular/router';
 import { AccordionComponent, AccordionItemComponent } from './accordion';
 import { TableComponent } from './table';
 import { AlertsComponent } from './alerts';
-import ModalService from '@app/_services/ModalService';
 import { EmbedVideoService } from 'ngx-embed-video';
 import { VideoComponent } from './video';
 import { TranslateModule } from '@app/_modules/translate';
 import { FeedbackComponent } from './feedback';
 import { FormsModule } from '@angular/forms';
-import { RippleService, NgbDateCustomParserFormatter, AlertsService, SidemenuService } from '@app/_services';
+import { RippleService, NgbDateCustomParserFormatter, AlertsService } from '@app/_services';
 import { NgbModule, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { MenuComponent, SidemenuItemComponent } from './menu';
 import { HeaderComponent } from './header';
-import { ModalComponent, ModalContentComponent } from './modal';
 import { ScrollableContentComponent } from './scrollableContent';
 import { NgPipesModule } from 'ngx-pipes';
 import { FormItemComponent } from './formItem';
 import { RippleDirective } from '@app/_directives/rippleDirective';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { ModalComponent, ModalContentComponent } from './modal';
 import { BaseLayout } from './base-layout';
+import ModalService from '@app/_services/ModalService';
 
 const declarations = [
   BlockComponent,
@@ -49,14 +49,14 @@ const declarations = [
   AlertsComponent,
   VideoComponent,
   FeedbackComponent,
-  ModalComponent,
-  ModalContentComponent,
   ScrollableContentComponent,
   MenuComponent,
   SidemenuItemComponent,
   HeaderComponent,
   FormItemComponent,
   RippleDirective,
+  ModalComponent,
+  ModalContentComponent,
   BaseLayout,
 ];
 
@@ -69,8 +69,6 @@ const providers = [
   EmbedVideoService,
   RippleService,
   { provide: NgbDateParserFormatter, useClass: NgbDateCustomParserFormatter },
-  ModalService,
-  SidemenuService,
 ];
 
 @NgModule({
