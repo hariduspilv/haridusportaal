8be81e8835ba36b37149349f250ff5e3450ac90f
Madis Põld <madis.pold@twn.ee>
2019-08-13 18:07:14 +0300

    Wcag #HP-171

--- angular2/src/app/_components/more.block/more.block.component.ts
+++ angular2/src/app/_components/more.block/more.block.component.ts
@@ -12,15 +12,37 @@ export class MoreBlockComponent {
   public cutoff: number = 66;
   public active: boolean = false;
   public show: boolean = false;
+  public links: any;
+  public screenReaderContent: string = '';
   @HostBinding('class') get hostClasses(): string {
     return `${this.content ? 'content__box' : 'empty'} ${this.show ? 'show' : 'hide'}`;
   }
   ngAfterViewInit() {
+    this.screenReaderContent = this.content ? this.content.replace(/<[^>]*>/g, '') : '';
     const contentElem = document.getElementById(`moreContent-${this.id}`);
+    this.links = document.querySelectorAll(`#moreContent-${this.id} a`);
+    this.setInnerLinkStates(this.show);
     if (contentElem && contentElem.clientHeight >= this.cutoff) {
       this.active = true;
     } else {
       this.show = true;
+    }
+  }
+  setInnerLinkStates(state) {
+    this.links.forEach((link) => {
+      if (!state) {
+        setTimeout(() => link.setAttribute('style', `visibility: ${state ? 'visible' : 'hidden'}`), 225);
+      } else {
+        link.setAttribute('style', `visibility: ${state ? 'visible' : 'hidden'}`);
+      }
+    });
+  }
+  changeState() {
+    const newState = !this.show;
+    this.setInnerLinkStates(newState);
+    this.show = newState;
+    if (newState) {
+      document.getElementById(`wrapper-${this.id}`).focus();
     }
   }
 }
