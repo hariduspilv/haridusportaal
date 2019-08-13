221fd9cd84e525065b98a11f281877360a7d3a5b
Madis Põld <madis.pold@twn.ee>
2019-08-13 14:14:03 +0300

    Dashboard helper texts #HP-171

--- angular2/src/app/_components/more.block/more.block.component.ts
+++ angular2/src/app/_components/more.block/more.block.component.ts
@@ -8,8 +8,19 @@ import { Component, Input, HostBinding } from '@angular/core';
 
 export class MoreBlockComponent {
   @Input() content: string;
+  @Input() id: string;
+  public cutoff: number = 66;
+  public active: boolean = false;
   public show: boolean = false;
   @HostBinding('class') get hostClasses(): string {
-    return `content__box ${this.show ? 'show' : 'hide'}`;
+    return `${this.content ? 'content__box' : 'empty'} ${this.show ? 'show' : 'hide'}`;
+  }
+  ngAfterViewInit() {
+    const contentElem = document.getElementById(`moreContent-${this.id}`);
+    if (contentElem && contentElem.clientHeight >= this.cutoff) {
+      this.active = true;
+    } else {
+      this.show = true;
+    }
   }
 }
