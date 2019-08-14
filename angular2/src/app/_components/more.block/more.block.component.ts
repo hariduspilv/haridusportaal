78c9b9a126326dc3896e1b5916a113862d9e8913
Madis Põld <madis.pold@twn.ee>
2019-08-14 13:08:26 +0300

    Debugging #HP-171

--- angular2/src/app/_components/more.block/more.block.component.ts
+++ angular2/src/app/_components/more.block/more.block.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, HostBinding } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'more-block',
@@ -9,7 +10,10 @@ import { Component, Input, HostBinding } from '@angular/core';
 export class MoreBlockComponent {
   @Input() content: string;
   @Input() id: string;
-  public cutoff: number = 66;
+  public cutoffs: Object = {
+    lg: 66,
+    sm: 110,
+  };
   public active: boolean = false;
   public show: boolean = false;
   public links: any;
@@ -17,12 +21,15 @@ export class MoreBlockComponent {
   @HostBinding('class') get hostClasses(): string {
     return `${this.content ? 'content__box' : 'empty'} ${this.show ? 'show' : 'hide'}`;
   }
+  constructor(private deviceDetector: DeviceDetectorService) {}
   ngAfterViewInit() {
     this.screenReaderContent = this.content ? this.content.replace(/<[^>]*>/g, '') : '';
     const contentElem = document.getElementById(`moreContent-${this.id}`);
     this.links = document.querySelectorAll(`#moreContent-${this.id} a`);
     this.setInnerLinkStates(this.show);
-    if (contentElem && contentElem.clientHeight >= this.cutoff) {
+    if (this.deviceDetector.isDesktop() && contentElem && contentElem.clientHeight >= this.cutoffs['lg']) {
+      this.active = true;
+    } else if (this.deviceDetector.isMobile() && contentElem && contentElem.clientHeight >= this.cutoffs['sm']) {
       this.active = true;
     } else {
       this.show = true;
