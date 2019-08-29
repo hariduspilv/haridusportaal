e2043224578716bcc22822d425a1467c36c6aecb
root <lars.soonvald@gmail.com>
2019-08-29 10:25:42 +0000

    old dump removed

--- web/modules/custom/htm_custom_study_programme_import/src/Controller/StudyProgrammeController.php
+++ web/modules/custom/htm_custom_study_programme_import/src/Controller/StudyProgrammeController.php
@@ -13,15 +13,6 @@ class StudyProgrammeController extends ControllerBase {
 
     public function import() {
 
-      $paragraphs = \Drupal::entityTypeManager()->getStorage('paragraph')->loadByProperties(['type'=>'iscedf_search']);
-      $i = 0;
-      foreach($paragraphs as $paragraph){
-        if($i <= 0){
-          $paragraph->delete();
-        }
-        $i++;
-      }
-
         $this->programmenodes = [];
         $schools = $this->get_existing_schools();
         $taxonomies['studyprogrammetype'] = $this->get_taxonomy_terms('studyprogrammetype');
