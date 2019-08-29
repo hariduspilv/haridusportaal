beae04f63014e8ec5da3d7a8dae9e068c36ea46f
lsoonvald <lars.soonvald@gmail.com>
2019-08-29 14:59:43 +0300

    added IBAN validation

--- angular2/src/app/_views/xjson/xjson.component.ts
+++ angular2/src/app/_views/xjson/xjson.component.ts
@@ -397,10 +397,10 @@ export class XjsonComponent implements OnInit, OnDestroy {
       return moment((String(date).split('.')).reverse().join('-'));
   }
 
-
   selectListCompare(a, b) {
     return a && b ? a === b : a === b;
   }
+
   isFieldDisabled(readonly): boolean {
     if (readonly === true) {
       return true;
@@ -707,6 +707,11 @@ export class XjsonComponent implements OnInit, OnDestroy {
       if (field.type === 'email') {
         const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (reg.test(field.value) === false) { return { valid: false, message: this.translate.get('xjson.enter_valid_email')['value'] }; }
+      }
+
+      if(field.type === 'iban'){
+        const reg = /^(?:(?:IT|SM)\d{2}[A-Z]\d{22}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})$/i;
+        if (reg.test(field.value) === false) { return { valid: false, message: this.translate.get('xjson.enter_valid_iban')['value'] }; }
       }
     }
     return { valid: true, message: 'valid' };
