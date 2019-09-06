a676ec82d84ea57674565fe64a34af40106b146d
root <lars.soonvald@gmail.com>
2019-09-06 09:21:05 +0000

    minor fix

--- web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
+++ web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
@@ -352,6 +352,7 @@ class xJsonService implements xJsonServiceInterface {
         $additional_keys = [];
         switch ($element_type) {
             case 'heading':
+            case 'iban':
             case 'helpertext':
                 $acceptable_keys = ['type', 'title'];
                 break;
