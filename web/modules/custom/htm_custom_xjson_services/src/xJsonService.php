16b6eb5f8a37a13550dc5916b16542ab4ecb5d5d
root <lars.soonvald@gmail.com>
2019-09-19 10:05:50 +0000

    testing xjson

--- web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
+++ web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
@@ -418,7 +418,7 @@ class xJsonService implements xJsonServiceInterface {
                         }
                     }
                 }
-                if(!$element['max_size'] || $element['max_size'] > ini_get('upload_max_filesize')){
+                if(!$table && (!$element['max_size'] || $element['max_size'] > ini_get('upload_max_filesize'))){
                     $element['max_size'] = intval(preg_replace('/\D/', '', ini_get('upload_max_filesize')));
                 }
                 break;
