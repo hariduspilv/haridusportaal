dfb8773ec93dca8bbbffbe67676e90db1a6fb95a
root <lars.soonvald@gmail.com>
2019-10-10 12:42:54 +0000

    added rest for form path

---
+++ web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonGetFormPath.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\htm_custom_xjson_services\Plugin\rest\resource;
+
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "x_json_form_path_rest_resource",
+ *   label = @Translation("X json form path rest resource"),
+ *   uri_paths = {
+ *     "canonical" = "/xjson_service/form_path/{form_name}"
+ *   }
+ * )
+ */
+
+class xJsonGetFormPath extends ResourceBase {
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new xJsonPathRestResource object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('htm_custom_xjson_services'),
+      $container->get('current_user')
+    );
+  }
+
+
+  /**
+   * @param $form_name
+   * @return ResourceResponse
+   */
+  public function get($form_name) {
+    // You must to implement the logic of your REST Resource here.
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+
+    $entityStorage = \Drupal::entityTypeManager()->getStorage('x_json_entity');
+    $connection = \Drupal::database();
+    $query = $connection->query("SELECT id FROM x_json_entity WHERE xjson_definition->'header'->>'form_name' = :id ", [':id' => $form_name]);
+    $result = $query->fetchField();
+    if ($result) {
+      $entity = $entityStorage->load($result);
+      return new ResourceResponse($entity->toUrl()->toString());
+    } else {
+      return new ModifiedResourceResponse('Path cannot be found', 400);
+    }
+  }
+}
