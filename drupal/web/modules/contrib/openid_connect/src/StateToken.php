55cf577081876e9cf33c60897d3da19ce779bda3
root <lars.soonvald@gmail.com>
2019-10-17 12:12:35 +0000

    testing harid

--- drupal/web/modules/contrib/openid_connect/src/StateToken.php
+++ drupal/web/modules/contrib/openid_connect/src/StateToken.php
@@ -20,6 +20,7 @@ class StateToken {
   public static function create() {
     $state = Crypt::randomBytesBase64();
     $_SESSION['openid_connect_state'] = $state;
+    \Drupal::logger('htm_custom_tara_authentication')->notice('created'.$state);
     return $state;
   }
 
