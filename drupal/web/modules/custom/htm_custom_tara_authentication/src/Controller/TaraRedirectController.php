c17b05a5f826497e45398a1efe0c371d1e3ee649
root <lars.soonvald@gmail.com>
2019-10-17 11:08:58 +0000

    testing harid

--- drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
+++ drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
@@ -40,8 +40,6 @@ class TaraRedirectController extends RedirectController{
 	}
 
   public function access() {
-	  dump('jou');
-	  die();
     // Confirm anti-forgery state token. This round-trip verification helps to
     // ensure that the user, not a malicious script, is making the request.
     $query = $this->requestStack->getCurrentRequest()->query;
@@ -53,8 +51,6 @@ class TaraRedirectController extends RedirectController{
   }
 
 	public function authenticate ($client_name) {
-	  dump('test');
-	  die();
 		header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 		header("Cache-Control: post-check=0, pre-check=0", false);
 		header("Pragma: no-cache");
