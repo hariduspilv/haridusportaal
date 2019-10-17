3f9d8dc3ea509f66d6001a1b48a17845a65b04d5
root <lars.soonvald@gmail.com>
2019-10-17 10:40:58 +0000

    testing harid

--- drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
+++ drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
@@ -53,6 +53,8 @@ class TaraRedirectController extends RedirectController{
   }
 
 	public function authenticate ($client_name) {
+	  dump('test');
+	  die();
 		header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 		header("Cache-Control: post-check=0, pre-check=0", false);
 		header("Pragma: no-cache");
