fd22f7473258a07ade4ff0d61834edcaa4eea0ec
root <lars.soonvald@gmail.com>
2019-10-17 10:09:37 +0000

    testing harid

--- web/modules/contrib/openid_connect/src/Controller/RedirectController.php
+++ web/modules/contrib/openid_connect/src/Controller/RedirectController.php
@@ -93,6 +93,9 @@ class RedirectController extends ControllerBase implements AccessInterface {
     if ($state_token && StateToken::confirm($state_token)) {
       return AccessResult::allowed();
     }
+    dump($state_token);
+    dump(StateToken::confirm($state_token));
+    die();
     return AccessResult::forbidden();
   }
 
