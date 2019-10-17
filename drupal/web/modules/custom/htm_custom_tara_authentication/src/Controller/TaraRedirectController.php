2d1f8980b1d21e2c0dd35f2a4b85a79b49a88b78
root <lars.soonvald@gmail.com>
2019-10-17 11:30:22 +0000

    testing harid

--- drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
+++ drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
@@ -47,7 +47,7 @@ class TaraRedirectController extends RedirectController{
     if ($state_token && StateToken::confirm($state_token)) {
       return AccessResult::allowed();
     }
-    return AccessResult::allowed();
+    return AccessResult::forbidden();
   }
 
 	public function authenticate ($client_name) {
