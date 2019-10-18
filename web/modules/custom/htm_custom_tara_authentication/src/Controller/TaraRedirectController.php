23aa0d486dd9db07c7ce8be27ebadfaef7a8a658
root <lars.soonvald@gmail.com>
2019-10-18 06:29:50 +0000

    testing harid

--- web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
+++ web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
@@ -45,7 +45,9 @@ class TaraRedirectController extends RedirectController{
     $query = $this->requestStack->getCurrentRequest();
     $state_token = $query->query->get('state');
     if(!isset($_SESSION['openid_connect_state'])){
-      return AccessResult::forbidden();
+      //return AccessResult::forbidden();
+      dump($query);
+      die();
     } else if ($state_token && StateToken::confirm($state_token)) {
         return AccessResult::allowed();
     }
