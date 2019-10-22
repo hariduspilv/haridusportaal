3e0df7ea3362bbf40d832fd3bfa39e1288cde3a0
lsoonvald <lars.soonvald@gmail.com>
2019-10-22 12:24:05 +0300

    settings service

--- angular-fe/src/app/_services/SettingsService.ts
+++ angular-fe/src/app/_services/SettingsService.ts
@@ -1,7 +1,8 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DOCUMENT } from '@angular/common';
-​
+import { Subject } from 'rxjs';
+
 @Injectable()
 export class SettingsService {
   constructor(
@@ -14,7 +15,7 @@ export class SettingsService {
       this.url = this.urlTemplates.otherwise;
     }
   }
-​
+
   public url: string = '';
   private urlTemplates = {
     '192.168.6.193': 'https://htm.wiseman.ee',
@@ -27,12 +28,13 @@ export class SettingsService {
     localhost: 'https://htm.wiseman.ee',
     otherwise: 'https://api.hp.edu.ee',
   };
-​
+
   public login = '/api/v1/token?_format=json';
   public mobileLogin = '/custom/login/mobile_id?_format=json';
   public error: boolean = false;
   public data: any;
-​
+  public compareObservable = new Subject<any>();
+
   private findObj(obj, path) {
     return path
     .replace(/\[|\]\.?/g, '.')
@@ -40,7 +42,7 @@ export class SettingsService {
     .filter(s => s)
     .reduce((acc, val) => acc && acc[val], obj);
   }
-​
+
   public query(name: string = '', variables: object = {}) {
     const requestName = this.get(`request.${name}`);
     let path = `${this.url}/graphql?queryName=${name}&queryId=${requestName}`;
@@ -49,7 +51,7 @@ export class SettingsService {
     }
     return path;
   }
-​
+
   public get(key:string = '') {
     this.findObj(this.data, key);
     let output = this.findObj(this.data, key) || undefined;
@@ -58,7 +60,7 @@ export class SettingsService {
     }
     return output;
   }
-​
+
   public load() {
     return new Promise((resolve, reject) => {
       const path = `${this.url}/variables?_format=json&lang=et`;
