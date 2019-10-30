e8e005861250adc5948312014e25bf05619b8e01
root <lars.soonvald@gmail.com>
2019-10-30 17:19:17 +0000

    value formatting

--- web/modules/custom/htm_custom_infograph/src/Plugin/GraphQL/Fields/InfographQuery/InfographQuery.php
+++ web/modules/custom/htm_custom_infograph/src/Plugin/GraphQL/Fields/InfographQuery/InfographQuery.php
@@ -25,214 +25,219 @@ use League\Csv\Statement;
  */
 class InfographQuery extends FieldPluginBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
+  /**
+   * {@inheritdoc}
+   */
+  public function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
 
-        $value['ChartValue'] = $this->getChartValue($args);
+    $value['ChartValue'] = $this->getChartValue($args);
 
-        yield $value;
+    yield $value;
+  }
+
+  /**
+   * Retrieve the list of cache dependencies for a given value and arguments.
+   *
+   * @param array $result
+   *   The result of the field.
+   * @param mixed $parent
+   *   The parent value.
+   * @param array $args
+   *   The arguments passed to the field.
+   * @param \Drupal\graphql\GraphQL\Execution\ResolveContext $context
+   *   The resolve context.
+   * @param \GraphQL\Type\Definition\ResolveInfo $info
+   *   The resolve info object.
+   *
+   * @return array
+   *   A list of cacheable dependencies.
+   */
+  protected function getCacheDependencies(array $result, $parent, array $args, ResolveContext $context, ResolveInfo $info) {
+    $cache = parent::getCacheDependencies($result, $parent, $args, $context, $info);
+
+    $cache[0]->setCacheTags([$args['filters']['file'].'_csv']);
+
+    return $cache;
+  }
+
+  public function getChartValue($args){
+
+    $graph_info = $args['filters'];
+
+    $filter_values = $graph_info;
+    $graph_info_fields = [
+      'file',
+      'graph_set',
+      'graph_type',
+      'secondary_graph_type',
+      'graph_group_by',
+      'graph_v_axis'
+    ];
+
+    foreach($graph_info as $key => $value){
+      if(!in_array($key, $graph_info_fields)){
+        $filter_values[$key] = $value;
+        unset($graph_info[$key]);
+      }else{
+        unset($filter_values[$key]);
+      }
     }
 
-    /**
-     * Retrieve the list of cache dependencies for a given value and arguments.
-     *
-     * @param array $result
-     *   The result of the field.
-     * @param mixed $parent
-     *   The parent value.
-     * @param array $args
-     *   The arguments passed to the field.
-     * @param \Drupal\graphql\GraphQL\Execution\ResolveContext $context
-     *   The resolve context.
-     * @param \GraphQL\Type\Definition\ResolveInfo $info
-     *   The resolve info object.
-     *
-     * @return array
-     *   A list of cacheable dependencies.
-     */
-    protected function getCacheDependencies(array $result, $parent, array $args, ResolveContext $context, ResolveInfo $info) {
-        $cache = parent::getCacheDependencies($result, $parent, $args, $context, $info);
-
-        $cache[0]->setCacheTags([$args['filters']['file'].'_csv']);
-
-        return $cache;
+    if(isset($filter_values['secondary_graph_indicator'])){
+      foreach($filter_values['secondary_graph_indicator'] as $value){
+        $filter_values['indicator'][] = $value;
+      }
+      unset($filter_values['secondary_graph_indicator']);
     }
 
-    public function getChartValue($args){
+    $filter_values['naitaja'] = $filter_values['indicator'];
+    unset($filter_values['indicator']);
 
-        $graph_info = $args['filters'];
+    $this->filter_values = $filter_values;
 
-        $filter_values = $graph_info;
-        $graph_info_fields = [
-            'file',
-            'graph_set',
-            'graph_type',
-            'secondary_graph_type',
-            'graph_group_by',
-            'graph_v_axis'
-        ];
+    $reader = Reader::createFromPath('/app/drupal/web/sites/default/files/private/infograph/'.$graph_info['file'].'.csv', 'r');
+    $reader->setDelimiter(';');
+    $reader->setHeaderOffset(0);
+    $stmt = (new Statement())
+      ->where([$this,'applyFilters']);
 
-        foreach($graph_info as $key => $value){
-            if(!in_array($key, $graph_info_fields)){
-                $filter_values[$key] = $value;
-                unset($graph_info[$key]);
-            }else{
-                unset($filter_values[$key]);
-            }
-        }
+    $records = iterator_to_array($stmt->process($reader), false);
 
-        if(isset($filter_values['secondary_graph_indicator'])){
-            foreach($filter_values['secondary_graph_indicator'] as $value){
-                $filter_values['indicator'][] = $value;
-            }
-            unset($filter_values['secondary_graph_indicator']);
-        }
+    if(isset($records) && count($records) > 0){
+      $graph_value = $this->formResponse($records, $graph_info, $filter_values);
 
-        $filter_values['naitaja'] = $filter_values['indicator'];
-        unset($filter_values['indicator']);
+      return $graph_value;
+    }else{
+      return NULL;
+    }
+  }
 
-        $this->filter_values = $filter_values;
+  public function formResponse($records, $graph_info, $filter_values){
 
-        $reader = Reader::createFromPath('/app/drupal/web/sites/default/files/private/infograph/'.$graph_info['file'].'.csv', 'r');
-        $reader->setDelimiter(';');
-        $reader->setHeaderOffset(0);
-        $stmt = (new Statement())
-            ->where([$this,'applyFilters']);
+    $data_array = NULL;
+    $graph_data = $graph_info;
 
-        $records = iterator_to_array($stmt->process($reader), false);
+    foreach($graph_data as $key => $type){
+      if($type == ''){
+        unset($graph_data[$key]);
+      }
+    }
 
-        if(isset($records) && count($records) > 0){
-            $graph_value = $this->formResponse($records, $graph_info, $filter_values);
+    #find label and value fields
+    $label_field = $graph_data['graph_v_axis'];
+    $value_field = 'vaartus';
 
-            return $graph_value;
+    $indicator_field = $graph_data['graph_group_by'];
+
+    if($label_field && $value_field){
+      $labelsums = [];
+      $xlabels = [];
+
+      #get value for each label, sum reoccurring labels
+      foreach($records as $record){
+
+        if(isset($record[$label_field]) && $record[$label_field] != ''){
+          $xlabel = $record[$label_field];
         }else{
-            return NULL;
+          continue;
         }
+
+        $ylabel = (string)t($label_field);
+        $val = $record[$value_field];
+
+        if(isset($record[$indicator_field]) && $record[$indicator_field] != ''){
+          $value_label = $record[$indicator_field];
+        }else{
+          continue;
+        }
+
+        $labelsums[$ylabel][$xlabel] = $xlabel;
+        if(!isset($labelsums[$value_label][$xlabel])){
+          $labelsums[$value_label][$xlabel] = empty($val) ? null : floatval(str_replace(",",".", $val));
+        }else{
+          $labelsums[$value_label][$xlabel] += empty($val) ? null : floatval(str_replace(",",".", $val));
+        }
+        if(!in_array($xlabel, $xlabels)){
+          $xlabels[] = $xlabel;
+        }
+      }
+
+      if($indicator_field == 'naitaja'){
+        #sort data array by indicators
+        #add first fixed row to new array
+        $new_labelsums[key($labelsums)] = reset($labelsums);
+
+        #get correct key order by indicator
+        $key_order = $filter_values[$indicator_field];
+
+        #put values in new order to the array
+        foreach($key_order as $value){
+          $new_labelsums[$value] = $labelsums[$value];
+        }
+
+        $labelsums = $new_labelsums;
+      }
+
+      #add values to empty fields
+      if(count($xlabels) > 0){
+        foreach($xlabels as $label){
+          $labelsums = $this->fillEmptyFields($labelsums, $label);
+        }
+        #add labels for chart
+        foreach($labelsums as $label => $value){
+          $data_array[0][] = (string)$label;
+          foreach($value as $key => $val){
+            if(is_string($val)){
+              $val = preg_replace('/\s+/', '', $val);
+            } else {
+              $val = round($val, 2);
+            }
+            $data_array[$key][] = $val;
+          }
+        }
+        $data_array = array_values($data_array);
+      }
     }
 
-    public function formResponse($records, $graph_info, $filter_values){
+    return $data_array != NULL ? json_encode($data_array, TRUE) : NULL;
+  }
 
-        $data_array = NULL;
-        $graph_data = $graph_info;
-
-        foreach($graph_data as $key => $type){
-            if($type == ''){
-                unset($graph_data[$key]);
-            }
-        }
-
-        #find label and value fields
-        $label_field = $graph_data['graph_v_axis'];
-        $value_field = 'vaartus';
-
-        $indicator_field = $graph_data['graph_group_by'];
-
-        if($label_field && $value_field){
-            $labelsums = [];
-            $xlabels = [];
-
-            #get value for each label, sum reoccurring labels
-            foreach($records as $record){
-
-                if(isset($record[$label_field]) && $record[$label_field] != ''){
-                    $xlabel = $record[$label_field];
-                }else{
-                    continue;
-                }
-
-                $ylabel = (string)t($label_field);
-                $val = $record[$value_field];
-
-                if(isset($record[$indicator_field]) && $record[$indicator_field] != ''){
-                    $value_label = $record[$indicator_field];
-                }else{
-                    continue;
-                }
-
-                $labelsums[$ylabel][$xlabel] = $xlabel;
-                if(!isset($labelsums[$value_label][$xlabel])){
-                    $labelsums[$value_label][$xlabel] = empty($val) ? null : floatval(str_replace(",",".", $val));
-                }else{
-                    $labelsums[$value_label][$xlabel] += empty($val) ? null : floatval(str_replace(",",".", $val));
-                }
-                if(!in_array($xlabel, $xlabels)){
-                    $xlabels[] = $xlabel;
-                }
-            }
-
-            if($indicator_field == 'naitaja'){
-                #sort data array by indicators
-                #add first fixed row to new array
-                $new_labelsums[key($labelsums)] = reset($labelsums);
-
-                #get correct key order by indicator
-                $key_order = $filter_values[$indicator_field];
-
-                #put values in new order to the array
-                foreach($key_order as $value){
-                    $new_labelsums[$value] = $labelsums[$value];
-                }
-
-                $labelsums = $new_labelsums;
-            }
-
-            #add values to empty fields
-            if(count($xlabels) > 0){
-                foreach($xlabels as $label){
-                    $labelsums = $this->fillEmptyFields($labelsums, $label);
-                }
-                #add labels for chart
-                foreach($labelsums as $label => $value){
-                    $data_array[0][] = (string)$label;
-                    foreach($value as $key => $val){
-                        $data_array[$key][] = is_string($val) || $val === null ? $val : round($val, 2);
-                    }
-                }
-                $data_array = array_values($data_array);
-            }
-        }
-
-        return $data_array != NULL ? json_encode($data_array, TRUE) : NULL;
+  public function fillEmptyFields($labelsums, $xlabelval){
+    foreach($labelsums as $key => $label){
+      if(!isset($label[$xlabelval])){
+        $labelsums[$key][$xlabelval] = json_decode('null');
+      }
     }
 
-    public function fillEmptyFields($labelsums, $xlabelval){
-        foreach($labelsums as $key => $label){
-            if(!isset($label[$xlabelval])){
-                $labelsums[$key][$xlabelval] = json_decode('null');
-            }
-        }
+    return $labelsums;
+  }
 
-        return $labelsums;
+
+  public function applyFilters($row){
+
+    foreach($this->filter_values as $key => $filters){
+      $match = true;
+
+      if(isset($row[$key])){
+        if(is_array($filters) && count($filters) > 0){
+          if(!in_array($row[$key], $filters)){
+            $match = false;
+            break;
+          }
+        }else if(count($filters) == 0){
+          $match = true;
+        }else{
+          if(strpos($row[$key], $filters) == FALSE){
+            $match = false;
+            break;
+          }
+        }
+      }else{
+        $match = false;
+        break;
+      }
     }
 
-
-    public function applyFilters($row){
-
-        foreach($this->filter_values as $key => $filters){
-            $match = true;
-
-            if(isset($row[$key])){
-                if(is_array($filters) && count($filters) > 0){
-                    if(!in_array($row[$key], $filters)){
-                        $match = false;
-                        break;
-                    }
-                }else if(count($filters) == 0){
-                    $match = true;
-                }else{
-                    if(strpos($row[$key], $filters) == FALSE){
-                        $match = false;
-                        break;
-                    }
-                }
-            }else{
-                $match = false;
-                break;
-            }
-        }
-
-        return $match;
-    }
+    return $match;
+  }
 }
