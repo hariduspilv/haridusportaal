693cf6847e5fd04254a579cc12025a30f49d3112
root <lars.soonvald@gmail.com>
2019-11-03 17:26:06 +0000

    testing xjson

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -117,9 +117,7 @@ class EhisConnectorService {
       /*TODO make post URL configurable*/
       if($type === 'get'){
         if($service_name === 'getDocument'){
-          dump($this->loime_url.$service_name . '/' . $params['form_name'].'/'.$params['idcode'].'/'. implode($params['url'], '/'));
-          die();
-          $response = $client->get($this->loime_url.$service_name . '/' . $params['form_name'].'/'.$params['idcode'].'/'. implode($params['url'], '/'));
+          $response = $client->get($this->loime_url.$service_name . '/' . $params['form_name'].'/'.$params['idcode'].'?'. implode($params['url'], '&'));
         } else {
           $response = $client->get($this->loime_url.$service_name . '/' . implode($params['url'], '/'));
         }
