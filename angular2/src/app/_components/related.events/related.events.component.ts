50bc5691c6862c90845557e311cf7ccc0d89cba2
Madis Põld <madis.pold@twn.ee>
2019-11-04 10:56:27 +0200

    Related events refactor and sort

--- angular2/src/app/_components/related.events/related.events.component.ts
+++ angular2/src/app/_components/related.events/related.events.component.ts
@@ -1,55 +1,39 @@
 import { Component, OnDestroy, ViewChild, Input, OnInit } from '@angular/core';
 import { RootScopeService } from '@app/_services/rootScopeService';
-import { Subscription } from 'rxjs/Subscription'; 
+import { Subscription } from 'rxjs/Subscription';
 import { HttpService } from '@app/_services/httpService';
 
 @Component({
-	selector: 'related-events',
-	templateUrl: './related.events.component.html',
-	styleUrls: ['./related.events.component.scss']
+  selector: 'related-events',
+  templateUrl: './related.events.component.html',
+  styleUrls: ['./related.events.component.scss']
 })
 
 export class RelatedEventsComponent implements OnInit {
 
-	@Input() groupID: number;
-	@Input() nid: any;
+  @Input() groupID: number;
+  @Input() nid: any;
+  public content: object[];
+  public lang: string;
 
-	error: boolean;
-	content: any;
+  constructor(
+    private rootScope: RootScopeService,
+    private http: HttpService
+  ) {}
 
-	lang: any;
-
-	constructor(
-		private rootScope: RootScopeService,
-		private http: HttpService
-	) {}
-
-	ngOnInit() {
-
-		let that = this;
-
-		this.lang = this.rootScope.get('lang');
-
-		let variables = {
-			groupID: this.groupID.toString(),
-			nid: this.nid.toString(),
-			lang: this.lang.toUpperCase()
-		};
-
-		let subscription = this.http.get('getRelatedEvents', { params: variables }).subscribe((response) => {
-			
-			let data = response['data'];
-
-			if ( data['nodeQuery'] == null ) {
-				that.error = true;
-			} else {
-				that.content = data['nodeQuery']['entities'];
-			}
-
-			subscription.unsubscribe();
-		});
-
-	}
-
+  ngOnInit() {
+    const variables = {
+      groupID: this.groupID.toString(),
+      nid: this.nid.toString(),
+      lang: this.rootScope.get('lang').toUpperCase(),
+    };
+    const subscription = this.http.get('getRelatedEvents', { params: variables }).subscribe((response) => {
+      const data = response['data'];
+      if (data['nodeQuery']) {
+        this.content = data['nodeQuery']['entities'].sort((a: object, b: object) => (a['fieldEventMainDate']['date'] > b['fieldEventMainDate']['date'] ? -1 : 1));
+      }
+      subscription.unsubscribe();
+    });
+  }
 }
 
