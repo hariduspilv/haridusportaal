f7242dc8a86b6999fd32250882807f66efe21464
root <lars.soonvald@gmail.com>
2019-11-04 10:01:50 +0000

    testing xjson

--- web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
+++ web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
@@ -434,7 +434,6 @@ class xJsonService implements xJsonServiceInterface {
         if (isset($element['options_list'])) {
           $params['hash'] = $element['options_list'];
           $element['options'] = $this->ehisconnector->getOptionsTaxonomy($params);
-          dump($element);
         }
 
         $recfunc = function ($options, $keys = []) use (&$recfunc) {
