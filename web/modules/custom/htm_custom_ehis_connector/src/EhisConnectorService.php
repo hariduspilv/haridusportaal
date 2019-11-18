62e12a178a412f626d4538c817ba40241b4936da
root <lars.soonvald@gmail.com>
2019-11-18 09:46:11 +0000

    testing xjson

--- web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -366,12 +366,12 @@ class EhisConnectorService {
   public function getOptionsTaxonomy(array $params = []){
     $params['key'] = 'klassifikaator';
     $return = $this->invokeWithRedis('mtsysKlfTeenus', $params, FALSE);
+    dump($return);
+    die();
     if(!$return['redis_hit']){
       return (isset($return[$params['hash']])) ? $return[$params['hash']] : [];
     }
     unset($return['redis_hit']);
-    dump($return);
-    die();
     return $return;
   }
 
