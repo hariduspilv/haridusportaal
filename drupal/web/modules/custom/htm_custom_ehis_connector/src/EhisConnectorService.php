e1f07c5d7a90b6e44628e72858f786d68320d190
root <lars.soonvald@gmail.com>
2019-11-18 19:04:59 +0000

    removed dumps

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -366,8 +366,6 @@ class EhisConnectorService {
   public function getOptionsTaxonomy(array $params = []){
     $params['key'] = 'klassifikaator';
     $return = $this->invokeWithRedis('mtsysKlfTeenus', $params, FALSE);
-    dump($return);
-    die();
     if(!$return['redis_hit']){
       return (isset($return[$params['hash']])) ? $return[$params['hash']] : [];
     }
