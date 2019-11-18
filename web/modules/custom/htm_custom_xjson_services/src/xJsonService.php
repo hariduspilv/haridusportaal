a807ba9bf1d5999e68f4d76aa14fc638e565f48b
root <lars.soonvald@gmail.com>
2019-11-18 19:28:43 +0000

    testing xjson

--- web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
+++ web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
@@ -441,8 +441,6 @@ class xJsonService implements xJsonServiceInterface {
               return $recfunc($option['options'], $keys);
             }
           }
-          dump($keys);
-          die();
           return $keys;
         };
 
