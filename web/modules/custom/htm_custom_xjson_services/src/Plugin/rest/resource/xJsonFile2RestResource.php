c297cef84df57e37e22df84be41684f62c7fabf6
root <lars.soonvald@gmail.com>
2019-11-19 14:36:50 +0000

    testing xjson

--- web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonFile2RestResource.php
+++ web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonFile2RestResource.php
@@ -144,9 +144,6 @@ class xJsonFile2RestResource extends ResourceBase {
         $file_obj['value'] = $this->ehisService->getDocumentFileFromRedis($params);
         $file_obj['fileName'] = $file_name;
 
-        dump($file_obj);
-        die();
-
         if(!$file_obj['value']){
             $file_obj = $this->ehisService->getDocumentFile(['file_id' => $file_id]);
         }
