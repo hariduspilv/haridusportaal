f9f67921895f0374968964a5a6d19701309ddff7
lsoonvald <lars.soonvald@gmail.com>
2019-11-25 22:59:24 +0200

    pictos

---
+++ angular2/src/app/_components/picto/helpers/picto.ts
@@ -0,0 +1,143 @@
+export const pictoDesigns = [
+  {
+    circles: [
+      {
+        right: -7.5,
+        top: 1.5,
+        size: 5,
+        z: 3,
+        color: '#ffe7c1',
+      },
+      {
+        right: -9,
+        top: 3,
+        size: 4,
+        z: 4,
+        color: '#9dd6e4',
+      },
+    ],
+    triangles: [],
+  },
+  {
+    circles: [
+      {
+        right: -6.5,
+        top: 3,
+        size: 4,
+        z: 3,
+        color: '#ffe7c1',
+      },
+      {
+        right: -9,
+        top: 1,
+        size: 3.5,
+        z: 4,
+        color: '#9dd6e4',
+      },
+      {
+        right: -9.2,
+        top: 4.5,
+        size: 3,
+        z: 3,
+        color: '#e7d6d8',
+      },
+    ],
+    triangles: [],
+  },
+  {
+    circles: [
+      {
+        right: -9.5,
+        top: 2.5,
+        size: 5,
+        z: 4,
+        color: '#ffe7c1',
+      },
+      {
+        right: -7,
+        top: 2,
+        size: 4,
+        z: 3,
+        color: '#9dd6e4',
+      },
+    ],
+    triangles: [],
+  },
+  {
+    circles: [
+      {
+        right: -8,
+        top: 2,
+        size: 5,
+        z: 3,
+        color: '#e7d6d8',
+      },
+    ],
+    triangles: [
+      {
+        width: 4,
+        height: 6,
+        z: 4,
+        right: -9,
+        top: 1.5,
+        color: 'transparent #9dd6e4 transparent transparent',
+        rotate: 0,
+      },
+    ],
+  },
+  {
+    circles: [
+      {
+        right: -5,
+        top: 1.5,
+        size: 2.5,
+        z: 3,
+        color: '#9dd6e4',
+      },
+      {
+        right: -7.5,
+        top: 3,
+        size: 4.5,
+        z: 3,
+        color: '#ffe7c1',
+      },
+      {
+        right: -9,
+        top: 1.5,
+        size: 3.5,
+        z: 2,
+        color: '#e7d6d8',
+      },
+    ],
+    triangles: [],
+  },
+  {
+    circles: [
+      {
+        right: -8,
+        top: 2,
+        size: 4.5,
+        z: 3,
+        color: '#9dd6e4',
+      },
+      {
+        right: -9.7,
+        top: 4.8,
+        size: 3,
+        z: 3,
+        color: '#e7d6d8',
+      },
+    ],
+    triangles: [
+      {
+        width: 3.5,
+        height: 5,
+        z: 2,
+        right: -5.5,
+        top: 2.5,
+        color: 'transparent #ffe7c1 transparent transparent',
+        rotate: 180,
+      },
+    ],
+  },
+];
