f132ce7e8027a294701ee2b230131f4523f0ec67
root <lars.soonvald@gmail.com>
2019-12-30 08:02:25 +0000

    testing xjson

--- drupal/web/modules/custom/htm_custom_ehis_connector/modules/htm_custom_professional_certific/src/Plugin/rest/resource/EducationalFormRestResource.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/modules/htm_custom_professional_certific/src/Plugin/rest/resource/EducationalFormRestResource.php
@@ -124,8 +124,7 @@ class EducationalFormRestResource extends ResourceBase {
 		  case 'edit':
 			  if($validation[0]){
 			  	$response = $this->ehisConnector->editInstitution(['data' => $data]);
-          \Drupal::logger('xjson')->notice('<pre><code>response' . print_r($response, TRUE) . '</code></pre>' );
-			  	if($response['edId']){
+			  	if(isset($response['message'])){
             $this->ehisConnector->deleteKeyFromredis($this->ehisConnector->getCurrentUserIdRegCode(FALSE));
           }
 				  return new ModifiedResourceResponse($response);
