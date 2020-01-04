d1d29fb6d95c762a47b46a54c0143979d3a2804d
root <lars.soonvald@gmail.com>
2020-01-04 02:04:31 +0000

    testing jwt

--- web/modules/contrib/key/src/Entity/Key.php
+++ web/modules/contrib/key/src/Entity/Key.php
@@ -260,11 +260,12 @@ class Key extends ConfigEntityBase implements KeyInterface, EntityWithPluginColl
     $key_id = $this->id();
     $key_values = &drupal_static(__FUNCTION__);
 
+    dump($this->getKeyValues());
+
     // If the key value has not already been retrieved during this page
     // request or if the static variable storage needs to be reset for
     // this key, retrieve the value using the key provider.
     if (!isset($key_values[$key_id]) || $reset) {
-      dump($this->getKeyProvider());
       $key_values[$key_id] = $this->getKeyProvider()->getKeyValue($this);
     }
 
