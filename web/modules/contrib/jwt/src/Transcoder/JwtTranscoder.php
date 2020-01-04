e7d937714dde9f5996cb41fc50a4e0a28bd7deff
root <lars.soonvald@gmail.com>
2020-01-04 02:46:03 +0000

    testing jwt

--- web/modules/contrib/jwt/src/Transcoder/JwtTranscoder.php
+++ web/modules/contrib/jwt/src/Transcoder/JwtTranscoder.php
@@ -110,6 +110,7 @@ class JwtTranscoder implements JwtTranscoderInterface {
         } else {
           $key_value = $key->getKeyValue();
         }
+        dump($key_value);
         if ($this->algorithmType == 'jwt_hs') {
           // Symmetric algorithm so we set the secret.
           $this->setSecret($key_value);
