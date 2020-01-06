62816f2fb59180fb5c9388c44c1d668a53ccfd96
root <lars.soonvald@gmail.com>
2020-01-06 11:51:43 +0000

    testing json

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/Plugin/rest/resource/EhisJwt.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/Plugin/rest/resource/EhisJwt.php
@@ -94,10 +94,7 @@ class EhisJwt extends ResourceBase {
 
     if(isset($data['jwt'])) {
       $client = \Drupal::httpClient();
-      $params['headers'] = [
-        'Content-Type' => 'application/json'
-      ];
-      $params['form_params'] = [
+      $params['json'] = [
         'jwt' => $data['jwt']
       ];
       try {
