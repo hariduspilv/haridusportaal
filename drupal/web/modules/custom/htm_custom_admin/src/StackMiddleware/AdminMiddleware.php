2079a2cc6b353e8b5d1f43f32b3b55087976740e
root <lars.soonvald@gmail.com>
2020-02-12 11:53:16 +0000

    logging for testing

--- drupal/web/modules/custom/htm_custom_admin/src/StackMiddleware/AdminMiddleware.php
+++ drupal/web/modules/custom/htm_custom_admin/src/StackMiddleware/AdminMiddleware.php
@@ -37,6 +37,8 @@ class AdminMiddleware implements HttpKernelInterface {
       '/user'
     ];
 
+    \Drupal::logger('middleware')->notice('<pre><code>test ' . print_r($request, TRUE) . '</code></pre>' );
+
     if(!in_array($request->getPathInfo(), $safePaths) && \Drupal::currentUser()->isAnonymous()){
       $fe_url = \Drupal::config('htm_custom_admin_form.customadmin')->get('general.fe_url');
       $response = new RedirectResponse($fe_url);
