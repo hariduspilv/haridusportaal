a8e47d2895fe659e1542c94417b349323e3d8d11
root <lars.soonvald@gmail.com>
2020-02-27 07:40:36 +0000

    dump

--- web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
+++ web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
@@ -145,6 +145,7 @@ class xJsonRestResource extends ResourceBase {
     }
     $params['form_name'] = $data['form_name'];
     $response = $this->ehisService->getDocument($params);
+    dump($response);
     \Drupal::logger('xjson')->notice('<pre><code>ehis response ' . print_r($response, TRUE) . '</code></pre>' );
     $response['header'] += [
       'endpoint' => 'empty'
