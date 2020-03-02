c85bf33059ca66252efc7efa1f5bd91ab1476e3e
root <lars.soonvald@gmail.com>
2020-03-02 09:22:54 +0000

    get form action correctly

--- web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
+++ web/modules/custom/htm_custom_xjson_services/src/Plugin/rest/resource/xJsonRestResource.php
@@ -199,7 +199,9 @@ class xJsonRestResource extends ResourceBase {
   private function checkxJsonForm ($data) {
     $path = $data['form_name'];
     if(isset($data['form_info'])){
-      $this->formAction = $data['form_info']['header']['activity'] || $data['activity'];
+      $this->formAction = $data['form_info']['header']['activity'];
+    } else if(isset($data['activity'])){
+      $this->formAction = $data['activity'];
     }
 
     $path = \Drupal::service('path.alias_manager')->getPathByAlias($path);
