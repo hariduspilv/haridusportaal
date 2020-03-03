e1e9df63746970e31241b457368bec7770509d94
Ragnar <raxthepro1@gmail.com>
2020-03-03 17:45:32 +0200

    HP-264 Add WCAG compatibility to Sidemenu, Favourites, Header/Login modal components.

--- angular-fe/src/app/_assets/header/header.component.ts
+++ angular-fe/src/app/_assets/header/header.component.ts
@@ -1,235 +1,236 @@
-import { Component, OnInit, HostBinding, Input, ChangeDetectorRef } from '@angular/core';
+import {ChangeDetectorRef, Component, HostBinding, Input, OnInit} from '@angular/core';
 import {
-  SidemenuService,
-  ModalService,
-  AuthService,
-  SettingsService,
-  AlertsService,
-  AnalyticsService,
+	AlertsService,
+	AnalyticsService,
+	AuthService,
+	ModalService,
+	SettingsService,
+	SidemenuService,
 } from '@app/_services';
-import { TranslateService } from '@app/_modules/translate/translate.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
-import { Router, ActivatedRoute } from '@angular/router';
+import {TranslateService} from '@app/_modules/translate/translate.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Subscription} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: 'htm-header',
-  templateUrl: './header.template.html',
-  styleUrls: ['./header.styles.scss'],
+	selector: 'htm-header',
+	templateUrl: './header.template.html',
+	styleUrls: ['./header.styles.scss'],
 })
 
 export class HeaderComponent implements OnInit {
-  public active: boolean;
-  @Input() loginStatus: boolean = false;
-  @Input() user: string = '';
-  @HostBinding('class') get hostClasses(): string {
-    return 'header';
-  }
-  @HostBinding('attr.aria-label') ariaLabel: string = this.translate.get('frontpage.header');
-  @HostBinding('attr.role') role: string = 'banner';
-  public searchTerm: any;
-  public logoutActive = false;
+	public active: boolean;
+	@Input() loginStatus: boolean = false;
+	@Input() user: string = '';
+	@HostBinding('attr.aria-label') ariaLabel: string = this.translate.get('frontpage.header');
+	@HostBinding('attr.role') role: string = 'banner';
+	public searchTerm: any;
+	public logoutActive = false;
+	public searchString = '';
+	public loading = false;
+	public mobileId = {
+		challengeId: '',
+	};
+	public authMethods = {
+		harid: false,
+		tara: false,
+		mobile_id: false,
+		basic: false,
+	};
+	public availableAuthMethods = [];
+	public mobileIdRequest: Subscription = new Subscription();
+	public offClickHandler: any;
+	public loginForm: FormGroup = this.formBuilder.group({
+		password: ['', Validators.required],
+		username: ['', Validators.required],
+	});
+	public mobileIdForm: FormGroup = this.formBuilder.group({
+		phoneNumber: ['', Validators.required],
+	});
+	public search: string = '';
 
-  public searchString = '';
+	constructor(
+		public sidemenuService: SidemenuService,
+		public modalService: ModalService,
+		private translate: TranslateService,
+		private formBuilder: FormBuilder,
+		public auth: AuthService,
+		private settings: SettingsService,
+		private http: HttpClient,
+		private alertsService: AlertsService,
+		private router: Router,
+		private activatedRoute: ActivatedRoute,
+		private cdr: ChangeDetectorRef,
+		private analytics: AnalyticsService,
+	) {
+	}
 
-  public loading = false;
-  public mobileId = {
-    challengeId: '',
-  };
+	@HostBinding('class') get hostClasses(): string {
+		return 'header';
+	}
 
-  public authMethods = {
-    harid: false,
-    tara: false,
-    mobile_id: false,
-    basic: false,
-  };
+	public basicLogin(): void {
+		this.alertsService.clear('login-modal');
+		const data = {...this.loginForm.value, auth_method: 'basic'};
+		const subscription = this.auth.login(data).subscribe((response) => {
+			this.modalService.close('login');
+			subscription.unsubscribe();
+		});
+	}
 
-  public availableAuthMethods = [];
+	isNumber(e) {
+		return e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 9 ? true : !isNaN(Number(e.key));
+	}
 
-  public mobileIdRequest: Subscription = new Subscription();
-  public offClickHandler: any;
-  public loginForm: FormGroup = this.formBuilder.group({
-    password: ['', Validators.required],
-    username: ['', Validators.required],
-  });
+	public taraLogin(): void {
+		window.location.href = `${this.settings.url}/external-login/tara`;
+	}
 
-  public mobileIdForm: FormGroup = this.formBuilder.group({
-    phoneNumber: ['', Validators.required],
-  });
+	public harIdLogin(): void {
+		window.location.href = `${this.settings.url}/external-login/harid`;
+	}
 
-  constructor(
-    public sidemenuService: SidemenuService,
-    public modalService: ModalService,
-    private translate: TranslateService,
-    private formBuilder: FormBuilder,
-    public auth: AuthService,
-    private settings: SettingsService,
-    private http: HttpClient,
-    private alertsService: AlertsService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private cdr: ChangeDetectorRef,
-    private analytics: AnalyticsService,
-  ) { }
+	// needs to be finished
+	public mobileIdLogin(): void {
+		this.alertsService.clear('login-modal');
+		this.loading = true;
+		this.http.post(
+			`${this.settings.url}${this.settings.mobileLogin}`,
+			{telno: this.mobileIdForm.controls.phoneNumber.value},
+		).subscribe(
+			(data: any) => {
+				this.loading = false;
+				this.mobileId.challengeId = data.ChallengeID;
+				const consecutiveForm = {
+					session_code: data.Sesscode,
+					id_code: data.UserIDCode,
+					auth_method: 'mobile_id',
+				};
+				this.mobileIdRequest = this.auth.login(consecutiveForm)
+					.subscribe(
+						(response) => {
+							this.modalService.close('login');
+							this.mobileId.challengeId = '';
+						},
+						(err) => {
+							this.alertsService
+								.error(this.translate.get(err.error.message), 'login-modal', false, true);
+							this.mobileId.challengeId = '';
+						},
+						() => {
+						});
+			},
+			(err: any) => {
+			},
+			() => {
+				this.loading = false;
+			},
+		);
+	}
 
-  public basicLogin(): void {
-    this.alertsService.clear('login-modal');
-    const data = { ...this.loginForm.value, auth_method: 'basic' };
-    const subscription = this.auth.login(data).subscribe((response) => {
-      this.modalService.close('login');
-      subscription.unsubscribe();
-    });
-  }
+	toggleSidemenu(): void {
+		this.sidemenuService.toggle();
+		this.active = this.sidemenuService.isVisible;
+	}
 
-  public taraLogin(): void {
-    window.location.href = `${this.settings.url}/external-login/tara`;
-  }
+	mobileIdCancel() {
+		this.mobileId.challengeId = '';
+		this.mobileIdRequest.unsubscribe();
+	}
 
-  public harIdLogin(): void {
-    window.location.href = `${this.settings.url}/external-login/harid`;
-  }
+	subscribeToAuth() {
+		this.auth.isAuthenticated.subscribe((val) => {
+			this.loginStatus = val;
+		});
+	}
 
-  // needs to be finished
-  public mobileIdLogin(): void {
-    this.alertsService.clear('login-modal');
-    this.loading = true;
-    this.http.post(
-      `${this.settings.url}${this.settings.mobileLogin}`,
-      { telno: this.mobileIdForm.controls.phoneNumber.value },
-    ).subscribe(
-      (data: any) => {
-        this.loading = false;
-        this.mobileId.challengeId = data.ChallengeID;
-        const consecutiveForm = {
-          session_code: data.Sesscode,
-          id_code: data.UserIDCode,
-          auth_method: 'mobile_id',
-        };
-        this.mobileIdRequest = this.auth.login(consecutiveForm)
-          .subscribe(
-            (response) => {
-              this.modalService.close('login');
-              this.mobileId.challengeId = '';
-            },
-            (err) => {
-              this.alertsService
-                .error(this.translate.get(err.error.message), 'login-modal', false, true);
-              this.mobileId.challengeId = '';
-            },
-            () => {
-            });
-      },
-      (err: any) => {
-      },
-      () => {
-        this.loading = false;
-      },
-    );
-  }
+	public openLoginModal() {
+		this.loginForm.reset();
+		this.mobileIdForm.reset();
+		this.getAuthMethods();
+		this.modalService.toggle('login');
+	}
 
-  toggleSidemenu(): void {
-    this.sidemenuService.toggle();
-    this.active = this.sidemenuService.isVisible;
-  }
+	getAuthMethods() {
+		this.loading = true;
+		if (
+			this.settings.url === 'https://htm.wiseman.ee' ||
+			this.settings.url === 'http://test-htm.wiseman.ee:30000' ||
+			this.settings.url === 'https://apitest.hp.edu.ee'
+		) {
+			this.authMethods.basic = true;
+			this.authMethods.mobile_id = true;
+		}
+		this.http.get(`${this.settings.url}/auth_methods?_format=json`).subscribe(
+			(response) => {
+				this.authMethods = Object.assign({}, this.authMethods, {...response['auth_methods']});
+				this.availableAuthMethods =
+					Object.entries(this.authMethods).filter(method => method[1]);
+				if (!this.availableAuthMethods.length) {
+					this.alertsService.info('login.unavailable', 'login', false);
+				}
+			},
+			(response) => {
+				this.alertsService.error(response.error.message, 'login', false);
+				this.loading = false;
+			},
+			() => {
+				this.loading = false;
+			});
+	}
 
-  mobileIdCancel() {
-    this.mobileId.challengeId = '';
-    this.mobileIdRequest.unsubscribe();
-  }
+	searchRoute(e, timeout: boolean = false) {
 
-  subscribeToAuth() {
-    this.auth.isAuthenticated.subscribe((val) => {
-      this.loginStatus = val;
-    });
-  }
+		const term = !e ? this.searchTerm : (
+			e instanceof Event ? e.target['0'].value : e
+		);
 
-  public openLoginModal() {
-    this.loginForm.reset();
-    this.mobileIdForm.reset();
-    this.getAuthMethods();
-    this.modalService.toggle('login');
-  }
+		const url = `/otsing?term=${term}`;
+		this.searchTerm = '';
+		this.sendAnalyticsData(term);
+		this.cdr.detectChanges();
+		this.router.navigateByUrl(url);
+	}
 
-  getAuthMethods() {
-    this.loading = true;
-    if (
-      this.settings.url === 'https://htm.wiseman.ee' ||
-      this.settings.url === 'http://test-htm.wiseman.ee:30000' ||
-      this.settings.url === 'https://apitest.hp.edu.ee'
-    ) {
-      this.authMethods.basic = true;
-      this.authMethods.mobile_id = true;
-    }
-    this.http.get(`${this.settings.url}/auth_methods?_format=json`).subscribe(
-      (response) => {
-        this.authMethods = Object.assign({}, this.authMethods, { ...response['auth_methods'] });
-        this.availableAuthMethods =
-          Object.entries(this.authMethods).filter(method => method[1]);
-        if (!this.availableAuthMethods.length) {
-          this.alertsService.info('login.unavailable', 'login', false);
-        }
-      },
-      (response) => {
-        this.alertsService.error(response.error.message, 'login', false);
-        this.loading = false;
-      },
-      () => {
-        this.loading = false;
-      });
-  }
+	headerSearchEnabled() {
+		return this.activatedRoute.snapshot.firstChild &&
+			this.activatedRoute.snapshot.firstChild.routeConfig.path !== 'otsing';
+	}
 
-  searchRoute(e, timeout: boolean = false) {
+	sendAnalyticsData(term) {
+		this.analytics.trackEvent('homeSearch', 'submit', term);
+	}
 
-    const term = !e ? this.searchTerm : (
-      e instanceof Event ? e.target['0'].value : e
-    );
+	// wtf
+	openLogOutDropdown(event: Event) {
+		if (!this.logoutActive) {
+			document.addEventListener(
+				'click',
+				this.offClickHandler = () => this.offClickListener(),
+			);
+		}
+	}
 
-    const url = `/otsing?term=${term}`;
-    this.searchTerm = '';
-    this.sendAnalyticsData(term);
-    this.cdr.detectChanges();
-    this.router.navigateByUrl(url);
-  }
+	offClickListener() {
+		if (this.logoutActive) {
+			document.removeEventListener('click', this.offClickHandler);
+			this.logoutActive = false;
+		} else {
+			this.logoutActive = true;
+		}
+	}
 
-  headerSearchEnabled() {
-    return this.activatedRoute.snapshot.firstChild &&
-      this.activatedRoute.snapshot.firstChild.routeConfig.path !== 'otsing';
-  }
+	logOut() {
+		document.removeEventListener('click', this.offClickHandler);
+		this.logoutActive = false;
+		this.auth.logout();
+	}
 
-  sendAnalyticsData(term) {
-    this.analytics.trackEvent('homeSearch', 'submit', term);
-  }
-
-  // wtf
-  openLogOutDropdown(event: Event) {
-    if (!this.logoutActive) {
-      document.addEventListener(
-        'click',
-        this.offClickHandler = () => this.offClickListener(),
-      );
-    }
-  }
-
-  offClickListener() {
-    if (this.logoutActive) {
-      document.removeEventListener('click', this.offClickHandler);
-      this.logoutActive = false;
-    } else {
-      this.logoutActive = true;
-    }
-  }
-
-  logOut() {
-    document.removeEventListener('click', this.offClickHandler);
-    this.logoutActive = false;
-    this.auth.logout();
-  }
-
-  ngOnInit(): void {
-    this.active = this.sidemenuService.isVisible;
-    this.loginStatus = this.auth.isAuthenticated.getValue();
-    this.subscribeToAuth();
-    this.getAuthMethods();
-  }
+	ngOnInit(): void {
+		this.active = this.sidemenuService.isVisible;
+		this.loginStatus = this.auth.isAuthenticated.getValue();
+		this.subscribeToAuth();
+		this.getAuthMethods();
+	}
 }
