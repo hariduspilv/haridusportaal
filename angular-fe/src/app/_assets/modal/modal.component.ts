664b52724f3dc687d254e24dd18e175fe5bf6228
Ragnar <raxthepro1@gmail.com>
2020-03-05 18:58:13 +0200

    sprint-47-HP-392 WCAG

--- angular-fe/src/app/_assets/modal/modal.component.ts
+++ angular-fe/src/app/_assets/modal/modal.component.ts
@@ -1,94 +1,97 @@
 import {
-	Component,
-	Input,
-	OnInit,
-	ElementRef,
-	HostBinding,
-	Output,
-	EventEmitter,
-	ViewChildren,
-	QueryList,
-	ContentChild,
-	TemplateRef
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Output,
+  QueryList,
+  TemplateRef,
+  ViewChildren
 } from "@angular/core";
 import { ModalService } from "@app/_services";
 
 @Component({
-	selector: "modal-content",
-	template:
-		'<ng-content *ngIf="!loading"></ng-content>' +
-		'<loader *ngIf="loading"></loader>'
+  selector: "modal-content",
+  template:
+    '<ng-content *ngIf="!loading"></ng-content>' +
+    '<loader *ngIf="loading"></loader>'
 })
 export class ModalContentComponent {
-	@Input() id: string;
-	@Input() loading: boolean = false;
-	@ContentChild(TemplateRef, { static: false }) templateRef: TemplateRef<any>;
-	constructor(private modalService: ModalService) {}
+  @Input() public id: string;
+  @Input() public loading: boolean = false;
+  @ContentChild(TemplateRef, { static: false }) public templateRef: TemplateRef<any>;
+
+  constructor(private modalService: ModalService) {
+  }
 }
 
 @Component({
-	selector: "htm-modal",
-	templateUrl: "./modal.template.html",
-	styleUrls: ["./modal.styles.scss"]
+  selector: "htm-modal",
+  templateUrl: "./modal.template.html",
+  styleUrls: ["./modal.styles.scss"]
 })
 export class ModalComponent implements OnInit {
-	@Input() id: string;
-	@Input() modalTitle: string = "";
-	public opened: boolean = false;
-	private element: any;
-	public modalIds: any;
-	@Input() titleExists: boolean = true;
-	@Input() topAction: boolean = true;
-	@Input() bottomAction: boolean = true;
-	@Input() size: string = "default";
-	@Input() reloadOnClose: boolean = false;
-	// Modal opening button for story
-	@Input() stateButton: boolean = false;
-	@Output() onClose: EventEmitter<any> = new EventEmitter();
-	@ViewChildren("modalContent") contents: QueryList<any> = new QueryList();
-	@ContentChild(TemplateRef, { static: false }) templateRef: TemplateRef<any>;
 
-	@HostBinding("class") get hostClasses(): string {
-		const classes = [];
-		classes.push(this.opened ? "" : "modal-hidden");
-		classes.push(`size-${this.size}`);
-		return classes.join(" ");
-	}
+  @Input() public id: string;
+  @Input() public modalTitle: string = "";
+  public opened: boolean = false;
+  public modalIds: any;
+  @Input() public titleExists: boolean = true;
+  @Input() public topAction: boolean = true;
+  @Input() public bottomAction: boolean = true;
+  @Input() public size: string = "default";
+  @Input() public reloadOnClose: boolean = false;
+  // Modal opening button for story
+  @Input() public stateButton: boolean = false;
+  @Output() public onClose: EventEmitter<any> = new EventEmitter();
+  @ViewChildren("modalContent") public contents: QueryList<any> = new QueryList();
+  @ContentChild(TemplateRef, { static: false }) public templateRef: TemplateRef<any>;
+  private element: any;
 
-	constructor(private elem: ElementRef, private modalService: ModalService) {
-		this.element = elem.nativeElement;
-	}
+  constructor(private elem: ElementRef, private modalService: ModalService) {
+    this.element = elem.nativeElement;
+  }
 
-	ngOnInit() {
-		// Outside click close
-		this.element.addEventListener("click", el => {
-			if (
-				el.target.className &&
-				el.target.className.includes("modal__backdrop")
-			) {
-				this.stateChange(false);
-			}
-		});
-		this.modalService.add(this);
-		// Modal selection in story
-		if (this.modalService.modals && this.modalService.modals.length) {
-			this.modalIds = this.modalService.modals.map(item => item.id);
-		}
-	}
+  @HostBinding("class") get hostClasses(): string {
+    const classes = [];
+    classes.push(this.opened ? "" : "modal-hidden");
+    classes.push(`size-${this.size}`);
+    return classes.join(" ");
+  }
 
-	ngOnDestroy(): void {
-		this.modalService.remove(this.id);
-	}
+  @HostListener('document:keydown', ['$event'])
+  public onKeyDownHandler(event: KeyboardEvent) {
+    if (event.key === "Escape") {
+      this.stateChange(false)
+    }
+  }
 
-	stateChange(state: boolean): void {
-		const isOpened = this.modalService.modalOpened[this.id];
-		this.modalService.modalOpened[this.id] = state;
-		if (this.opened && !state && this.reloadOnClose) {
-			window.location.reload();
-		}
-		this.opened = state;
-		if (isOpened && !state) {
-			this.onClose.emit(true);
-		}
-	}
+  public ngOnInit() {
+    // Outside click close
+    this.modalService.add(this);
+    // Modal selection in story
+    if (this.modalService.modals && this.modalService.modals.length) {
+      this.modalIds = this.modalService.modals.map(item => item.id);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+  }
+
+  public stateChange(state: boolean): void {
+    const isOpened = this.modalService.modalOpened[this.id];
+    this.modalService.modalOpened[this.id] = state;
+    if (this.opened && !state && this.reloadOnClose) {
+      window.location.reload();
+    }
+    this.opened = state;
+    if (isOpened && !state) {
+      this.onClose.emit(true);
+    }
+  }
 }
