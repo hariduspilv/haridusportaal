3811da39a17ecb7b739b164660339972f0d69a4a
root <lars.soonvald@gmail.com>
2020-03-10 09:39:16 +0000

    dump

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -195,6 +195,7 @@ class EhisConnectorService {
    * @return bool
    */
   public function useReg(){
+    dump($this->currentRole->getCurrentRole());
     if($this->currentRole['current_role']['type'] === 'juridical_person') return true;
     return false;
   }
