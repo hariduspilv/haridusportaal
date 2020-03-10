2db627211ad023b74cfc82b16ab22474ce5b3453
root <lars.soonvald@gmail.com>
2020-03-10 09:40:44 +0000

    dump

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -66,7 +66,7 @@ class EhisConnectorService {
     $this->currentUser = $current_user;
     $this->client = $client_factory->getClient();
     $this->logger = $logger->get('ehis_connector_service');
-    $this->currentRole = \Drupal::service('current_user.role_switcher')->getCurrentRole();
+    $this->currentRole = \Drupal::service('current_user.role_switcher');
     $this->loime_url = settings::get('loime_default_url');
   }
 
