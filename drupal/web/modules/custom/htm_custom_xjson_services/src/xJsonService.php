171b3031ce29a3e70818d6605303ccf3eda08f05
root <lars.soonvald@gmail.com>
2020-04-09 16:36:57 +0000

    testing

--- drupal/web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
+++ drupal/web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
@@ -258,11 +258,8 @@ class xJsonService implements xJsonServiceInterface {
 
     $xml = new DOMDocument();
     foreach($data as $key => $element) {
-      dump($key);
-      dump($element['value']);
-      if($element['value'] === '09.04.2020') $element['value'] = '2';
       $xml_field = $xml->createElement($key);
-      $xml_field_value = $xml->createElement($element['value']);
+      $xml_field_value = $xml->createTextNode($element['value']);
       $xml_field->appendChild($xml_field_value);
       $xml->appendChild($xml_field);
     }
