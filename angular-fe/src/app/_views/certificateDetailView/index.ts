1827e822b269866ef8370c24644d30f1fe82c1a7
pesarv <54806197+pesarv@users.noreply.github.com>
2020-04-20 15:36:20 +0300

    access code path changed (#1119)
    Co-authored-by: Pearu Sarv <pesarv@ttu.ee>

--- angular-fe/src/app/_views/certificateDetailView/index.ts
+++ angular-fe/src/app/_views/certificateDetailView/index.ts
@@ -14,10 +14,6 @@ const routes: Routes = [
     path: '',
     component: CertificateDetailView,
   },
-  {
-    path: ':certificateNr/:accessorCode',
-    component: CertificateDetailView,
-  },
 ];
 
 @NgModule({
