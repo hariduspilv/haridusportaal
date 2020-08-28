94c6f26fa3cca75f37de902fce8b15db5056dc37
root <lars.soonvald@gmail.com>
2020-08-28 10:36:11 +0000

    login fix

--- drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
+++ drupal/web/modules/custom/htm_custom_tara_authentication/src/Controller/TaraRedirectController.php
@@ -8,7 +8,7 @@ use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 use Drupal\htm_custom_authentication\Authentication\Provider\JsonAuthenticationProvider;
-use Drupal\openid_connect\Claims;
+use Drupal\openid_connect\OpenIDConnectClaims;
 use Drupal\openid_connect\Controller\RedirectController;
 use Drupal\openid_connect\Plugin\OpenIDConnectClientManager;
 use Drupal\openid_connect\StateToken;
@@ -21,7 +21,7 @@ class TaraRedirectController extends RedirectController{
 
 	protected $jsonAuth;
 
-	public function __construct (OpenIDConnectClientManager $plugin_manager, RequestStack $request_stack, LoggerChannelFactoryInterface $logger_factory, AccountInterface $current_user, Claims $claims, JsonAuthenticationProvider $jsonAuth) {
+	public function __construct (OpenIDConnectClientManager $plugin_manager, RequestStack $request_stack, LoggerChannelFactoryInterface $logger_factory, AccountInterface $current_user, OpenIDConnectClaims $claims, JsonAuthenticationProvider $jsonAuth) {
 		parent::__construct($plugin_manager, $request_stack, $logger_factory, $current_user);
 		$this->claims = $claims;
 		$this->jsonAuth = $jsonAuth;
