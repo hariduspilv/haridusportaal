f9b243d4b5a8f64008fd2a15c42fcc21d53b936b
Madis Põld <madis.pold@twn.ee>
2020-10-07 13:13:33 +0300

    Version 10.1.4 initial

--- angular-fe/src/app/app.module.ts
+++ angular-fe/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AuthInterceptor } from './_interceptors';
 import { AmpService } from './_services/ampService';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { DeviceDetectorModule } from 'ngx-device-detector';
+import { environment } from '../environments/environment';
 // We dont need short month names at all!
 localeEt[5][1] = localeEt[5][2].map((item) => {
   return item.charAt(0).toUpperCase() + item.slice(1);
@@ -33,7 +34,8 @@ registerLocaleData(localeEt);
     HttpClientJsonpModule,
     TranslateModule.forRoot(),
     DeviceDetectorModule.forRoot(),
-    ServiceWorkerModule.register('/ngsw-worker.js'),
+    ServiceWorkerModule.register('ngsw-worker.js'),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'et-EE' },
