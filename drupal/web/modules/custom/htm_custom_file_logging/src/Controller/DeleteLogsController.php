c87252b0ff6e528ecdf1ced5b7462dcc759e0677
lsoonvald <lars.soonvald@gmail.com>
2020-11-04 07:02:24 +0200

    Feature/hp 499 (#1161)
    * created cron job for removing old log files
    
    Co-authored-by: Lars Soonvald <lars.soonvald@twn.ee>

---
+++ drupal/web/modules/custom/htm_custom_file_logging/src/Controller/DeleteLogsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\htm_custom_file_logging\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class DeleteLogsController.
+ */
+class DeleteLogsController extends ControllerBase {
+
+  public $logpath = '/app/drupal/web/sites/default/files/logs/';
+
+  public function old_list() {
+    $directories = [];
+    $this->getOldYearDirectories($directories);
+    $this->getOldMonthDirectories($directories);
+    return $directories;
+  }
+
+  public function delete_log($path) {
+    if(!empty($path)) {
+      system("rm -rf ".escapeshellarg($path));
+    }
+  }
+
+  private function getOldYearDirectories(&$directories) {
+    $year = date('Y');
+    $scanned = array_diff(scandir($this->logpath), ['.', '..', '.htaccess', $year]);
+    $directories = array_map(function($directory) {
+      return $this->logpath . $directory;
+    }, $scanned);
+  }
+
+  private function getOldMonthDirectories(&$directories) {
+    $year = date('Y');
+    $month = (int) date('m');
+    $scanned = array_diff(scandir($this->logpath . $year), ['.', '..', '.htaccess']);
+    $directories = array_merge($directories,
+      array_filter(
+        array_map(function($directory) use ($year, $month) {
+          $dirValue = (int) ltrim($directory, '0');
+          if($dirValue > $month || $month - $dirValue >= 2) {
+            return $this->logpath . $year . '/' . $directory;
+          }
+        }, $scanned),
+        function($v) { return !is_null($v); }));
+  }
+}
