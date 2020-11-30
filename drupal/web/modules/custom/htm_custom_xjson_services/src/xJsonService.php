530c1c0bbe8e382f06cadba93c6fb30b2f1b2d18
Lars Soonvald <lars.soonvald@twn.ee>
2020-11-30 20:07:21 +0200

    testing xjson

--- drupal/web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
+++ drupal/web/modules/custom/htm_custom_xjson_services/src/xJsonService.php
@@ -342,10 +342,10 @@ class xJsonService implements xJsonServiceInterface {
           }
           //Add step non data_elements
           unset($definition_body['steps'][$step_key]['data_elements']);
+          $return['body']['steps'][$step_key] += $definition_body['steps'][$step_key];
           dump($return['body']['steps'][$step_key]);
           dump($definition_body['steps'][$step_key]);
           die();
-          $return['body']['steps'][$step_key] += $definition_body['steps'][$step_key];
           // add each step messages aswel
           if (isset($response_body['steps'][$step_key]['messages'])) {
             $return['body']['steps'][$step_key]['messages'] = $response_body['steps'][$step_key]['messages'];
