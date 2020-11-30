3409de7fb3ae37c4a5dec42594d638510e24540d
Evert Prants <evert.prants@twn.ee>
2020-11-30 16:29:27 +0200

    menuu, värvid

--- angular-fe/src/app/_assets/menu/sidemenu.component.ts
+++ angular-fe/src/app/_assets/menu/sidemenu.component.ts
@@ -10,6 +10,7 @@ import {
   QueryList,
   Output,
   EventEmitter,
+  HostListener,
 } from '@angular/core';
 import { RippleService, SidemenuService, SettingsService, AuthService } from '@app/_services';
 import { Subscription } from 'rxjs';
@@ -17,36 +18,54 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Location } from '@angular/common';
 import { NavigationEnd, RouterEvent, Router } from '@angular/router';
 import { environment } from '@env/environment';
+import { mergeAnalyzedFiles } from '@angular/compiler';
+
+interface IMenuURL {
+  path: string;
+  internal: boolean;
+}
+
+interface IMenuData {
+  label: string;
+  description?: string;
+  links: IMenuData[];
+  url: IMenuURL;
+  expanded?: boolean;
+  active?: boolean;
+  userClosed?: boolean;
+}
 
 @Component({
   selector: 'sidemenu-item',
+  templateUrl: './sidemenu.item.template.html',
   styleUrls: ['./sidemenu.styles.scss'],
-  templateUrl: 'sidemenu.item.template.html',
 })
-export class SidemenuItemComponent {
-  public isOpen: boolean = false;
-  @Output() openStateChange = new EventEmitter;
-  @Input() item: any = {};
+export class MenuItemComponent {
+  @Input() public items: IMenuData[];
+  @Input() public type = 'item';
+  @Output() public toggle: EventEmitter<IMenuData> = new EventEmitter<IMenuData>();
 
-  constructor(private ripple: RippleService, private cdr: ChangeDetectorRef) { }
+  constructor(private router: Router, private location: Location) {}
 
-  toggle(e) {
-    this.isOpen = !this.isOpen;
-    this.openStateChange.emit(this.item['label']);
-    this.animateRipple(e);
-  }
+  clickMenuItem(item: IMenuData, event: any) {
+    event.stopPropagation();
+    const path = decodeURI(this.location.path());
+    const match = item.url.path === path.split('?')[0].split('#')[0] ||
+                  path.includes(item.url.path);
 
-  public close() {
-    this.isOpen = false;
-    this.cdr.detectChanges();
-  }
-
-  public open() {
-    this.isOpen = true;
-    this.cdr.detectChanges();
-  }
-  animateRipple(event) {
-    this.ripple.animate(event, 'dark');
+    if (!match && item.url.path !== '#nolink') {
+      this.router.navigateByUrl(item.url.path);
+    } else {
+      if (item.links.length) {
+        item.expanded = !item.expanded;
+        if (!item.expanded) {
+          item.userClosed = true;
+        } else if (item.userClosed) {
+          item.userClosed = false;
+        }
+        this.toggle.emit(item);
+      }
+    }
   }
 }
 
@@ -55,120 +74,209 @@ export class SidemenuItemComponent {
   templateUrl: './sidemenu.template.html',
   styleUrls: ['./sidemenu.styles.scss'],
 })
-
 export class MenuComponent implements OnInit, OnDestroy {
-
-  public version: any = environment.VERSION;
   public isVisible: boolean;
   private subscription: Subscription = new Subscription();
-  private authSub: Subscription = new Subscription();
   private routerSub: Subscription = new Subscription();
-  @ViewChildren(SidemenuItemComponent) sidemenuItems: QueryList<SidemenuItemComponent>;
-  @Input() data;
+  @ViewChildren(MenuItemComponent) private menus: QueryList<MenuItemComponent>;
+  public data = {
+    menu: {
+      links: [
+        {
+          description: null,
+          label: 'Õppimine',
+          links: [
+            {
+              description: null,
+              label: 'Õppeasutused',
+              links: [],
+              url: {
+                path: '/kool',
+                internal: true,
+              },
+            },
+            {
+              description: null,
+              label: 'Õppekavad',
+              links: [],
+              url: {
+                path: '/erialad',
+                internal: true,
+              },
+            },
+            {
+              description: null,
+              label: 'Õpitee',
+              links: [
+                {
+                  description: null,
+                  label: 'Algharidus',
+                  links: [],
+                  url: {
+                    path: '/artiklid/artikkel-mugudest',
+                    internal: true,
+                  },
+                },
+                {
+                  description: null,
+                  label: 'Põhiharidus',
+                  links: [],
+                  url: {
+                    path: '/artiklid/asdf',
+                    internal: true,
+                  },
+                },
+              ],
+              url: {
+                path: '#nolink',
+                internal: true,
+              },
+            },
+          ],
+          url: {
+            internal: true,
+            path: '/õppimine',
+          },
+        },
+        {
+          description: null,
+          label: 'Karjäär',
+          links: [
+            {
+              description: null,
+              label: 'Valdkonnad tööturul',
+              links: [],
+              url: {
+                path: '/artiklid/linkide-ja-failide-näide',
+                internal: true,
+              },
+            },
+            {
+              description: null,
+              label: 'Tööta ja õpi',
+              links: [
+                {
+                  description: null,
+                  label: 'Koolitused',
+                  links: [],
+                  url: {
+                    path: '/ametialad/oska-põhikutseala-test-05102018',
+                    internal: true,
+                  },
+                },
+                {
+                  description: null,
+                  label: 'õõõõ',
+                  links: [],
+                  url: {
+                    path: '/tööjõuprognoos/tööjõuprognoos-pilt-ja-videod-test',
+                    internal: true,
+                  },
+                },
+              ],
+              url: {
+                path: '#nolink',
+                internal: true,
+              },
+            },
+          ],
+          url: {
+            internal: true,
+            path: '/karjäär',
+          },
+        },
+        {
+          description: null,
+          label: 'Infosüsteemid',
+          links: [],
+          url: {
+            path: '/artiklid/afefewf',
+            internal: true,
+          },
+        },
+      ],
+    },
+  };
+
   @HostBinding('class') get hostClasses(): string {
     return this.isVisible ? 'sidemenu is-visible' : 'sidemenu';
   }
 
   constructor(
     private sidemenuService: SidemenuService,
-    private http: HttpClient,
-    private settings: SettingsService,
-    private auth: AuthService,
-    private cdr: ChangeDetectorRef,
-    private location: Location,
     private router: Router,
-  ) {}
+    private location: Location,
+    private cdr: ChangeDetectorRef) {}
 
-  closeSidemenu(): void {
-    this.sidemenuService.close();
+  subscribeToRouter(): void {
+    this.routerSub = this.router.events.subscribe((event:RouterEvent) => {
+      if (event instanceof NavigationEnd) {
+        /*if (this.isVisible) {
+          this.sidemenuService.close();
+        }*/
+        this.makeActive();
+      }
+    });
   }
 
-  subscribeToService():void {
+  subscribeToService(): void {
     this.subscription = this.sidemenuService.isVisibleSubscription.subscribe((value) => {
       this.isVisible = value;
     });
   }
 
-  subscribeToAuth():void {
-    this.authSub = this.auth.isAuthenticated.subscribe((val) => {
-      this.getData();
-    });
-  }
+  hasActiveInTree(items: IMenuData[], path: string, depth: number): boolean {
+    let hasExpanded = false;
+    for (const item of items) {
+      const match =
+        item.url.path === path.split('?')[0].split('#')[0] ||
+        path.includes(item.url.path);
 
-  subscribeToRouter(): void {
-    this.routerSub = this.router.events.subscribe((event:RouterEvent) => {
-      if (event instanceof NavigationEnd) {
-        if (this.isVisible) {
-          this.sidemenuService.close();
+      if (item.links.length) {
+        const has = this.hasActiveInTree(item.links, path, depth + 1);
+        if (depth === 1) {
+          item.expanded = !item.userClosed;
+          item.active = has;
+          if (!hasExpanded && (match || has)) {
+            hasExpanded = match || has;
+          }
+        } else {
+          if ((match || has) && !item.userClosed) {
+            item.expanded = true;
+            item.active = match || has;
+            hasExpanded = true;
+          } else {
+            item.expanded = false;
+            item.active = false;
+          }
         }
-        this.makeActive();
+      } else if (match) {
+        item.active = true;
+        hasExpanded = true;
+      } else {
+        item.active = false;
       }
-    });
-  }
-  getData(init: boolean = false):void {
-    const variables = {
-      language: this.settings.activeLang,
-    };
-    const path = this.settings.query('getMenu', variables);
-    // force to not use disk cache
-    this.http.get(path, {
-      headers: new HttpHeaders({ 'Cache-Control': 'no-cache' }),
-    }).subscribe((response) => {
-      this.data = response['data'];
-      this.cdr.detectChanges();
-      if (init) {
-        this.makeActive();
-      }
-    });
-  }
-
-  makeActive() {
-    const path = decodeURI(this.location.path());
-    const categories = this.sidemenuItems.toArray().filter((el) => {
-      if (el.item.links.length > 0) {
-        return true;
-      }
-    });
-
-    const activeCategory = categories.find((el) => {
-      return el.item.links.find((link) => {
-        if (link.url.path === path.split('?')[0].split('#')[0]) {
-          return true;
-        }
-        const pathRoot = path.split('/');
-
-        if (link.url.path.includes(`${pathRoot[0]}/${pathRoot[1]}`)) {
-          return true;
-        }
-        return false;
-      });
-    });
-    if (activeCategory) {
-      activeCategory.open();
-    } else {
-      this.closeOthers('');
     }
+
+    return hasExpanded;
+  }
+
+  makeActive(): void {
+    const path = decodeURI(this.location.path());
+
     this.cdr.detectChanges();
+    for (const menu of this.menus) {
+      this.hasActiveInTree(menu.items, path, 0);
+    }
   }
 
-  closeOthers(item: any = '') {
-    this.sidemenuItems.toArray().map((el: any) => {
-      if (el.item.label !== item) {
-        el.close();
-      }
-    });
-  }
-
-  ngOnInit():void {
-    this.subscribeToAuth();
+  ngOnInit(): void {
     this.subscribeToService();
     this.subscribeToRouter();
-    this.getData(true);
   }
 
-  ngOnDestroy():void {
-    this.subscription.unsubscribe();
-    this.authSub.unsubscribe();
+  ngOnDestroy(): void {
     this.routerSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
