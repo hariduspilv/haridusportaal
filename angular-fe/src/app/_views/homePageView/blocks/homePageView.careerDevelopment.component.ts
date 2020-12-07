006c4549dccf5d5c591a67564804ee6fbd50b9d6
Evert <evert@lunasqu.ee>
2020-12-07 15:28:19 +0200

    Sprint 53 hp 432 (#1173)
    * komponent lahku ajada et oleks mugavam töötada
    
    * õppimise vaheleht ootab assetite taga
    
    * õppimise vaheavalehe kujunduselemendid lisatud
    
    * eemaldada withCredentials taotlusest
    
    * create homepageservice
    
    * big cleaning, models, remove theme-dependent ifs in components
    
    * fieldvary and typings
    
    * forgot this here
    
    * accidentally broke the slogan
    
    * requested changes
    
    * prevent topical image from going outside the view
    
    * home page view scroll overflow fixes

---
+++ angular-fe/src/app/_views/homePageView/blocks/homePageView.careerDevelopment.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { ICareerSlide } from '../homePage.model';
+import { HomePageService } from '../homePage.service';
+
+@Component({
+  selector: 'homepage-careerDevelopment',
+  templateUrl: 'homePageView.careerDevelopment.html',
+})
+export class HomePageCareerDevelopmentComponent implements OnInit {
+  @Input() title: string;
+  @Input() description: string;
+  @Input() url: string;
+  @Input() theme: string;
+  @Input() line: number = 3;
+  public data: ICareerSlide[] = [];
+
+  constructor(
+    private service: HomePageService,
+  ) {}
+
+  @HostBinding('class') get hostClasses(): string {
+    return `theme--${this.theme}`;
+  }
+
+  ngOnInit() {
+    if (this.url) {
+      this.service.getCareerDevelopmentSlides(this.url).subscribe(
+        (data: ICareerSlide[]) => this.data = data);
+    }
+  }
+}
