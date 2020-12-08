a7a520e49608c58f02e6de821af5cd3bffd9efa7
Lars Soonvald <lars.soonvald@twn.ee>
2020-12-08 19:24:31 +0200

    fixed detail views

--- angular-fe/src/app/_views/detailView/detailView.component.ts
+++ angular-fe/src/app/_views/detailView/detailView.component.ts
@@ -211,10 +211,14 @@ export class DetailViewComponent {
       this.data.video = this.data.video[0];
     }
 
+    console.log(this.data.video);
+
     this.data.processedImages =
       this.data.additionalImages ?
         [this.data.image, ...this.data?.additionalImages] :
         this.data.image;
+
+    console.log(this.data.processedImages);
 
     this.data['fieldAccordion'] = this.data.reverseFieldOskaFieldParagraph &&
       this.data.reverseFieldOskaFieldParagraph.entities.length ?
