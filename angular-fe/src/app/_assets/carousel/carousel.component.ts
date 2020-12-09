9146cee5ac8d8e3cfc0f4560b1907639f145b8c9
Evert Prants <evert.prants@twn.ee>
2020-12-09 16:31:23 +0200

    new carousel using swiper

---
+++ angular-fe/src/app/_assets/carousel/carousel.component.ts
@@ -0,0 +1,42 @@
+import { Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { TranslateService } from '@app/_modules/translate/translate.service';
+import { SwiperComponent } from 'ngx-useful-swiper';
+import { SwiperOptions } from 'swiper';
+import { CarouselItem } from './carousel.model';
+
+@Component({
+  selector: 'carousel',
+  templateUrl: './carousel.template.html',
+  styleUrls: ['./carousel.styles.scss'],
+})
+export class CarouselComponent {
+  @Input() public data: CarouselItem[];
+  @ViewChild('usefulSwiper', { static: false }) usefulSwiper: SwiperComponent;
+  config: SwiperOptions = {
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: {
+      nextEl: '.slides__arrow--right',
+      prevEl: '.slides__arrow--left'
+    },
+    a11y: {
+      prevSlideMessage: this.translate.get('carousel.prev'),
+      nextSlideMessage: this.translate.get('carousel.next'),
+      paginationBulletMessage: this.translate.get('carousel.to_slide'),
+    },
+    breakpoints: {
+      720: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      1280: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      }
+    },
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
+  };
+
+  constructor(public translate: TranslateService) {}
+}
