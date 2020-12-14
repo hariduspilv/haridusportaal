4f9d7f069b8b37b19fb4e40f066dc9ddfc122df7
Evert <evert.prants@twn.ee>
2020-12-14 14:55:56 +0200

    add swiper carousel to storybook (#1203)

---
+++ angular-fe/stories/assets/carousel/carousel.data.ts
@@ -0,0 +1,63 @@
+
+export const data = [
+  {
+    link: {
+      url: {
+        routed: true,
+        path: '/uudised',
+      },
+      title: 'Vaata rohkem',
+    },
+    title: 'Tulemused',
+    image: {
+      alt: 'Alt tekst',
+      url: 'http://htm.wiseman.ee/sites/default/files/2020-02/homepage-slides-1.svg',
+    },
+    content: 'Tunnistuste ja riigieksamite tulemused sisseloginud kasutajale.',
+  },
+  {
+    link: {
+      url: {
+        routed: true,
+        path: '/uudised',
+      },
+      title: 'Vaata rohkem',
+    },
+    title: 'Andmed EHIS-es',
+    image: {
+      alt: 'Alt tekst',
+      url: 'http://htm.wiseman.ee/sites/default/files/2020-02/homepage-slides-2.svg',
+    },
+    content: 'Õpingute ja õpetamisega seotud andmed Eesti Hariduse Infosüsteemis.',
+  },
+  {
+    link: {
+      url: {
+        routed: true,
+        path: '/uudised',
+      },
+      title: 'Haridus- ja Teadusministeeriumi koduleht',
+    },
+    title: 'Vajaduspõhine',
+    image: {
+      alt: 'Alt tekst',
+      url: 'http://htm.wiseman.ee/sites/default/files/2020-02/homepage-slides-3.svg',
+    },
+    content: 'Üliõpilasele kõrghariduse omandamisega kaasnevate kulutuste katmiseks.',
+  },
+  {
+    link: {
+      url: {
+        routed: false,
+        path: 'http://edu.twn.ee/töölaud',
+      },
+      title: 'Logi sisse',
+    },
+    title: 'Vajaduspõhine taotlus',
+    image: {
+      alt: 'Alt tekst sellele pildile pulamula',
+      url: 'http://htm.wiseman.ee/sites/default/files/2020-03/homepage-slides-3-2_0.svg',
+    },
+    content: 'Alates 2013/2014. õppeaastast toetab riik vähemkindlustatud perekondadest pärit tudengeid kõrgharid.',
+  },
+];
