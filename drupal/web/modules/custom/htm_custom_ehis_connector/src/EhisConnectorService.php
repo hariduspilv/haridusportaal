158db359f558452fc07e53dcf8b639469ff0a8b4
Lars Soonvald <lars.soonvald@twn.ee>
2020-12-18 06:40:11 +0200

    OLT debug

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -87,10 +87,9 @@ class EhisConnectorService {
             $redis_response = [];
             $redis_hits = 0;
             foreach($params['hash'] as $hash) {
-              if($response = $this->getValue($params['key'], $hash)){
+              if($response = $this->getValue($params['key'], $hash) && $hash !== 'OLT'){
                 $redis_hits++;
                 $redis_response = array_merge_recursive($redis_response, $response);
-
               }
             }
             if(!empty($redis_response) && $redis_hits === count($params['hash'])) {
@@ -113,7 +112,7 @@ class EhisConnectorService {
           $redis_response = [];
           $redis_hits = 0;
           foreach($params['hash'] as $hash) {
-            if($response = $this->getValue($params['key'], $hash)){
+            if($response = $this->getValue($params['key'], $hash) && $hash !== 'OLT'){
               $redis_hits++;
               $redis_response = array_merge_recursive($redis_response, $response);
             } else {
