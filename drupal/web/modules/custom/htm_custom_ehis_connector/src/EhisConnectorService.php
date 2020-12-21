a2d9678624007b5bbca53a8362576e37fcaa5d7c
Lars Soonvald <lars.soonvald@twn.ee>
2020-12-21 16:27:21 +0200

    OLT in query

--- drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
+++ drupal/web/modules/custom/htm_custom_ehis_connector/src/EhisConnectorService.php
@@ -87,7 +87,7 @@ class EhisConnectorService {
             $redis_response = [];
             $redis_hits = 0;
             foreach($params['hash'] as $hash) {
-              if($response = $this->getValue($params['key'], $hash) && $hash !== 'OLT'){
+              if($response = $this->getValue($params['key'], $hash)){
                 $redis_hits++;
                 $redis_response = array_merge_recursive($redis_response, $response);
               }
