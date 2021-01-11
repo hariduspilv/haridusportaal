54ae2826efd584b07bd114b8952765940d822435
Evert Prants <evert.prants@twn.ee>
2021-01-11 13:40:26 +0200

    new title service, set titles to home pages

---
+++ angular-fe/src/app/_services/TitleService.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Injectable()
+export class TitleService {
+  public site = 'Haridusportaal.edu.ee';
+  public delimeter = '|';
+  public currentTitle: string = '';
+
+  constructor(
+    private titleService: Title) {}
+
+  public setTitle(title: string): void {
+    this.currentTitle = title;
+    this.titleService.setTitle(this.getFullTitle());
+  }
+
+  public getTitle(): string {
+    return this.currentTitle;
+  }
+
+  public getFullTitle(): string {
+    return this.currentTitle !== ''
+      ? `${this.currentTitle} ${this.delimeter} ${this.site}`
+      : this.site;
+  }
+}
