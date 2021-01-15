dcd2e9dff68004cc225961c0662b2468d83a469f
Evert Prants <evert.prants@twn.ee>
2021-01-15 12:09:01 +0200

    don't show duplicate information here

--- angular-fe/src/app/modules/certificates/components/certificate-detailed/certificate-detailed.component.ts
+++ angular-fe/src/app/modules/certificates/components/certificate-detailed/certificate-detailed.component.ts
@@ -14,6 +14,34 @@ export class CertificateDetailedComponent {
 
   @Input() documents: any = {};
 
+  private idCode(idCode: string): string {
+    if (!idCode || !isNaN(<any>idCode.charAt(0)) && !isNaN(<any>idCode.charAt(1))) {
+      return idCode;
+    }
+    return idCode.substring(2);
+  }
+
+  public get isNewFirstName(): boolean {
+    const { content } = this.documents.certificate;
+    return content.currentOwnerData &&
+      content.currentOwnerData.firstName.toLowerCase() !==
+      content.graduate.firstName.toLowerCase()
+  }
+
+  public get isNewLastName(): boolean {
+    const { content } = this.documents.certificate;
+    return content.currentOwnerData &&
+      content.currentOwnerData.lastName.toLowerCase() !==
+      content.graduate.lastName.toLowerCase()
+  }
+
+  public get isNewIDCode(): boolean {
+    const { content } = this.documents.certificate;
+    return content.currentOwnerData &&
+      this.idCode(content.currentOwnerData.idCode) !==
+      this.idCode(content.graduate.idCode)
+  }
+
   public get certificate(): CertificateDocument {
     return this.documents.certificate;
   }
