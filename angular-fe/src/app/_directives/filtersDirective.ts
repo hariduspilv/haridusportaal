fcfdc525fceaab6ce8ff41e2e43a1cb13c878aa5
Madis Põld <madis.pold@twn.ee>
2021-02-09 14:55:49 +0200

    Professions initially without counts #HP-625

--- angular-fe/src/app/_directives/filtersDirective.ts
+++ angular-fe/src/app/_directives/filtersDirective.ts
@@ -3,6 +3,7 @@ import {
   ContentChildren,
   Directive,
   ElementRef,
+  Input,
   OnDestroy,
   QueryList,
 } from '@angular/core';
@@ -19,6 +20,7 @@ export class FiltersDirective implements AfterViewInit, OnDestroy {
 
   @ContentChildren(FormItemComponent, { descendants: true })
     formItems: QueryList<FormItemComponent>;
+  @Input() outsideParameters: Object = {};
 
   private paramsWatcher: Subscription = new Subscription();
 
@@ -38,7 +40,10 @@ export class FiltersDirective implements AfterViewInit, OnDestroy {
   }
 
   gatherValues(): void {
-    const queryParams = {};
+    let queryParams = {};
+    if (this.outsideParameters) {
+      queryParams = { ...this.outsideParameters };
+    }
 
     this.formItems.forEach((item) => {
       const data = item.getValue();
