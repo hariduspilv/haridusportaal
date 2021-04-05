ad61d1ae513c276a1453d5480d8c3f2082b8d5b0
VoldemarPungar <voldemar.pungar@gmail.com>
2021-04-05 15:39:17 +0300

    HP-526-Drupali-uuendused-arenduses | initial commit

--- drupal/web/modules/contrib/graphql_metatag/src/Plugin/GraphQL/Fields/Metatag/Key.php
+++ drupal/web/modules/contrib/graphql_metatag/src/Plugin/GraphQL/Fields/Metatag/Key.php
@@ -21,12 +21,27 @@ class Key extends FieldPluginBase {
    * {@inheritdoc}
    */
   protected function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
-    if (isset($value['#tag']) && $value['#tag'] === 'meta') {
-      yield isset($value['#attributes']['property']) ? $value['#attributes']['property'] : $value['#attributes']['name'];
-    }
-    else if (isset($value['#tag']) && $value['#tag'] === 'link') {
-      yield $value['#attributes']['rel'];
+    if (isset($value['#tag'])) {
+      if ($value['#tag'] === 'meta') {
+        if (isset($value['#attributes']['property'])) {
+          yield $value['#attributes']['property'];
+        }
+        elseif (isset($value['#attributes']['http-equiv'])) {
+          yield $value['#attributes']['http-equiv'];
+        }
+        elseif (isset($value['#attributes']['itemprop'])) {
+          yield $value['#attributes']['itemprop'];
+        }
+        elseif (isset($value['#attributes']['name'])) {
+          yield $value['#attributes']['name'];
+        }
+        else {
+          yield '';
+        }
+      }
+      elseif ($value['#tag'] === 'link') {
+        yield $value['#attributes']['rel'];
+      }
     }
   }
-
 }
