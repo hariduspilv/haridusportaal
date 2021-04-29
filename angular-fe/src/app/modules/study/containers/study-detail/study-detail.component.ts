65195124826f526d66af643e93c3af8c598f1dc9
Dmitri Solntsev <dmitri.solntsev@twn.ee>
2021-04-29 12:04:49 +0300

    HP-729 corrections after testing ( join() from html to utility )

--- angular-fe/src/app/modules/study/containers/study-detail/study-detail.component.ts
+++ angular-fe/src/app/modules/study/containers/study-detail/study-detail.component.ts
@@ -5,25 +5,28 @@ import {Observable} from 'rxjs';
 import {map, tap} from 'rxjs/operators';
 import {StudyDetailViewQuery} from '@app/modules/study/models/study-detail-view-query';
 import {MappedStudyPage} from '@app/modules/study/models/mapped-study-page';
+import {StudyUtility} from "@app/modules/study/study-utility";
 
 @Component({
-	selector: 'study-detail',
-	templateUrl: './study-detail.component.html',
-	styleUrls: ['./study-detail.component.scss'],
+    selector: 'study-detail',
+    templateUrl: './study-detail.component.html',
+    styleUrls: ['./study-detail.component.scss'],
 })
 export class StudyDetailComponent {
 
-	public loading = true;
-	public studyPath = `/uuringud/${this.route.snapshot.params.id}`;
-	public study$: Observable<MappedStudyPage> = this.api.studyDetailViewQuery(this.studyPath)
-		.pipe(
-			tap(() => this.loading = false, error => console.log('Error: ', error)),
-			map((response: StudyDetailViewQuery) => response?.data?.route?.entity));
+    public loading = true;
+    public studyPath = `/uuringud/${this.route.snapshot.params.id}`;
+    public study$: Observable<MappedStudyPage> = this.api.studyDetailViewQuery(this.studyPath)
+        .pipe(
+            tap(() => this.loading = false, error => console.log('Error: ', error)),
+            map((response: StudyDetailViewQuery) => StudyUtility.mapStudyDetailData(response?.data?.route?.entity))
+            // map((response: StudyDetailViewQuery) => response?.data?.route?.entity)
+        );
 
-	constructor(
-		private api: StudyApiService,
-		private route: ActivatedRoute,
-	) {
-	}
+    constructor(
+        private api: StudyApiService,
+        private route: ActivatedRoute,
+    ) {
+    }
 
 }
