da2965042967971ff199161bc021f7bebeed5cb7
VoldemarPungar <voldemar.pungar@gmail.com>
2021-06-03 15:05:53 +0300

    HP-857 | run.sh chmod change test vol 2

--- drupal/docker/prod/run.sh
+++ drupal/docker/prod/run.sh
@@ -80,8 +80,8 @@ drush cr
 echo "importing translations"
 drush php-eval "htm_custom_translations_new_import_translations()"
 
-chown apache.apache -R /app/drupal/web/sites/default/files
-chmod -R 644 /app/drupal/web/sites/default/files/php
+chown -R apache:apache /app/drupal/web/sites/default/files
+chmod -R 664 /app/drupal/web/sites/default/files/php
 
 if [ -d /plumbr-agent-installer ] && [[ $ENVIRONMENT == "Live" ]]; then
   /plumbr-agent-installer/PlumbrAgentInstaller --unpack-only --cluster-id="Drupal-${ENVIRONMENT}"
