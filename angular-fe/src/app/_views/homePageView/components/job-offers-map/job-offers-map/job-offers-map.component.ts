123fa61601275cd3b83b66194f102043e4b02447
Evert Prants <evert.prants@twn.ee>
2021-06-10 13:53:20 +0300

    fix linter errors

--- angular-fe/src/app/_views/homePageView/components/job-offers-map/job-offers-map/job-offers-map.component.ts
+++ angular-fe/src/app/_views/homePageView/components/job-offers-map/job-offers-map/job-offers-map.component.ts
@@ -10,7 +10,7 @@ import { JobOffersMapService } from '../job-offers-map.service';
 export class JobOffersMapComponent implements OnInit {
   public loading = true;
   public markers: TootukassaJobOffer[] = [];
-  public options: Object = {
+  public options = {
     polygonType: 'investment',
     icon: '/assets/img/marker_blue.svg',
     clusterStyles: [
@@ -35,43 +35,36 @@ export class JobOffersMapComponent implements OnInit {
 
   constructor(private service: JobOffersMapService) {}
 
-  fixCoordinates(entities: TootukassaJobOffer[]) {
+  fixCoordinates(entities: TootukassaJobOffer[]): TootukassaJobOffer[] {
     const clusters: { [key: string]: TootukassaJobOffer[] } = {};
-
     // Identify clusters
-    for (const i in entities) {
-      const item = entities[i];
+    entities.forEach((item) => {
       const lat = parseFloat(`${item.Lat}`);
       const lon = parseFloat(`${item.Lon}`);
-      if (lat == null) continue;
-      const coords = `${lat}","${lon}`;
+      if (lat != null) {
+        const coords = `${lat}","${lon}`;
 
-      item.Lat = lat;
-      item.Lon = lon;
-
-      if (clusters[coords]) {
-        clusters[coords].push(item);
-      } else {
-        clusters[coords] = [item];
+        item.Lat = lat;
+        item.Lon = lon;
+        if (clusters[coords]) {
+          clusters[coords].push(item);
+        } else {
+          clusters[coords] = [item];
+        }
       }
-    }
+    });
 
     // Spread clusters
-    for (const coord in clusters) {
-      const items = clusters[coord];
-
-      if (items.length <= 1) {
-        continue;
+    Object.keys(clusters).forEach((coordinate) => {
+      const items = clusters[coordinate];
+      if (items.length) {
+        items.forEach((item, index) => {
+          const angle = 360.0 / items.length;
+          item.Lat += -.00004 * Math.cos((+angle * index) / 180 * Math.PI);
+          item.Lon += -.00004 * Math.sin((+angle * index) / 180 * Math.PI);
+        });
       }
-
-      for (const i in items) {
-        const index = parseInt(i, 10);
-        const item = items[i];
-        const angle = 360.0 / items.length;
-        item.Lat += -.00004 * Math.cos((+angle * index) / 180 * Math.PI);
-        item.Lon += -.00004 * Math.sin((+angle * index) / 180 * Math.PI);
-      }
-    }
+    });
 
     return entities;
   }
