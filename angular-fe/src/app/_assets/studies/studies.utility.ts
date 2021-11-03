6bed2dd126b5b044a96bfb23be9d4c84a4ddc375
Madis Põld <madis.pold@twn.ee>
2021-11-03 16:35:57 +0200

    HP-955 Extra studies bugfix

--- angular-fe/src/app/_assets/studies/studies.utility.ts
+++ angular-fe/src/app/_assets/studies/studies.utility.ts
@@ -7,9 +7,9 @@ export function dateSortFn(
 ): number {
   const field1 = a[field] || (a as ExternalQualifications).valjaandmKp;
   const field2 = b[field] || (b as ExternalQualifications).valjaandmKp;
-  const arrA = field1.split('.');
+  const arrA = field1 ? field1.split('.') : []
   const valA = `${arrA[2]}-${arrA[1]}-${arrA[0]}`;
-  const arrB = field2.split('.');
+  const arrB = field2 ? field2.split('.') : []
   const valB = `${arrB[2]}-${arrB[1]}-${arrB[0]}`;
   return +new Date(valB) - +new Date(valA);
 }
