987a572882923d01afea11a28d12dd0c08239a72
mitka0260 <mitkaut@gmail.com>
2022-02-11 11:45:26 +0200

    npx @angular/cli@12 update @angular/core@12 @angular/cli@12 --allow-dirty --force (@agm/core requires angular/common 9 || 10, would install 12 amd angular/core 9|10, would install 12)
     Remove deprecated options from 'angular.json' that are no longer present in v12.
        "build" target in "haridusportaal-fe" project is using a third-party builder.
        You may need to adjust the options to retain the existing behavior.
        For more information, see the breaking changes section within the release notes: https://github.com/angular/angular-cli/releases/tag/v12.0.0
        "serve" target in "haridusportaal-fe" project is using a third-party builder.
        You may need to adjust the options to retain the existing behavior.
        For more information, see the breaking changes section within the release notes: https://github.com/angular/angular-cli/releases/tag/v12.0.0
        "lint" target in "haridusportaal-fe" project is using a third-party builder.
        You may need to adjust the options to retain the existing behavior.
        For more information, see the breaking changes section within the release notes: https://github.com/angular/angular-cli/releases/tag/v12.0.0
      Migration completed.
    
      Remove 'emitDecoratorMetadata' TypeScript compiler option.
        Decorator metadata is no longer needed by Angular.
        Read more about this here: https://www.typescriptlang.org/docs/handbook/decorators.html#metadata
          Skipping migration as the workspace uses third-party builders which may require "emitDecoratorMetadata" TypeScript compiler option.
        Migration completed.

--- angular-fe/src/polyfills.ts
+++ angular-fe/src/polyfills.ts
@@ -64,7 +64,7 @@ import './custom-polyfills/nodelist-foreach.js';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 import '@angular/localize/init';
 
 
