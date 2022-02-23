b85df42b791bb2850430b70cbb10d6ce28a8b23a
VoldemarPungar <voldemar.pungar@gmail.com>
2022-02-23 16:36:42 +0200

    HP-1055 | Drupal updates

--- vendor/composer/composer/src/Composer/Util/AuthHelper.php
+++ vendor/composer/composer/src/Composer/Util/AuthHelper.php
@@ -92,6 +92,20 @@ class AuthHelper
             $message = "\n";
 
             $rateLimited = $gitHubUtil->isRateLimited($headers);
+            $requiresSso = $gitHubUtil->requiresSso($headers);
+
+            if ($requiresSso) {
+                $ssoUrl = $gitHubUtil->getSsoUrl($headers);
+                $message = 'GitHub API token requires SSO authorization. Authorize this token at ' . $ssoUrl . "\n";
+                $this->io->writeError($message);
+                if (!$this->io->isInteractive()) {
+                    throw new TransportException('Could not authenticate against ' . $origin, 403);
+                }
+                $this->io->ask('After authorizing your token, confirm that you would like to retry the request');
+
+                return array('retry' => true, 'storeAuth' => $storeAuth);
+            }
+
             if ($rateLimited) {
                 $rateLimit = $gitHubUtil->getRateLimit($headers);
                 if ($this->io->hasAuthentication($origin)) {
