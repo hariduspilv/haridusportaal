7f8aa6128dd561399538f937eb4d363a349cfd79
mitka0260 <mitkaut@gmail.com>
2022-04-25 23:13:09 +0300

    HP-1004 - added styles for mobile

--- angular-fe/src/app/_assets/header/dropdown-menu/dropdown-menu.component.ts
+++ angular-fe/src/app/_assets/header/dropdown-menu/dropdown-menu.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { LanguageCodes, SettingsService } from "@app/_services";
 import { TranslateService } from "@app/_modules/translate/translate.service";
+import { DeviceDetectorService } from "ngx-device-detector";
 
 @Component({
 	selector: 'dropdown-menu',
 	templateUrl: 'dropdown-menu.component.html',
 	styleUrls: ['dropdown-menu.component.scss']
 })
-export class DropdownMenuComponent {
-	@Input() options: Record<string, string | LanguageCodes>[];
+export class DropdownMenuComponent implements OnInit {
+	@Input() languages: Record<string, string | LanguageCodes>[];
 	@Output() onLanguageChange = new EventEmitter<LanguageCodes>();
 
 	activeLanguageCode: string = this.settings.currentAppLanguage;
@@ -17,11 +18,26 @@ export class DropdownMenuComponent {
 	constructor(
 		private settings: SettingsService,
 		private translate: TranslateService,
+		private deviceDetector: DeviceDetectorService,
 	) {}
 
-	onClick(code: LanguageCodes) {
+	ngOnInit() {
+		this.setStylesForMobile();
+	}
+
+	onClick(code: LanguageCodes): void {
 		this.activeLanguageCode = code;
 		this.activeLanguage = this.translate.get(`frontpage.${code}`);
 		this.onLanguageChange.emit(code);
+	}
+
+	private setStylesForMobile(): void {
+		if (!this.deviceDetector.isDesktop()) {
+			const dropdown = document.querySelector('.language-dropdown') as HTMLElement;
+			const dropbtn = dropdown.querySelector('.dropbtn') as HTMLElement;
+
+			dropdown.style.marginRight = '0';
+			dropbtn.style.paddingLeft = '0';
+		}
 	}
 }
