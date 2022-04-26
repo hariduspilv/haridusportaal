0d8387255d67b37608549ca822b50a7f4e971ba9
mitka0260 <mitkaut@gmail.com>
2022-04-26 10:34:02 +0300

    HP-1004 - corrections

--- angular-fe/src/app/_assets/header/dropdown-menu/dropdown-menu.component.ts
+++ angular-fe/src/app/_assets/header/dropdown-menu/dropdown-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { LanguageCodes, SettingsService } from "@app/_services";
 import { TranslateService } from "@app/_modules/translate/translate.service";
 import { DeviceDetectorService } from "ngx-device-detector";
@@ -8,7 +8,7 @@ import { DeviceDetectorService } from "ngx-device-detector";
 	templateUrl: 'dropdown-menu.component.html',
 	styleUrls: ['dropdown-menu.component.scss']
 })
-export class DropdownMenuComponent implements OnInit {
+export class DropdownMenuComponent {
 	@Input() languages: Record<string, string | LanguageCodes>[];
 	@Output() onLanguageChange = new EventEmitter<LanguageCodes>();
 
@@ -22,41 +22,9 @@ export class DropdownMenuComponent implements OnInit {
 		private deviceDetector: DeviceDetectorService,
 	) {}
 
-	ngOnInit() {
-		this.setStylesForMobile();
-	}
-
 	onClick(code: LanguageCodes): void {
 		this.activeLanguageCode = code;
 		this.activeLanguage = this.translate.get(`frontpage.${code}`);
 		this.onLanguageChange.emit(code);
-	}
-
-	private setStylesForMobile(): void {
-		setTimeout(() => {
-			const dropdown = document.querySelector('.language-dropdown') as HTMLElement;
-			const dropbtn = dropdown.querySelector('.dropbtn') as HTMLElement;
-			const dropdownContent = dropdown.querySelector('.dropdown-content') as HTMLElement;
-			const links = dropdownContent.querySelectorAll('a');
-
-			if (this.isDesktop) {
-				dropdownContent.style.right = '-1.75rem';
-
-				links.forEach((link) => {
-					link.style.marginLeft = '1rem';
-					link.style.marginRight = '1rem';
-				});
-			}
-
-			if (!this.isDesktop) {
-				dropdown.style.marginRight = '0';
-				dropbtn.style.paddingLeft = '0';
-
-				links.forEach((link) => {
-					link.style.marginLeft = '.25rem';
-					link.style.marginRight = '.25rem';
-				});
-			}
-		});
 	}
 }
