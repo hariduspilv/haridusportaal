ecca788c5f1010a501ccd792e034fb5474b7568c
Madis Loik <madis.loik@twn.ee>
2022-11-09 11:23:47 +0200

    Form encoding fixes

--- drupal/web/modules/custom/htm_custom_oska/src/Form/OskaImportDataForm.php
+++ drupal/web/modules/custom/htm_custom_oska/src/Form/OskaImportDataForm.php
@@ -76,6 +76,10 @@ class OskaImportDataForm extends FormBase {
                 $header_info = $this->detectCSVFileDelimiter($file_upload->getRealPath());
 
                 foreach($header_info['keys'] as $key => $value) {
+                  $value = utf8_encode($value);
+                  if (str_contains($value,'ï»¿')) {
+                    $value = str_replace('ï»¿','',$value);
+                  }
                     $header_info['keys'][cleanString($key)] = cleanString($value);
                 }
 
