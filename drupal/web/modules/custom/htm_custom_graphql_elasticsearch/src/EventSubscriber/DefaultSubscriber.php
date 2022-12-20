5c2bc3806953a06a04aa330e0bc4d4397f112987
Madis Loik <madis.loik@gmail.com>
2022-12-20 19:13:28 +0200

    Indexnamerequest fix

--- drupal/web/modules/custom/htm_custom_graphql_elasticsearch/src/EventSubscriber/DefaultSubscriber.php
+++ drupal/web/modules/custom/htm_custom_graphql_elasticsearch/src/EventSubscriber/DefaultSubscriber.php
@@ -72,7 +72,7 @@ class DefaultSubscriber implements EventSubscriberInterface {
      *   The event.
      */
     public function elasticsearchConnectorPrepareIndex(PrepareIndexEvent $event) {
-      $index = $this->loadIndexFromIndexName($event->getName());
+      $index = $this->loadIndexFromIndexName($event->getIndexName());
       $settings = $index->getThirdPartySettings('elasticsearch_connector');
 
         $indexConfig = $event->getIndexConfig();
