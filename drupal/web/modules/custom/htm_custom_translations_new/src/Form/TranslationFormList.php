2102718c52e6175f3a2e9f849c7d08c3bd53f3e0
Madis Loik <madis.loik@twn.ee>
2023-02-10 09:54:28 +0200

    Translations as state start

--- drupal/web/modules/custom/htm_custom_translations_new/src/Form/TranslationFormList.php
+++ drupal/web/modules/custom/htm_custom_translations_new/src/Form/TranslationFormList.php
@@ -39,16 +39,18 @@ class TranslationFormList extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('htm_custom_translations_new.translation');
+
 		$config_values = $config->get();
 
+    $state_keys = \Drupal::state()->get('translation_keys');
     $header = [
     	'key' => $this->t('Translation key'),
 			'translation_languages' => $this->t('Languages'),
 			'translation_value' => $this->t('Translation'),
-			'operations' => $this->t('Operations'),
+//			'operations' => $this->t('Operations'),
 		];
 
-		$form['table'] = [
+    $form['table'] = [
 			'#type' => 'table',
 			'#header' => $header,
 			'#empty' => t('No translations found. <a href="@add-url">Add translation.</a>', [
@@ -56,7 +58,51 @@ class TranslationFormList extends ConfigFormBase {
 			]),
 		];
 
-		$languages = \Drupal::languageManager()->getLanguages();
+    $languages = \Drupal::languageManager()->getLanguages();
+    foreach ($state_keys as $state_key => $state_key_value){
+      $form['table'][$state_key]['key'] = [
+        '#plain_text' => $state_key
+      ];
+      foreach ($languages as $language) {
+        $form['table'][$state_key]['translation_languages'][] = [
+          '#type' => 'item',
+          '#title' => $language->getId(),
+        ];
+      foreach ($state_key_value as $state_translation) {
+        if (str_contains($state_translation,'translation_type')) {
+          $translation_type = $translation = \Drupal::state()->get($state_translation);
+        }
+        else {
+          if (str_contains($state_translation, '.' . $language->getId())) {
+            $translation_value = \Drupal::state()->get($state_translation);
+            if (is_array($translation_value)) {
+              $translation_value = $translation_value['value'];
+            }
+            $form['table'][$state_key]['translation_value'][] = [
+              '#type' => 'item',
+              '#title' => ($translation_value) ? $translation_value : 'MISSING',
+            ];
+          }
+        }
+        }
+      }
+
+//      $form['table'][$state_key]['operations'] = [
+//        '#type' => 'operations',
+//        '#links' => []
+//      ];
+//      $form['table'][$state_key]['operations']['#links']['edit'] = [
+//        'title' => $this->t('Edit'),
+//        'url' => Url::fromRoute('htm_custom_translations_new.edit_translation', ['translation_key' => $state_key]),
+//      ];
+//      $form['table'][$state_key]['operations']['#links']['delete'] = [
+//        'title' => $this->t('Delete'),
+//        'url' => Url::fromRoute('htm_custom_translations_new.delete_translation', ['translation_key' => $state_key]),
+//      ];
+    }
+    return $form;
+
+    $languages = \Drupal::languageManager()->getLanguages();
 		$mapped_config = $this->formatter->flatten($config_values);
     foreach($mapped_config as $key => $data){
 
