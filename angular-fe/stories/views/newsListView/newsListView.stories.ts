b9346c469096c0f430995c2fbf367b666adafbed
Evert Prants <evert.prants@twn.ee>
2023-05-15 14:22:22 +0300

    HP-1272 fix storybook

--- angular-fe/stories/views/newsListView/newsListView.stories.ts
+++ angular-fe/stories/views/newsListView/newsListView.stories.ts
@@ -2,7 +2,12 @@ import { storiesOf } from '@storybook/angular';
 import { TranslateService } from '@app/_modules/translate/translate.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@app/_modules/translate/translate.module';
-import { SettingsService, RippleService, ModalService, AnalyticsService } from '@app/_services';
+import {
+	SettingsService,
+	RippleService,
+	ModalService,
+	AnalyticsService,
+} from '@app/_services';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import newsListViewHtml from './newsListView.html';
 import newsListViewMd from './newsListView.md';
@@ -13,23 +18,23 @@ import { ActivatedRoute } from '@angular/router';
 import { AddressService } from '@app/_services/AddressService';
 
 const moduleMetadata = {
-  imports: [
-    RouterTestingModule,
-    TranslateModule.forRoot(),
-    BrowserAnimationsModule,
-    NewsListViewModule,
-    AssetsModule,
-  ],
-  providers: [
-    AnalyticsService,
-    AddressService,
-    TranslateService,
-    SettingsService,
-    RippleService,
-    ModalService,
-    QueryParamsService,
-    { provide: ActivatedRoute, useValue: {} },
-  ],
+	imports: [
+		RouterTestingModule,
+		TranslateModule.forRoot(),
+		BrowserAnimationsModule,
+		NewsListViewModule,
+		AssetsModule,
+	],
+	providers: [
+		AnalyticsService,
+		AddressService,
+		TranslateService,
+		SettingsService,
+		RippleService,
+		ModalService,
+		QueryParamsService,
+		{ provide: ActivatedRoute, useValue: {} },
+	],
 };
 
 // const stories = storiesOf('List views', module);
