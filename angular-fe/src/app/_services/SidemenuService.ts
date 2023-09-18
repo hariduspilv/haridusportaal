f19ed951acd6b9bfe5b93300243ef32eaa1d286b
Madis Loik <madis.loik@twn.ee>
2023-09-18 16:05:43 +0300

    Sidemenu service change

--- angular-fe/src/app/_services/SidemenuService.ts
+++ angular-fe/src/app/_services/SidemenuService.ts
@@ -1,81 +1,92 @@
+import { DeviceDetectorService } from 'ngx-device-detector';
 import { Injectable } from '@angular/core';
 import { Subject, BehaviorSubject } from 'rxjs';
 import { translatePath } from "@app/_core/router-utility";
-
+import { Location } from '@angular/common';
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class SidemenuService {
+	// Open menu by default when front page is opened
+	private subject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+		this.isFrontPage() &&
+		this.deviceDetectorService.isDesktop()
+	);
+	private theme: BehaviorSubject<string> = new BehaviorSubject<string>('default');
+	private langSwitch = new Subject<any>();
 
-  private subject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private theme: BehaviorSubject<string> = new BehaviorSubject<string>('default');
-  private langSwitch = new Subject<any>();
-
-  /**
-   * These paths should not open the menu automatically on load.
-   */
-  public ignoreAutoOpen = ['/', translatePath('/õppimine'), translatePath('/karjäär'), translatePath('/õpetaja'), translatePath('/noored')];
-  public themes = {
-    õppimine: 'learning',
+	/**
+	 * These paths should not open the menu automatically on load.
+	 */
+	public ignoreAutoOpen = [translatePath('/õppimine'), translatePath('/karjäär'), translatePath('/õpetaja'), translatePath('/noored')];
+	public themes = {
+		õppimine: 'learning',
 		learning: 'learning',
-    õpetamine: 'teaching',
+		õpetamine: 'teaching',
 		teaching: 'teaching',
-    karjäär: 'career',
+		karjäär: 'career',
 		career: 'career',
-    noored: 'youth',
+		noored: 'youth',
 		youth: 'youth',
-    noortevaldkond: 'youth',
-  };
+		noortevaldkond: 'youth',
+	};
 
-  force = false;
-  lang: any;
+	constructor(private location: Location, private deviceDetectorService: DeviceDetectorService) {}
 
-  get isVisible() {
-    return this.subject.getValue();
-  }
+	force = false;
+	lang: any;
 
-  get isVisibleSubscription() {
-    return this.subject;
-  }
+	get isVisible() {
+		return this.subject.getValue();
+	}
 
-  get currentTheme(): string {
-    return this.theme.getValue();
-  }
+	get isVisibleSubscription() {
+		return this.subject;
+	}
 
-  get themeSubscription() {
-    return this.theme;
-  }
+	get currentTheme(): string {
+		return this.theme.getValue();
+	}
 
-  get isMobileView() {
-    return window.innerWidth <= 1024;
-  }
+	get themeSubscription() {
+		return this.theme;
+	}
 
-  toggle() {
-    this.subject.next(!this.subject.getValue());
-  }
+	get isMobileView() {
+		return window.innerWidth <= 1024;
+	}
 
-  close() {
-    this.subject.next(false);
-  }
+	toggle() {
+		this.subject.next(!this.subject.getValue());
+	}
 
-  setTheme(theme: string): void {
-    this.theme.next(theme);
-  }
+	close() {
+		this.subject.next(false);
+	}
 
-  triggerLang(force:boolean = false) {
+	setTheme(theme: string): void {
+		this.theme.next(theme);
+	}
 
-    // force language switch on login to load main nav
-    this.force = force;
+	triggerLang(force:boolean = false) {
 
-    this.langSwitch.next({ any: Math.random() * 1000000 });
-  }
+		// force language switch on login to load main nav
+		this.force = force;
 
-  resetPageFocus(): void {
-    if (document.activeElement instanceof HTMLElement) {
-      document.activeElement.blur();
-    }
-    document.body.setAttribute('tabindex', '-1');
-    document.body.focus();
-    document.body.removeAttribute('tabindex');
-  }
+		this.langSwitch.next({ any: Math.random() * 1000000 });
+	}
+
+	resetPageFocus(): void {
+		if (document.activeElement instanceof HTMLElement) {
+			document.activeElement.blur();
+		}
+		document.body.setAttribute('tabindex', '-1');
+		document.body.focus();
+		document.body.removeAttribute('tabindex');
+	}
+
+	isFrontPage(): boolean {
+		const path = decodeURI(this.location?.path());
+		return !path;
+	}
 }
